e307f44f97cca7a698819cdbe53beee67c5e4daf
feanar729 <feanar729@gmail.com>
2018-11-13 11:35:00 +0900

    update & refactoring: dropdown debounce기능 구현, menu display 버그 수정 필요, CSS 일부 요소 refactoring

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 import HiddenTopMenu from "./hiddenTopMenu.js";
 import LayerManager from "./layerManager.js";
 
-const searchDocEle = (classEle) => { return document.querySelector(classEle); }
+const searchDocEle = (classEle) => { return document.querySelector(classEle); };
 
 const layerManager = new LayerManager({
   dimmedEle: searchDocEle("#nav-dimmed-cover"),
   departmentLayerEle: searchDocEle(".nav-department"),
   listLayerEle: searchDocEle(".departments-layer-list"),
-  displayOuterEle: searchDocEle(".list-layer"),
-  innerLayerEle: searchDocEle(".inner-layer"),
+  innerLayerEle: document.querySelector(".inner-layer"),
 });
 
 const topMenu = new HiddenTopMenu({
@@ -19,7 +18,7 @@ const topMenu = new HiddenTopMenu({
   hiddenAllContentsEle: searchDocEle(".hidden-plans"),
   transDisplayEle: searchDocEle(".trans-display-block"),
   transShowOuterEle: searchDocEle(".trans-show-outerContents"),
-  transHiddenOuterEle: searchDocEle(".trans-hidden-outerContents"),
+  transHiddenOuterEle: searchDocEle(".trans-hidden-outerContents")
 });
 
 layerManager.init();
--- js/layerManager.js
+++ js/layerManager.js
@@ -4,32 +4,61 @@ export default class LayerManager {
   }
 
   init() {
+    // this.setMouseMoveBlockDisplay(departmentsEle, dimmedEle, listLayerEle, timer);
+    // this.setMouseOutNoneDisplay(departmentsEle, dimmedEle, listLayerEle);
     this.mouseOverOutLayer();
   }
 
+  setMouseMoveBlockDisplay(departmentsEle, dimmedEle, listLayerEle, timer) {
+  }
+
+  setMouseOutNoneDisplay(departmentsEle, dimmedEle, listLayerEle) {
+  }
+
   mouseOverOutLayer() {
-    const dimmedEle = this.ele.dimmedEle;
     const departmentsEle = this.ele.departmentLayerEle;
+    const dimmedEle = this.ele.dimmedEle;
     const listLayerEle = this.ele.listLayerEle;
-    const displayOuterEle = this.ele.displayOuterEle;
-    const innerLayerEle = this.ele.innerLayerEle;
+    let timer = false;
 
-    departmentsEle.addEventListener("mouseover", () => {
-      dimmedEle.setAttribute("style", "display: block;");
-      listLayerEle.setAttribute("style", "display: block;");
+    departmentsEle.addEventListener('mousemove', () => {
+      clearTimeout(timer);
+      timer = window.setTimeout(() => {
+        listLayerEle.setAttribute("style", "display: block;");
+        dimmedEle.setAttribute("style", "display: block;");
+      }, 350);
     });
 
-    departmentsEle.addEventListener("mouseout", () => {
-      dimmedEle.setAttribute("style", "display: none;");
-      listLayerEle.setAttribute("style", "display: none;");
+    departmentsEle.addEventListener("mouseout", (e) => {
+      const displayStatus = e.path[2].lastElementChild.style.display;
+      // console.log(e.path[2].childNodes[3].style.display !== "block")
+
+      console.log(e);
+      // console.log(e.path[2].lastElementChild.style.dis play)
+      // console.log(e.target.children);
+      console.log(e.target);
+      console.log(e.relatedTarget);
+      if (displayStatus !== "block") {
+        clearTimeout(timer);
+        listLayerEle.setAttribute("style", "display: none;");
+        dimmedEle.setAttribute("style", "display: none;");
+      }
+    });
+  }
+
+  checkPointInTriangle(p, a, b, c) {
+    const point = {};
+    window.addEventListener("mousemove", (m) => {
+      point = { x: m.x, y: m.y };
+      console.log(`x:${point.x} | y:${point.y}`, "mousepoint")
     });
 
-    displayOuterEle.addEventListener("mouseenter", () => {
-      innerLayerEle.classList.add("inner-display-layer");
-    });
+    let aPoint = (a.y * c.x - a.x * c.y + (c.y - a.y) * p.x + (a.x - c.x) * p.y);
+    let bPoint = (a.x * b.y - a.y * b.x + (a.y - b.y) * p.x + (b.x - a.x) * p.y);
 
-    displayOuterEle.addEventListener("mouseleave", () => {
-      innerLayerEle.classList.remove("inner-display-layer");
-    });
+    if (aPoint <= 0 || bPoint <= 0) return false;
+
+    let mousePoint = (-b.y * c.x + a.y * (-b.x + c.x) + a.x * (b.y - c.y) + b.x * c.y);
+    return (aPoint + bPoint) < mousePoint;
   }
 }
