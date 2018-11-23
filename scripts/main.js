faac2d93dd5f83d82988c61abf110b85724ef9a0
Boyoung KIM <boyoung@vanary.com>
2018-11-23 22:14:22 +0900

    Improve reusability of StickyNav class

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,69 @@
-function setBodyHeight() {
-  const body = document.querySelector("body");
-  const main = document.querySelector("main");
-  body.style.height = `${body.clientHeight + main.clientHeight - 1}px`; // Reduce 1px to remove white line on page bottom
-}
-class PrimeDetailLayer {
-  static displayOnClick() {
-    const stickyBar = document.querySelector(".stickyNav");
-    const seeMoreLink = stickyBar.querySelector(
+class StickyNav {
+  constructor({ htmlEl, threshold }) {
+    this.base = htmlEl;
+    this.threshold = threshold;
+  }
+  displayDetailOnClick() {
+    const seeMoreLink = this.base.querySelector(
       ".stickyNav__morePlanBtn .morePlanBtn__link"
     );
     seeMoreLink.addEventListener("click", () => {
-      const detailLayer = stickyBar.querySelector(".stickyNav__detailLayer");
-      const miniBar = stickyBar.querySelector(".stickyNav__miniBar");
+      const detailLayer = this.base.querySelector(".stickyNav__detailLayer");
+      const miniBar = this.base.querySelector(".stickyNav__miniBar");
 
       detailLayer.classList.replace("closed", "opened");
       miniBar.classList.replace("opened", "closed");
-      window.removeEventListener("scroll", StickyBar.updateVisibility);
+      window.removeEventListener("scroll", this.updateVisibility);
 
       event.preventDefault();
     });
   }
-  static closeOnClick() {
-    const detailLayer = document.querySelector(".stickyNav__detailLayer");
+  closeDetailOnClick() {
+    const detailLayer = this.base.querySelector(".stickyNav__detailLayer");
     const detailLayerCLoseBtns = detailLayer.querySelectorAll(".closeBtn");
 
     detailLayerCLoseBtns.forEach(el => {
       el.addEventListener("click", () => {
         detailLayer.classList.replace("opened", "closed");
-        window.addEventListener("scroll", StickyBar.updateVisibility);
-        StickyBar.updateVisibility();
+        window.addEventListener("scroll", this.updateVisibility);
+        this.updateVisibility();
 
         event.preventDefault();
       });
     });
   }
-}
-class StickyBar {
-  static updateVisibility() {
-    const mastheadHeight = document.querySelector(".masthead").clientHeight;
-    const headerHeight = document.querySelector(".header").clientHeight;
-    const miniBar = document.querySelector(".stickyNav__miniBar");
-    if (window.pageYOffset < mastheadHeight + headerHeight) {
+  updateVisibility() {
+    const miniBar = this.base.querySelector(".stickyNav__miniBar");
+    if (window.pageYOffset < this.threshold) {
       miniBar.classList.replace("opened", "closed");
       return;
     }
     miniBar.classList.replace("closed", "opened");
   }
+  setBodyHeight() {
+    const body = document.querySelector("body");
+    const totalHeight = Array.from(body.children).reduce((acc, el) => {
+      return acc + el.clientHeight;
+    }, 0);
+    body.style.height = `${totalHeight - 1}px`; // Reduce 1px to remove white line on page bottom
+  }
 }
 
-window.addEventListener("load", () => {
-  setBodyHeight(); //Extend body height after page load to fix position:sticky above position:absolute main element
-  PrimeDetailLayer.displayOnClick();
-  PrimeDetailLayer.closeOnClick();
+const stickyPlansLayer = new StickyNav({
+  htmlEl: document.querySelector(".stickyNav"),
+  threshold:
+    document.querySelector(".masthead").clientHeight +
+    document.querySelector(".header").clientHeight
 });
 
-/* Display sticky bar on scroll */
-window.addEventListener("scroll", StickyBar.updateVisibility);
+window.addEventListener("DOMContentLoaded", () => {
+  stickyPlansLayer.displayDetailOnClick();
+  stickyPlansLayer.closeDetailOnClick();
+});
+window.addEventListener("load", () => {
+  stickyPlansLayer.setBodyHeight(); //Extend body height to make position:sticky work properly
+  window.addEventListener("scroll", () => {
+    // Display sticky bar on scroll
+    stickyPlansLayer.updateVisibility();
+  });
+});
