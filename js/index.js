978a73f197ed6d19e6e655f440a49ca11df9a3cf
feanar729 <feanar729@gmail.com>
2018-12-14 15:01:43 +0900

    refactoring: querySelector method refactoring 진행

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 import HiddenTopMenu from "./hiddenTopMenu.js";
 import CarouselMenu from "./carousel.js";
 
+const searchDocEle = (classEle) => { return document.querySelector(classEle); };
+const searchDocAllEle = (classEle) => { return document.querySelectorAll(classEle); };
+
 const topMenu = new HiddenTopMenu({
-  closeButtonEle: document.querySelector(".close-button"),
-  otherCloseButtonEle: document.querySelector(".comparison-close-button"),
-  expandEle: document.querySelector(".expand-membership-card"),
-  hiddenInnerContentsEle: document.querySelector(".hidden-inner-contents"),
-  hiddenAllContentsEle: document.querySelector(".hidden-plans"),
-  transDisplayEle: document.querySelector(".trans-display-block"),
-  transShowOuterEle: document.querySelector(".trans-show-outerContents"),
-  transHiddenOuterEle: document.querySelector(".trans-hidden-outerContents"),
+  closeButtonEle: searchDocEle(".close-button"),
+  otherCloseButtonEle: searchDocEle(".comparison-close-button"),
+  expandEle: searchDocEle(".expand-membership-card"),
+  hiddenInnerContentsEle: searchDocEle(".hidden-inner-contents"),
+  hiddenAllContentsEle: searchDocEle(".hidden-plans"),
+  transDisplayEle: searchDocEle(".trans-display-block"),
+  transShowOuterEle: searchDocEle(".trans-show-outerContents"),
+  transHiddenOuterEle: searchDocEle(".trans-hidden-outerContents")
 });
 
 const carousel = new CarouselMenu();
