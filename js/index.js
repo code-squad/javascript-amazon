d8aa3c182ab37a59323fb5e9b3fbcd366a8c719e
feanar729 <feanar729@gmail.com>
2018-12-14 16:22:44 +0900

    refactoring: index.js querySelector refactoring 진행

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ import HiddenTopMenu from "./hiddenTopMenu.js";
 import LayerManager from "./layerManager.js";
 import CarouselMenu from "./carousel.js";
 
-const searchDocEle = (classEle) => { return document.querySelector(classEle); };
-const searchDocAllEle = (classEle) => { return document.querySelectorAll(classEle); };
+const $ = (classEle) => { return document.querySelector(classEle); };
+const $All = (classEle) => { return document.querySelectorAll(classEle); };
 
 const layerManager = new LayerManager({
-  dimmedEle: searchDocEle("#nav-dimmed-cover"),
-  titleEle: searchDocEle(".departments-title-layer"),
-  departmentListEle: searchDocEle(".departments-layer-list"),
-  outerEle: searchDocEle(".outer-layer"),
-  contentEle: searchDocAllEle(".content-layer"),
+  dimmedEle: $("#nav-dimmed-cover"),
+  titleEle: $(".departments-title-layer"),
+  departmentListEle: $(".departments-layer-list"),
+  outerEle: $(".outer-layer"),
+  contentEle: $All(".content-layer"),
 });
 
 const topMenu = new HiddenTopMenu({
-  closeButtonEle: searchDocEle(".close-button"),
-  otherCloseButtonEle: searchDocEle(".comparison-close-button"),
-  expandEle: searchDocEle(".expand-membership-card"),
-  hiddenInnerContentsEle: searchDocEle(".hidden-inner-contents"),
-  hiddenAllContentsEle: searchDocEle(".hidden-plans"),
-  transDisplayEle: searchDocEle(".trans-display-block"),
-  transShowOuterEle: searchDocEle(".trans-show-outerContents"),
-  transHiddenOuterEle: searchDocEle(".trans-hidden-outerContents")
+  closeButtonEle: $(".close-button"),
+  otherCloseButtonEle: $(".comparison-close-button"),
+  expandEle: $(".expand-membership-card"),
+  hiddenInnerContentsEle: $(".hidden-inner-contents"),
+  hiddenAllContentsEle: $(".hidden-plans"),
+  transDisplayEle: $(".trans-display-block"),
+  transShowOuterEle: $(".trans-show-outerContents"),
+  transHiddenOuterEle: $(".trans-hidden-outerContents")
 });
 
 const carousel = new CarouselMenu();
