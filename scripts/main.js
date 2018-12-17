5baf0618a5b7d518675d7a6c4de82f887f7165ae
Boyoung KIM <boyoung@vanary.com>
2018-12-17 14:09:31 +0900

    Add mini carousel an initial content fetch method

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import StickyNav from './stickyNav/StickyNav.js';
 import MegaMenu from './megaMenu/MegaMenu.js';
-// import MiniCarousel from './miniCarousel/MiniCarousel.js';
+import MiniCarousel from './miniCarousel/MiniCarousel.js';
 
 const stickyPlansLayer = new StickyNav({
   htmlEl: document.querySelector('.stickyNav'),
@@ -13,12 +13,15 @@ const megaMenu = new MegaMenu({
   triggerEl: document.querySelector('.megaMenu__trigger'),
   canvasEl: document.querySelector('.megaMenu__trackerCanvas'),
 });
+const miniCarousel = new MiniCarousel();
 
 window.addEventListener('DOMContentLoaded', () => {
   stickyPlansLayer.displayDetailOnClick();
   stickyPlansLayer.closeDetailOnClick();
   megaMenu.setMenuOpenMouseEvent();
   megaMenu.setDetailOpenMouseEvent();
+  miniCarousel.fetchPrimeMusicCarousel();
+  miniCarousel.fetchPrimeOriginalCarousel();
 });
 window.addEventListener('load', () => {
   stickyPlansLayer.setMainHeight(); // Extend body height to make position:sticky work properly
--- scripts/miniCarousel/MiniCarousel.js
+++ scripts/miniCarousel/MiniCarousel.js
@@ -1,28 +1,41 @@
-function createLiWithJSON({ id, src, alt }) {
-  return `<li><img data-imgId="${id}" src="${src}" alt="${alt}" /></li>`;
-}
+export default class MiniCarousel {
+  createLiHTMLWithJSON({ id, src, alt }) {
+    return `<li><img data-imgId="${id}" src="${src}" alt="${alt}" /></li>`;
+  }
 
-function fetchCarouselJSON(resURI, targetEl, useFetch = false) {
-  if (useFetch) {
+  fetchCarouselJSON(resURI, targetEl, useXHR = false) {
+    if (useXHR) {
+      /* equivalent XHR codes */
+    }
+
     fetch(resURI)
       .then((res) => {
         if (!res.ok) throw new Error(`HTTP error, status = ${res.status}`);
 
         return res.json();
       })
-      .then(res => res.reduce((acc, obj) => `${acc}${createLiWithJSON(obj)}\n`, '\n'))
+      .then(res => res.reduce((acc, obj) => `${acc}${this.createLiHTMLWithJSON(obj)}\n`, '\n'))
       .then((res) => {
-        targetEl.innerHTML = res;
+        const target = targetEl;
+        target.innerHTML = res;
       });
   }
 
-  /* equivalent XHR codes */
+  fetchPrimeMusicCarousel() {
+    const URIPrimeMusic = '/res/primeMusic.json';
+    const primeMusicEl = document.querySelector(
+      '.horizontalBanners__prime-music .mini-carousel .carousel__cardSlot',
+    );
+
+    this.fetchCarouselJSON(URIPrimeMusic, primeMusicEl);
+  }
+
+  fetchPrimeOriginalCarousel() {
+    const URIPrimeOriginal = '/res/primeOriginal.json';
+    const primeOriginalEl = document.querySelector(
+      '.horizontalBanners__prime-original .mini-carousel .carousel__cardSlot',
+    );
+
+    this.fetchCarouselJSON(URIPrimeOriginal, primeOriginalEl);
+  }
 }
-
-const URIPrimeMusic = '/res/primeMusic.json';
-const primeMusicEl = document.querySelector(
-  '.horizontalBanners__prime-music .mini-carousel .placeholder',
-);
-// const URIPrimeOriginal = '/res/primeOriginal.json';
-
-fetchCarouselJSON(URIPrimeMusic, primeMusicEl, true);
