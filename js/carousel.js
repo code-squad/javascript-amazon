d1cc7ed45e2ece030cd4297ca28b17dff8c079b2
feanar729 <feanar729@gmail.com>
2018-12-21 14:53:39 +0900

    update & refactoring: CSS value 수정, refactoring & 좌우슬라이드 이동 범위 설정 기능 추가

--- js/carousel.js
+++ js/carousel.js
@@ -5,13 +5,12 @@ export default class Carousel {
       innerCarousel: layer.innerCarousel,
       carouselItem: layer.a_CarouselItem,
       allCarousel: layer.a_CarouselList,
-      prevIMG: layer.prev,
-      nextIMG: layer.next,
+      prev: layer.prev,
+      next: layer.next,
     }
     this.itemWidth = this.layer.carouselItem.offsetWidth;
     this.itemHeight = this.layer.carouselItem.offsetHeight;
     this.itemLength = this.layer.allCarousel.length;
-    console.log(this.layer.prevIMG, this.layer.nextIMG)
 
     this.offset = 0;
     this.currentItem = 1;
@@ -39,19 +38,40 @@ export default class Carousel {
   }
 
   attachEvent() {
-    this.layer.prevIMG.addEventListener("click", this.moveToPrev.bind(this));
-    this.layer.nextIMG.addEventListener("click", this.moveToNext.bind(this));
+    this.layer.prev.addEventListener("click", this.moveToPrev.bind(this));
+    this.layer.next.addEventListener("click", this.moveToNext.bind(this));
   }
 
   moveToPrev() {
     this.offset += this.itemWidth;
     this.move();
     this.currentItem--;
+    this.checkMovable();
   }
 
   moveToNext() {
     this.offset -= this.itemWidth;
     this.move();
     this.currentItem++;
+    this.checkMovable();
+  }
+
+  // prev, next 버튼 활성화/비활성화 결정
+  checkMovable() {
+    if (this.currentItem === 1) {
+      this.layer.prev.disabled = true;
+      this.layer.prev.classList.add('disabled');
+    } else {
+      this.layer.prev.disabled = false;
+      this.layer.prev.classList.remove('disabled');
+    }
+
+    if (this.currentItem === this.itemLength) {
+      this.layer.next.disabled = true;
+      this.layer.next.classList.add('disabled');
+    } else {
+      this.layer.next.disabled = false;
+      this.layer.next.classList.remove('disabled');
+    }
   }
 }
