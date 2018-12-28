46187b580cd77f7f09b2c71abc201122e2d22b1b
feanar729 <feanar729@gmail.com>
2018-12-28 14:48:18 +0900

    refactoring: CSS show 코드 및 setCSS 메서드 refactoring

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,5 @@
+import { throttle, debounce } from './setThrottleDebounce.js';
+
 export default class Carousel {
   constructor(layer) {
     this.carousel = {
@@ -17,13 +19,10 @@ export default class Carousel {
     this.currentItem = 1;
 
     this.config = Carousel.mergeConfig(carousel);
-    this.infinite = true;
   }
 
   static mergeConfig(carouselConfig) {
     const defaultConfig = {
-      duration: 200,
-      easing: 'ease-out',
       infinite: true,
     };
 
@@ -34,14 +33,31 @@ export default class Carousel {
   }
 
   init() {
-    this.setCarouselStyle();
     this.moveController();
     this.attachEvent();
   }
 
   attachEvent() {
+    if (this.config.infinite) {
+      // this.carousel.olLayer.addEventListener("load", this.setRAF(this.carousel, this.itemWidth));
+    } else {
+
+    }
     this.carousel.prev.addEventListener("click", this.moveToPrev.bind(this));
     this.carousel.next.addEventListener("click", this.moveToNext.bind(this));
+  }
+
+  setRAF(carousel, itemWidth) {
+    let moveWidth = 0;
+    this.move();
+    function run() {
+      console.log(moveWidth);
+      carousel.olLayer.style.transform = `translate3D(${parseInt(0) + moveWidth}px, 0, 0)`;
+      console.log(carousel.olLayer.style.transform);
+      moveWidth = moveWidth - itemWidth;
+      setTimeout(() => requestAnimationFrame(run), 3000);
+    }
+    requestAnimationFrame(run);
   }
 
   moveController() {
@@ -54,20 +70,17 @@ export default class Carousel {
     }
   }
 
-  setCarouselStyle() {
-    this.carousel.showLayer.classList.add("show");
-  }
-
   isClone() {
     return this.currentItem === 0 || this.currentItem === this.itemLength + 1;
   }
 
   delayMoveWithoutAnimation() {
+    // TODO: [] 빠르게 연속적으로 click event 발생시 미세한 event Animation Time의 격차 발생 수정 필요. (debounce? throttle 기능으로 가능?)
     return setTimeout(() => this.moveWithoutAnimation(), 200);
+    // return debounce(200, this.moveWithoutAnimation()); // 문제있음..
   }
 
   move() {
-    this.isTransiting = true;
     this.carousel.olLayer.classList.add("active");
     this.carousel.olLayer.style.transform = `translate3D(${this.offset}px, 0, 0)`;
   }
@@ -81,14 +94,16 @@ export default class Carousel {
     this.offset += this.itemWidth;
     this.move();
     this.currentItem--;
-    (this.config.infinite) ? this.setPrevMove() : this.checkMovable();
+    // (this.config.infinite) ? this.setPrevMove() : this.checkMovable();
+    this.setPrevMove();
   }
 
   moveToNext() {
     this.offset -= this.itemWidth;
     this.move();
     this.currentItem++;
-    (this.config.infinite) ? this.setNextMove() : this.checkMovable();
+    // (this.config.infinite) ? this.setNextMove() : this.checkMovable();
+    this.setNextMove();
   }
 
   setPrevMove() {
@@ -115,8 +130,8 @@ export default class Carousel {
     this.carousel.olLayer.appendChild(firstItem.cloneNode(true));
   }
 
-  checkMovable() {
-    (this.currentItem === 1) ? this.carousel.prev.disabled = true : this.carousel.prev.disabled = false;
-    (this.currentItem === this.itemLength) ? this.carousel.next.disabled = true : this.carousel.next.disabled = false;
-  }
+  // checkMovable() {
+  //   (this.currentItem === 1) ? this.carousel.prev.disabled = true : this.carousel.prev.disabled = false;
+  //   (this.currentItem === this.itemLength) ? this.carousel.next.disabled = true : this.carousel.next.disabled = false;
+  // }
 }
