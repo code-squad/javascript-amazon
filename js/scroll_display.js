61c52bee79e976c9564a2db13be3cae847372570
cohily12 <k96079123@gmail.com>
2018-12-30 13:48:31 +0900

    [Modify]plan layer animation bug

--- js/scroll_display.js
+++ js/scroll_display.js
@@ -1,26 +1,30 @@
 const navLower = document.querySelector(".nav-lower");
 const primeButton = document.querySelector(".prime-header-content .btn-prime-container");
 const planLayer = document.querySelector(".nav-plan-layer");
-const hiddenContent = document.querySelector(".nav-hidden-plan");
+const hiddenPlan = document.querySelector(".nav-hidden-plan");
 const hiddenBar = document.querySelector(".nav-hidden-bar");
 const barCloseBtn = document.querySelector(".nav-hidden-close");
 let isHiddenBarShow = false;
 
+window.addEventListener("scroll", fixHeader);
 window.addEventListener("scroll", showHiddenBar);
 
-function showHiddenBar() {
+function fixHeader(){
     const offsetForPosition = navLower.offsetTop + navLower.offsetHeight;
-    const offsetForPlanLayer = primeButton.offsetTop + primeButton.offsetHeight;
     const isPassedNavLower = this.scrollY >= offsetForPosition;
-    const isPassedPrimeButton = this.scrollY >= offsetForPlanLayer;
-    
+
     if(isPassedNavLower) planLayer.classList.add("fixed");
     else planLayer.classList.remove("fixed");
+}
 
+function showHiddenBar() {
+    const offsetForPlanLayer = primeButton.offsetTop + primeButton.offsetHeight;
+    const isPassedPrimeButton = this.scrollY >= offsetForPlanLayer;
+    
     if(isPassedPrimeButton) {
         hiddenBar.classList.replace("hidden", "visible");
         setTimeout(() => {
-            if(isHiddenBarShow) hiddenBar.style.height = "4.2rem"
+            if(isHiddenBarShow) hiddenBar.style.height = "4.6rem"
         }, 0);
         isHiddenBarShow = true;
     } 
@@ -36,16 +40,25 @@ function showHiddenBar() {
 const seeMoreBtn = document.querySelector(".bar-see-more .down-arrow");
 
 seeMoreBtn.addEventListener("click", function(){
-    // window.removeEventListener("scroll", showHiddenBar);
-    hiddenContent.classList.replace("hidden", "visible");
+    window.removeEventListener("scroll", showHiddenBar);
+    hiddenPlan.classList.replace("hidden", "visible");
+    setTimeout(() => {
+        hiddenPlan.style.height = "42rem"
+    }, 0);
+    
     hiddenBar.classList.replace("visible", "hidden");
     barCloseBtn.classList.replace("hidden", "visible");
 
     const contentCloseBtn = document.querySelectorAll("#content-close-btn");
     contentCloseBtn.forEach(el => el.addEventListener("click", function(){
         window.addEventListener("scroll", showHiddenBar);
-        hiddenContent.classList.replace("visible", "hidden");
-        hiddenBar.classList.replace("hidden", "visible");
+        hiddenPlan.style.height = "0";
+        setTimeout(() => {
+            hiddenPlan.classList.replace("visible", "hidden")
+        }, 700);
+        console.log(isHiddenBarShow);
+        if(isHiddenBarShow) hiddenBar.classList.replace("hidden", "visible");
         barCloseBtn.classList.replace("visible", "hidden");
+
     }));
 })
