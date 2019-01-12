88c20163536a8fa4535168bcee4d0d53db3109ec
Boyoung KIM <boyoung@vanary.com>
2019-01-12 11:59:26 +0900

    Move template function into individual module

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import StickyNav from './stickyNav/StickyNav.js';
 import MegaMenu from './megaMenu/MegaMenu.js';
 import MiniCarousel from './miniCarousel/MiniCarousel.js';
 import SearchAutoFill from './searchAutoFill/SearchAutoFill.js';
+import template from './templates/templates.js';
 
 const stickyPlansLayer = new StickyNav({
   htmlEl: document.querySelector('.stickyNav'),
@@ -29,28 +30,13 @@ const miniCarouselOriginal = new MiniCarousel({
   timer: miniCarouselTiming,
 });
 
-function suggestionTemplateFn({ value, refTag }, searchWord) {
-  const replaceWhiteSpace = (string, replaceChar) => string.replace(/\s/g, replaceChar);
-  const ref = replaceWhiteSpace(refTag, '+');
-  const fieldKeyword = replaceWhiteSpace(value, '+');
-  const prefix = replaceWhiteSpace(searchWord, '+');
-
-  const leftoverStr = value.slice(searchWord.length);
-  const displayVal = `<span class="search__suggestion--matchStr">${searchWord}</span>`
-    + `<span class="search__suggestion--otherStr">${leftoverStr}</span>`;
-
-  return `
-  <li class="search__suggestionLi">
-    <a href="/search?ref=${ref}&field-keywords=${fieldKeyword}&prefix=${prefix}">${displayVal}</a>
-  </li>`;
-}
 const searchAutoFill = new SearchAutoFill({
   apiURI: 'https://codesquadacapi.herokuapp.com/ac/',
   el: {
     inputEl: document.querySelector('.search__input'),
     suggestionWrapperEl: document.querySelector('.search__suggestion'),
   },
-  suggestionTemplateFn,
+  suggestionTemplateFn: template.suggestion,
   awaitTiming: 500,
 });
 
---
+++ scripts/templates/templates.js
@@ -0,0 +1,17 @@
+export default (template = {
+  suggestion({ value, refTag }, searchWord) {
+    const replaceWhiteSpace = (string, replaceChar) => string.replace(/\s/g, replaceChar);
+    const ref = replaceWhiteSpace(refTag, '+');
+    const fieldKeyword = replaceWhiteSpace(value, '+');
+    const prefix = replaceWhiteSpace(searchWord, '+');
+
+    const leftoverStr = value.slice(searchWord.length);
+    const displayVal = `<span class="search__suggestion--matchStr">${searchWord}</span>`
+      + `<span class="search__suggestion--otherStr">${leftoverStr}</span>`;
+
+    return `
+    <li class="search__suggestionLi">
+      <a href="/search?ref=${ref}&field-keywords=${fieldKeyword}&prefix=${prefix}">${displayVal}</a>
+    </li>`;
+  },
+});
