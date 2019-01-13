8cf806533962ae6cca7bac63f57d0314b5305293
cohily12 <k96079123@gmail.com>
2019-01-14 00:57:06 +0900

    [Modify]carousel arguments

--- js/app.js
+++ js/app.js
@@ -3,19 +3,19 @@ import { Carousel } from "./module/Carousel.js";
 
 const stickyLayer = new StickyLayer({ stickyEl: document.querySelector(".nav-plan-layer") });
 const videoCarousel = new Carousel({
-    targetHTML: document.querySelector(".video-carousel"),  
+    targetHTML: document.querySelector(".video-carousel"), 
     intervalTime: 3000,
     delayTime: 5000
 });
 const musicCarousel = new Carousel({
-    targetHTML: document.querySelector(".music-carousel"),  
+    targetHTML: document.querySelector(".music-carousel"), 
     intervalTime: 3000,
-    delayTime: 5000 
+    delayTime: 5000
 })
+
 
 window.addEventListener("load", () => {
     stickyLayer.run();
     videoCarousel.run();
     musicCarousel.run();
 });
-
