8a127233af39d3fa1f08b01ce1dd7c888653bef3
Head <mrdoo309@gmail.com>
2019-01-17 17:26:49 +0900

    자바스크립트 완성

---
+++ amazonJS/contentLoader.js
@@ -0,0 +1,20 @@
+import { TopLayerEvent } from "./topLayer.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const headerHeight = window.getComputedStyle(document.querySelector('header')).height;
+  const topLayerEvent = new TopLayerEvent(
+    parseInt(headerHeight),
+    document.querySelector(".top-layer-container"),
+    document.querySelector(".prime-member-container")
+  );
+  document.addEventListener(
+    "scroll",
+    topLayerEvent.topLayerDrop.bind(topLayerEvent)
+  );
+  const closeBtn = document.querySelector(".close-button");
+  const closeArrowBtn = document.querySelector(".close-button-foot");
+  const openArrowBtn = document.querySelector(".top-layer-trigger-button");
+  closeBtn.addEventListener("click", topLayerEvent.removePrimeMember.bind(topLayerEvent));
+  closeArrowBtn.addEventListener("click", topLayerEvent.removePrimeMember.bind(topLayerEvent));
+  openArrowBtn.addEventListener("click", topLayerEvent.addPrimeMember.bind(topLayerEvent));
+});
