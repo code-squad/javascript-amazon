33498d3d7176be4fc675ce058f960ae908601ac3
Head <mrdoo309@gmail.com>
2019-01-24 12:04:06 +0900

    쓸데없는 파일 제거 sticky쪽 스크롤 이벤트 수정
    인터섹션옵저버를 사용하여 스크롤 이벤트 구현

--- amazonJS/app.js
+++ amazonJS/app.js
@@ -1,9 +1,8 @@
 import { ScrollEvent_sticky } from "./scrollEvent_sticky.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const headerHeight = window.getComputedStyle(document.querySelector("header")).height;
   new ScrollEvent_sticky(
-    parseInt(headerHeight),
+    document.querySelector("header"),
     document.querySelector(".top-layer-container"),
     document.querySelector(".prime-member-container")
   );
--- amazonJS/scrollEvent_Sticky.js
+++ amazonJS/scrollEvent_Sticky.js
@@ -1,41 +1,32 @@
 class ScrollEvent_sticky {
   constructor(dropBenchMark, stickyLayer, hiddenLayer) {
-    this.dropBenchMark = dropBenchMark;
     this.stickyLayer = stickyLayer;
     this.hiddenLayer = hiddenLayer;
     this.init();
+    this.makeIO(dropBenchMark);
   }
-  addIO() {
-    const io = new IntersectionObserver ((entries) => {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > this.dropBenchMark) {
-          entry.target.classList.add('tada');
-        }
-        // 그 외의 경우 'tada' 클래스 제거
-        else {
-          entry.target.classList.remove('tada');
-        }
-      })
+
+  makeIO(root) {
+    const io = new IntersectionObserver(entries => {
+      if (!entries[0].isIntersecting) {
+        this.stickyLayer.classList.add("top-layer-active");
+        this.hiddenLayer.classList.add("prime-member-container-scroll-active");
+      } else {
+        this.stickyLayer.classList.remove("top-layer-active");
+        this.hiddenLayer.classList.remove("prime-member-container-scroll-active");
+      }
+    });
+    return io.observe(root);
   }
+
   init() {
-    document.addEventListener("scroll", this.stickyLayerDrop.bind(this));
     const closeBtn = document.querySelector(".close-button");
     const closeArrowBtn = document.querySelector(".close-button-foot");
     const openArrowBtn = document.querySelector(".top-layer-trigger-button");
-    
+
     closeBtn.addEventListener("click", this.hideLayer.bind(this));
     closeArrowBtn.addEventListener("click", this.hideLayer.bind(this));
     openArrowBtn.addEventListener("click", this.showHiddenLayer.bind(this));
-  }
-
-  stickyLayerDrop() {
-    if (window.scrollY >= this.dropBenchMark) {
-      this.stickyLayer.classList.add("top-layer-active");
-      this.hiddenLayer.classList.add("prime-member-container-scroll-active");
-    } else {
-      this.stickyLayer.classList.remove("top-layer-active");
-      this.hiddenLayer.classList.remove("prime-member-container-scroll-active");
-    }
   }
 
   showHiddenLayer() {
