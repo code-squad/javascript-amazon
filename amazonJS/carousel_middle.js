f46dbb352e18a5678463e0ea8f54050d9eed5920
Head <mrdoo309@gmail.com>
2019-01-26 22:20:43 +0900

    오른쪽으로 움직이는 부분 완성

--- amazonJS/carousel_middle.js
+++ amazonJS/carousel_middle.js
@@ -5,6 +5,7 @@ class Carousel_middle {
     this.container = container;
     this.right = right;
     this.left = left;
+    this.parsedArr = [];
     this.init();
   }
 
@@ -12,41 +13,33 @@ class Carousel_middle {
     this.module.getAjax(this.handler.bind(this), `./jsonData/data.json`);
   }
 
-  partDivider() {
-    const playBool = false;
-    const container = this.module.qs(".middle-body-carousel-list");
-    const right = this.module.qs(".middle-body-carousel-right-button");
-    const left = this.module.qs(".middle-body-carousel-left-button");
-    return [playBool, container, right, left];
-  }
-
   handler(parsedObj) {
     //롸잇, 레프트
-    const partArr = this.partDivider();
-    this.parsedObj = parsedObj;
-    partArr[2].addEventListener("click", this.moveRight);
-    partArr[3].addEventListener("click", this.moveLeft);
+    const backgroundUrlArr = parsedObj.backgroundUrl
+    for(let i = 0; i < backgroundUrlArr.length; i++) {
+      this.parsedArr.push(parsedObj['backgroundUrl'][i]);
+    }
+    this.right.addEventListener("click", this.moveRight.bind(this));
+    this.left.addEventListener("click", this.moveLeft.bind(this));
   }
 
   moveRight() {
     //오른쪽으로 움직이는 부분
-    const partArr = this.partDivider().bind(this);
-    console.log("hi@");
-    if (partArr[0]) return;
-    partArr[0] = true;
-    this.parsedObj.backgroundUrl.unshift(parsedObj.backgroundUrl.pop());
+    if (this.playBool) return;
+    this.playBool = true;
+    this.parsedArr.unshift(this.parsedArr.pop());
     //shifting
-    partArr[1].style.transform = "translateX(230px)";
-    partArr[1].addEventListener("transitionend", () => {
-      this.parsedObj.backgroundUrl.forEach((v, i) => {
+    this.container.style.transform = "translateX(230px)";
+    this.container.addEventListener("transitionend", () => {
+      this.parsedArr.forEach((v, i) => {
         const part = this.module.qs(`.index${i}`);
         part.style = `background-image:url(${v})`;
       });
-      partArr[1].style.transition = "all 0s";
-      partArr[1].style.transform = "translateX(0px)";
-      partArr[0] = false;
+      this.container.style.transition = "all 0s";
+      this.container.style.transform = "translateX(0px)";
+      this.playBool = false;
     });
-    partArr[1].style.transition = "all 0.5s";
+    this.container.style.transition = "all 0.5s";
   }
 
   moveLeft(container) {
