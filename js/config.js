48f918213e54728002b8324b24f77a8e210872d0
cohily12 <k96079123@gmail.com>
2019-01-28 14:55:24 +0900

    [Modify]acapi url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const URL = {
     SERVER: location.href,
-    ACAPI: "http://crong.codesquad.kr:8080/amazon/ac/",
-    ITEM: "http://crong.codesquad.kr:8080/amazon-search?"
+    ACAPI: "https://codesquadacapi.herokuapp.com/ac/",
+    ITEM: "https://crong.codesquad.kr:8080/amazon-search?"
 }
