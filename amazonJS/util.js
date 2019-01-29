2e07d49249d4e8ed8b3602ce7163aa7cbfabb2d0
Head <mrdoo309@gmail.com>
2019-01-29 15:51:00 +0900

    util파일 변경
    객체를 export하는게 아닌 함수들을 각자 export해서 파일마다 쓰이는 기능만 다로 export하도록
    작성

--- amazonJS/util.js
+++ amazonJS/util.js
@@ -1,19 +1,13 @@
-class Module {
-  getAjax(handler, url) {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", () => {
-      const parsedObject = JSON.parse(xhr.responseText);
-      handler(parsedObject);
-    });
-    xhr.open(`GET`, `${url}`);
-    xhr.send();
-  }
-
-  
+export function getAjax(handler, url) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => {
+    const parsedObject = JSON.parse(xhr.responseText);
+    handler(parsedObject);
+  });
+  xhr.open(`GET`, `${url}`);
+  xhr.send();
 }
 
 export function qs(value) {
   return document.querySelector(value);
 }
-
-// export { Module };
