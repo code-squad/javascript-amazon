ebc1e467a239eecd3fb5bf0524c8a4feffc2f453
kakafe <sk2504@naver.com>
2019-01-30 11:56:04 +0900

    planpage class명 수정 및 nav.js의 변수명을 수정
    가독성 향상을 위해 planpage를 plan으로 변경, Nav_Elements 를 NavElements로 수정함

--- JS/nav.js
+++ JS/nav.js
@@ -1,11 +1,11 @@
-class Nav_Elements {
+class NavElements {
   constructor() {
-    this.planPage = document.querySelector(".planpage");
-    this.morePage = document.querySelector(".planpage-more-contents");
-    this.planPageCnt = document.querySelector(".planpage-contents");
+    this.plan = document.querySelector(".plan");
+    this.morePage = document.querySelector(".plan-more-contents");
+    this.planCnt = document.querySelector(".plan-contents");
     this.moreCntBtn = document.querySelector(".more-contents-closebtn");
     this.moreCntArrow = document.querySelector(".more-contents-arrow-img");
-    this.morePlanBtn = document.querySelector(".planpage-contents-txt");
+    this.morePlanBtn = document.querySelector(".plan-contents-txt");
     this.navPosition = 112;
     this.primBtnPosition = 465;
   }
@@ -13,9 +13,9 @@ class Nav_Elements {
     el.classList.remove(className);
   }
   removeClass() {
-    this.removeClassList(this.planPage, "planpage-more-click");
-    this.removeClassList(this.morePage, "planpage-more-click");
-    this.removeClassList(this.planPageCnt, "planpage-contents-downsize");
+    this.removeClassList(this.plan, "plan-more-click");
+    this.removeClassList(this.morePage, "plan-more-click");
+    this.removeClassList(this.planCnt, "plan-contents-downsize");
   }
   addMorePlanDownSizeEvent() {
     this.moreCntBtn.addEventListener("click", this.removeClass.bind(this));
@@ -25,23 +25,23 @@ class Nav_Elements {
     window.addEventListener("scroll", this.scrollHandler.bind(this));
   }
   scrollHandler() {
-    const planClassList = this.planPage.classList;
-    if (pageYOffset > this.navPosition) planClassList.add("planpage-scroll-middle");
-    else planClassList.remove("planpage-scroll-middle");
-    if (pageYOffset > this.primBtnPosition) planClassList.add("planpage-scroll-bottom");
-    else planClassList.remove("planpage-scroll-bottom");
+    const planClassList = this.plan.classList;
+    if (pageYOffset > this.navPosition) planClassList.add("plan-scroll-middle");
+    else planClassList.remove("plan-scroll-middle");
+    if (pageYOffset > this.primBtnPosition) planClassList.add("plan-scroll-bottom");
+    else planClassList.remove("plan-scroll-bottom");
   }
   addMoreClickEvent() {
     this.morePlanBtn.addEventListener("click", this.addClass.bind(this));
   }
   addClass() {
-    this.planPage.classList.add("planpage-more-click");
-    this.morePage.classList.add("planpage-more-click");
-    this.planPageCnt.classList.add("planpage-contents-downsize");
+    this.plan.classList.add("plan-more-click");
+    this.morePage.classList.add("plan-more-click");
+    this.planCnt.classList.add("plan-contents-downsize");
   }
 }
 
-const NavElements = new Nav_Elements();
-NavElements.addScrollEvent();
-NavElements.addMoreClickEvent();
-NavElements.addMorePlanDownSizeEvent();
+const navElement = new NavElements();
+navElement.addScrollEvent();
+navElement.addMoreClickEvent();
+navElement.addMorePlanDownSizeEvent();
