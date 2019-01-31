c6c2b91d94073231b3e4c514eaf3178668e1d7af
developersoom <developer.soom@gmail.com>
2019-01-31 15:44:07 +0900

    Add extender transition when near header

--- layer.js
+++ layer.js
@@ -6,14 +6,30 @@ window.addEventListener("scroll", function (e) {
 });
 
 const seeMoreBtn = document.querySelector('.plans-see-more-txt');
-const plansPopUp = document.querySelector('.plans-pop-up');
-const closePopUpBtn = document.querySelector('.pop-up-btn');
+const plansExtender = document.querySelector('.plans-extender');
+const closeExtenderBtn = document.querySelector('.plans-extender-btn');
 const header = document.querySelector('.header')
 
-seeMoreBtn.addEventListener("click", function () {
-    plansPopUp.classList.add('shown');
+document.addEventListener("scroll", function () {
+    const plansExtenderShown = document.querySelector('.plans-extender.shown');
+    if (window.scrollY < 99 && plansExtenderShown) {
+        plansExtender.style.position = 'absolute';
+        plansExtender.style.top = '6.5rem';
+        header.style.position = 'relative';
+        plansExtenderShown.style.transition = "none";
+    }
+    else if (plansExtenderShown) {
+        plansExtender.style.position = 'fixed';
+        plansExtender.style.top = '0';
+        header.style.position = '';
+        plansExtenderShown.style.transition = "all 0.5s ease-in-out;";
+    }
 });
 
-closePopUpBtn.addEventListener("click", function (e) {
-    plansPopUp.classList.remove('shown');
+seeMoreBtn.addEventListener("click", function () {
+    plansExtender.classList.add('shown');
+});
+
+closeExtenderBtn.addEventListener("click", function (e) {
+    plansExtender.classList.remove('shown');
 })
