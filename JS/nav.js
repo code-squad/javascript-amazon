257d5cf3249b2ae77359b051f22e66e9758687e0
KaKa <sk2504@naver.com>
2019-01-31 09:47:22 +0900

    Step3 PR (#34)
    * menulist의 오타 수정
    
    * main.css 리팩토링
    
    중복된 값들을 묶어 가독성을 높임, nav-top부분에 wrap 을 만들어, page크기가 달라졌을때,
    가운데 정렬되도록 수정
    
    * step3 최종기능 구현
    
    스크롤 했을때, plan page 가 펼처지고, 그안에서의 인터렉션 구현 CSS파일을 section별로 구분,
    nav.js파일생성 및 기능구현
    
    *  planpage class명 수정 및 nav.js의 변수명을 수정
    
    가독성 향상을 위해 planpage를 plan으로 변경, Nav_Elements 를 NavElements로 수정함
    
    * navElements 의 프로퍼티를 각메서드의 지역변수로 옮긴다
    
    2개이상의 메서드에서 사용되는 프로퍼티를 제외하고 나머지를 addMoreClickEvent 메서드와
    scrollHandler,addMorePlanDownSizeEvent메서드에 지역변수로 넣는다
    
    *  classMap 객체를 생성
    
    classMap 객체를 생성하여, 변수명을 classMap 객체를통해 맵핑하는 방식으로 변경한다
    
    * initInteraction 메서드를 생성
    
    기존 3개의 메서드를 실행시켜 인터렉션을 작동하는데, initInteraction메서드로 묶어 한번에 실행

---
+++ JS/nav.js
@@ -0,0 +1,58 @@
+class NavElements {
+  constructor() {
+    this.plan = document.querySelector(".plan");
+    this.morePage = document.querySelector(".plan-more-contents");
+    this.planCnt = document.querySelector(".plan-contents");
+    this.navPosition = 112;
+    this.primBtnPosition = 465;
+  }
+  removeClassList(el, className) {
+    el.classList.remove(className);
+  }
+  removeClass() {
+    this.removeClassList(this.plan, classMap.click);
+    this.removeClassList(this.morePage, classMap.click);
+    this.removeClassList(this.planCnt, classMap.donwSize);
+  }
+  addMorePlanDownSizeEvent() {
+    const moreCntBtn = document.querySelector(".more-contents-closebtn");
+    const moreCntArrow = document.querySelector(".more-contents-arrow-img");
+    moreCntBtn.addEventListener("click", this.removeClass.bind(this));
+    moreCntArrow.addEventListener("click", this.removeClass.bind(this));
+  }
+  addScrollEvent() {
+    window.addEventListener("scroll", this.scrollHandler.bind(this));
+  }
+  scrollHandler() {
+    const planClassList = this.plan.classList;
+    if (pageYOffset > this.navPosition) planClassList.add(classMap.middleIntrc);
+    else planClassList.remove(classMap.middleIntrc);
+    if (pageYOffset > this.primBtnPosition)
+      planClassList.add(classMap.bottomIntrc);
+    else planClassList.remove(classMap.bottomIntrc);
+  }
+  addMoreClickEvent() {
+    const morePlanBtn = document.querySelector(".plan-contents-txt");
+    morePlanBtn.addEventListener("click", this.addClass.bind(this));
+  }
+  addClass() {
+    this.plan.classList.add(classMap.click);
+    this.morePage.classList.add(classMap.click);
+    this.planCnt.classList.add(classMap.donwSize);
+  }
+  initInteraction() {
+    this.addScrollEvent();
+    this.addMoreClickEvent();
+    this.addMorePlanDownSizeEvent();
+  }
+}
+
+const classMap = {
+  middleIntrc: "plan-scroll-middle",
+  bottomIntrc: "plan-scroll-bottom",
+  click: "plan-more-click",
+  donwSize: "plan-contents-downsize"
+};
+
+const navElement = new NavElements();
+navElement.initInteraction();
