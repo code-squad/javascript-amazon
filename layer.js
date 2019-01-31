efbdc960ff4118ffe6d7dff6d07e192acf6ddbd3
developersoom <developer.soom@gmail.com>
2019-02-01 03:29:13 +0900

    Add class PlansUI and edit accordingly

--- layer.js
+++ layer.js
@@ -1,37 +1,64 @@
-const plansLayer = document.querySelector('.plans-layer');
-const plansExtender = document.querySelector('.plans-extender');
-const plansMoreBtn = document.querySelector('.plans-see-more');
-const closeExtenderBtn = document.querySelector('.plans-extender-btn');
-let isExtenderShown = false;
-
-document.addEventListener("scroll", function () {
-    console.log(window.scrollY)
-    if (isExtenderShown) {
-        if(window.scrollY < 99) plansExtender.classList.add('top');
-        else plansExtender.classList.remove('top');
+class PlansUI {
+    constructor() {
+        this.isExtenderShown = false;
+        this.closeExtenderBtn = document.querySelector('.plans-extender-btn');
+        this.plansMoreBtn = document.querySelector('.plans-see-more');
     }
-    else if (!isExtenderShown){
+
+    showLayer() {
+        this.isExtenderShow ? plansUI.checkExtender() : plansUI.showPlansLayer();
+    };
+
+    showPlansLayer() {
+        const plansLayer = document.querySelector('.plans-layer');
         if (window.scrollY > 366) plansLayer.classList.add('shown');
         else plansLayer.classList.remove('shown');
-    }
-});
 
-plansMoreBtn.addEventListener("click", function (){
-    plansLayer.classList.remove('shown');
-    plansExtender.classList.add('shown');
-    isExtenderShown = true;
-})
+    };
 
-closeExtenderBtn.addEventListener("click", function(){
-    if (window.scrollY > 366) {
-        plansExtender.classList.remove('shown');
-        plansLayer.classList.add('shown');
-        plansExtender.classList.remove('top');
-        isExtenderShown = false;
-    }
-    else {
-        plansExtender.classList.remove('shown');
-        plansExtender.classList.remove('top');
-        isExtenderShown = false;
-    }
-})
+    checkExtender() {
+        const header = document.querySelector('.header');
+        const plansExtender = document.querySelector('.plans-extender');
+
+        if (window.scrollY < 99) {
+            plansExtender.classList.add('top');
+            header.classList.add('extended');
+        } else {
+            plansExtender.classList.remove('top');
+            header.classList.remove('extended');
+        }
+    };
+
+    showExtender(){
+        const plansLayer = document.querySelector('.plans-layer');
+        const plansExtender = document.querySelector('.plans-extender');
+
+        plansLayer.classList.remove('shown');
+        plansExtender.classList.add('shown');
+        this.isExtenderShown = true;
+    };
+    
+    closeExtender(){
+        const plansExtender = document.querySelector('.plans-extender');
+        const plansLayer = document.querySelector('.plans-layer');
+
+        if (window.scrollY > 366) {
+            plansExtender.classList.remove('shown');
+            plansLayer.classList.add('shown');
+            plansExtender.classList.remove('top');
+            isExtenderShown = false;
+        }
+        else {
+            plansExtender.classList.remove('shown');
+            plansExtender.classList.remove('top');
+            this.isExtenderShown = false;
+        }
+    }   
+}
+
+const plansUI = new PlansUI();
+
+document.addEventListener("scroll", plansUI.showLayer);
+document.addEventListener("scroll", plansUI.checkExtender);
+plansUI.closeExtenderBtn.addEventListener("click", plansUI.closeExtender);
+plansUI.plansMoreBtn.addEventListener("click", plansUI.showExtender);
