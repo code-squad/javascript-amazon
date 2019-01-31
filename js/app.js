e09ca61cc5351d28d28a23325c56e5c2667fbf72
cohily12 <k96079123@gmail.com>
2019-01-31 15:59:34 +0900

    [Modify]main css for dim

--- js/app.js
+++ js/app.js
@@ -2,19 +2,24 @@ import { StickyLayer } from "./module/StickyLayer.js"
 import { Carousel } from "./module/Carousel.js";
 import { Autocomplete } from "./module/Autocomplete.js"
 
-const stickyLayer = new StickyLayer(".plan-layer");
-const videoCarousel = new Carousel(".video-carousel", {
-    intervalTime: 3000,
-    delayTime: 5000,
-    itemUrl: "/videoCarousel.json"
-});
-const musicCarousel = new Carousel(".music-carousel", {
-    intervalTime: 3000,
-    delayTime: 5000,
-    itemUrl: "/musicCarousel.json"
-});
-const autocomplete = new Autocomplete(".nav-input-bar", {
-    keywordsContainer: ".nav-search-autocomplete",
-    acTime: 500,
-    bDimmer: true
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const stickyLayer = new StickyLayer(".plan-layer");
+
+    const videoCarousel = new Carousel(".video-carousel", {
+        intervalTime: 3000,
+        delayTime: 5000,
+        itemUrl: "/videoCarousel.json"
+    });
+
+    const musicCarousel = new Carousel(".music-carousel", {
+        intervalTime: 3000,
+        delayTime: 5000,
+        itemUrl: "/musicCarousel.json"
+    });
+
+    const autocomplete = new Autocomplete(".nav-input-bar", {
+        keywordsContainer: ".nav-search-autocomplete",
+        acTime: 500,
+        bDimmer: true
+    });
+})
