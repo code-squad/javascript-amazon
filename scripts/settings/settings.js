bb40e8c1473bde9e1a74118dc50c704b3f16a27b
Boyoung KIM <boyoung@vanary.com>
2019-02-26 14:23:53 +0900

    Setting(searchAPI): Update API URI & debouce delay

--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -1,42 +1,42 @@
-import template from '../templates/templates.js';
+import template from "../templates/templates.js";
 
 const settings = {
   stickyPlansLayer: {
-    htmlEl: document.querySelector('.stickyNav'),
+    htmlEl: document.querySelector(".stickyNav"),
     threshold:
-      document.querySelector('.masthead').clientHeight
-      + document.querySelector('.header').clientHeight,
+      document.querySelector(".masthead").clientHeight +
+      document.querySelector(".header").clientHeight
   },
   megaMenu: {
-    htmlEl: document.querySelector('.megaMenu'),
-    triggerEl: document.querySelector('.megaMenu__trigger'),
-    canvasEl: document.querySelector('.megaMenu__trackerCanvas'),
+    htmlEl: document.querySelector(".megaMenu"),
+    triggerEl: document.querySelector(".megaMenu__trigger"),
+    canvasEl: document.querySelector(".megaMenu__trackerCanvas")
   },
   miniCarousel: {
     music: {
-      htmlElSelector: '.horizontalBanners__prime-music',
+      htmlElSelector: ".horizontalBanners__prime-music",
       timer: {
         autoRotationTiming: 3000, // ms
-        autoRotationDebounce: 5000, // ms
-      },
+        autoRotationDebounce: 5000 // ms
+      }
     },
     original: {
-      htmlElSelector: '.horizontalBanners__prime-original',
+      htmlElSelector: ".horizontalBanners__prime-original",
       timer: {
         autoRotationTiming: 3000, // ms
-        autoRotationDebounce: 5000, // ms
-      },
-    },
+        autoRotationDebounce: 5000 // ms
+      }
+    }
   },
   searchAutoFill: {
-    apiURI: 'https://codesquadacapi.herokuapp.com/ac/',
+    apiURI: "https://codesquadapi.herokuapp.com/ac/",
     el: {
-      inputEl: document.querySelector('.search__input'),
-      suggestionWrapperEl: document.querySelector('.search__suggestion'),
+      inputEl: document.querySelector(".search__input"),
+      suggestionWrapperEl: document.querySelector(".search__suggestion")
     },
     suggestionTemplateFn: template.suggestion,
-    awaitTiming: 500,
-  },
+    awaitTiming: 300
+  }
 };
 
 export default settings;
