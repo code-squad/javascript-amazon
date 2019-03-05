861ac4f7e7ebdd079921dfeadd4a21811fc5b71f
developersoom <developer.soom@gmail.com>
2019-03-05 16:43:24 +0900

    Add functions to count seconds

--- autoComplete.js
+++ autoComplete.js
@@ -1,42 +1,62 @@
 export default class AutoComplete {
     constructor() {
         this.currentItem = 0;
+        this.inputTime = 0;
     }
 
     init() {
         this.addEvent();
     }
 
+    addEvent() {
+        const schInput = document.querySelector(".sch-input");
+        const searchedResult = document.querySelector(".searched-result");
+        const searchBtn = document.querySelector(".sch-btn");
+        const url = "http://crong.codesquad.kr:8080/amazon/ac/";
+        let value;
+
+        searchBtn.addEventListener("click", (e) => e.preventDefault);
+        schInput.addEventListener("input", () => {
+            value = schInput.value;
+            this.inputTime = new Date();
+        });
+        schInput.addEventListener("keydown", (e) => this.checkKeyCode(e, schInput, searchedResult));
+        schInput.addEventListener("keyup", () => setTimeout(() => this.showResult(url, value, searchedResult), 1000));
+    }
+
+    isInputOneSecondAgo() {
+        return new Date() - this.inputTime > 1000;
+    }
+
+    showResult(url, value, searchedResult) {
+        if(this.isInputOneSecondAgo()) this.getData(url, value, searchedResult);
+    }
+
     getData(url, value, searchedResult) {
         fetch(url + value)
             .then(response => response.json())
             .then(data => {
-                let arr = data.suggestions;
-                let temp = "";
-                arr.forEach(v => temp += `<li class="searched-item">${v.value}</li>`);
-                searchedResult.innerHTML = temp;
-                searchedResult.style.display = 'block';
+                const dataList = data.suggestions;
+                const prefix = data.prefix;                
+                searchedResult.innerHTML = this.template(dataList, prefix);
             })
             .catch(err => console.log(err));
     }
 
-    addEvent() {
-        const inputWrapper = document.querySelector(".sch-input-wrapper");
-        const schInput = document.querySelector(".sch-input");
-        const searchedResult = document.querySelector(".searched-result");
-        const url = "http://crong.codesquad.kr:8080/amazon/ac/"
-        inputWrapper.addEventListener("click", (e) => e.preventDefault);
-        schInput.addEventListener("input", () => {
-            const value = schInput.value;
-            this.getData(url, value, searchedResult);
-        });
-        schInput.addEventListener("keydown", (e) => this.checkKeyCode(e, schInput, searchedResult));
+    template(dataList, prefix) {
+        const url = "http://crong.codesquad.kr:8080/amazon-search"
+        return dataList.map(v => { 
+            const itemName = v.value;
+            v.value = v.value.replace(/\s/g, "+");
+            return `<li class="searched-item"><a href="${url}?ref=${v.refTag}&field-keywords=${v.value}&prefix=${prefix}">
+            ${itemName}
+            </a></li>`
+        }).join('');
     }
 
     checkKeyCode(e, schInput, searchedResult) {
         const searchedItems = document.querySelectorAll(".searched-item");
         const itemLength = searchedItems.length;
-        console.log(this.currentItem)
         if (e.keyCode === 40 && this.currentItem === itemLength) {
             this.removeSelected(searchedItems);
             this.currentItem = 0;
@@ -52,6 +72,7 @@ export default class AutoComplete {
             searchedItems[this.currentItem-1].classList.add("selected");
             this.currentItem--;
         } else if (e.keyCode === 13) {
+            e.preventDefault();
             schInput.value = searchedItems[this.currentItem - 1].innerText;
             this.removeSelected(searchedItems);
             searchedResult.style.display = 'none';
