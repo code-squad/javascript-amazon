27560b7c91e932e886f3b1c14f0c8aa26c4e755d
HTMLhead <mrdoo309@gmail.com>
2019-03-06 18:21:32 +0900

    Feat: 서버의 분기점이 되는 app.js 제작

--- amazonDataServer/app.js
+++ amazonDataServer/app.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const router = require('./route/dataRoute');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+app.use(router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const dataRouter = require('./route/dataRoute');
+
+app.use('/*******', dataRouter);
+
+app.listen(8000, () => {
+    console.log("Start server on port 8000!!");
+});
