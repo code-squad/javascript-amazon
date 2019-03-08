4d43a09e508bf869ee5445b46f24d37acf450ddf
juchanhwang <ghkdwncks96@gmail.com>
2019-03-08 16:30:12 +0900

    검색 자동완성 노출 검색어 자동완성을 노출하는데까지 구현

--- searchUI_script/Controller/controller.js
+++ searchUI_script/Controller/controller.js
@@ -12,8 +12,13 @@ export default class Controller {
     const inputVal = event.target.value;
     const responseUrl = `https://completion.amazon.com/api/2017/suggestions?session-id=146-2216035-3218645&customer-id=&request-id=DV2W3G68C9YMG9FR19CF&page-type=PrimeLandingPageHorizonte&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=73&prefix=${inputVal}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD&_=1551712792496`;
     this.model.fetchData(responseUrl)
-      .then(suggestionsArr => {
-        this.view.isSuggestion(suggestionsArr);
+      .then(suggestionArr => {
+        this.showSuggestionVal(suggestionArr);
       });
+  }
+
+  showSuggestionVal(suggestionArr) {
+    const suggestionValArr = this.model.getSuggestionVal(suggestionArr)
+    this.view.isSuggestion(suggestionValArr);
   }
 }
--- searchUI_script/Model/model.js
+++ searchUI_script/Model/model.js
@@ -6,11 +6,18 @@ export default class Model {
     const fetchVal = fetch(responseUrl)
       .then(res => res.json())
       .then(data => {
-        return this.suggestionArr = data.suggestions.map(suggestion => {
-          return suggestion.value;
-        });
+        return data.suggestions
       })
     return fetchVal;
+  }
+
+
+  getSuggestionVal(suggestionArr) {
+    const suggestionValArr = suggestionArr.map(suggestion => {
+      return suggestion.value;
+    });
+    return suggestionValArr;
+
   }
 
 
