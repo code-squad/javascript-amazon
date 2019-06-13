f613e48d3686e4ab3552b0548dd9c1a3be331ac1
fed-gren <deveb22@gmail.com>
2019-06-13 15:56:22 +0900

    feat: move card slider

--- carousel.js
+++ carousel.js
@@ -6,14 +6,50 @@ class Carousel {
     this.cardWrapper = this.container.querySelector(".card-wrapper");
     this.cardSlider = this.container.querySelector(".card-slider");
     this.card = this.cardSlider.querySelector(".card");
-    this.prevButton = this.cardWrapper.querySelector(".prev");
-    this.nextButton = this.cardWrapper.querySelector(".next");
+    this.prevButton = this.container.querySelector(".prev");
+    this.nextButton = this.container.querySelector(".next");
     this.itemWidth = this.card.offsetWidth;
+    this.offset = 0;
+    this.currentItem = 1;
+    this.itemLength = this.cardSlider.querySelectorAll(".card").length;
   }
 
   init() {
     this.cardWrapper.style.width = `${this.itemWidth}px`;
+    this.attatchEvent();
+    this.isMovable();
     this.container.style.opacity = 1;
+  }
+
+  attatchEvent() {
+    this.prevButton.addEventListener("click", this.moveToPrev.bind(this));
+    this.nextButton.addEventListener("click", this.moveToNext.bind(this));
+  }
+
+  moveToPrev() {
+    this.offset += this.itemWidth;
+    this.currentItem -= 1;
+    this.isMovable();
+    this.move();
+  }
+
+  moveToNext() {
+    this.offset -= this.itemWidth;
+    this.currentItem += 1;
+    this.isMovable();
+    this.move();
+  }
+
+  isMovable() {
+    this.prevButton.disabled = this.currentItem === 1 ? true : false;
+    this.nextButton.disabled =
+      this.currentItem === this.itemLength ? true : false;
+  }
+
+  move() {
+    this.cardSlider.style.transform = `translateX(${this.offset}px)`;
+
+    // debugger;
   }
 }
 
