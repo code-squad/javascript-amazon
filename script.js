061109136ab06330569d071bdc36b08bd7e3e42a
leehwarang <hwrng2@gmail.com>
2019-06-14 14:09:56 +0900

    feat 캐러셀 무한루프 작동을 위한 로직 추가
    - 각 캐러셀 아이템을 인덱스로 선택할 수 있도록 함
    - 첫 번째와 마지막 캐러셀 아이템을 클론하여 처음과 끝에 추가

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ let carouselFullWidth = carouselWidth * carouselCnt;
 carousel.style.width = `${carouselFullWidth}px`;
 carousel.classList.add("flex");
 
+for (let i = 0; i < carouselCnt; i += 1) {
+  carousel.children[i].setAttribute("data-index", i + 1);
+}
+
+let firstItem = carousel.children[0];
+let lastItem = carousel.children[carouselCnt - 1];
+
+let firstCloneItem = firstItem.cloneNode(true);
+let lastCloneItem = lastItem.cloneNode(true);
+
+carousel.prepend(lastCloneItem);
+carousel.appendChild(firstCloneItem);
+
+firstCloneItem.classList.add("clone");
+lastCloneItem.classList.add("clone");
+
+carouselCnt = carousel.children.length;
+carouselFullWidth = carouselWidth * carouselCnt;
+carousel.style.width = `${carouselFullWidth}px`;
+
+carousel.style.left = `-${carouselWidth}px`;
+
 let prevBtn = document.querySelector(".btn-prev");
 let nextBtn = document.querySelector(".btn-next");
 
