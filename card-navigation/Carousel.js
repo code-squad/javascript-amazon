ebdbeb7237c46a87fa34f6be6d784335cc223efb
papico <ocipap0531@gmail.com>
2019-06-14 10:38:32 +0900

    feat: 무한 슬라이드 로직 구현 & transition 중 이벤트 blocking
    - isCloneItem
    - cover에 transitionend 이벤트 추가

--- card-navigation/Carousel.js
+++ card-navigation/Carousel.js
@@ -1,15 +1,17 @@
 class Carousel {
   constructor(el, option) {
     this.el = document.querySelector(el);
-    this.cover = this.el.children[0];
+    this.cover = this.el.firstElementChild;
     this.items = this.cover.children;
     this.item = this.items[0];
     this.option = Carousel.mergeOption(option);
     this.prevBtn = document.querySelector(this.option.prevBtn);
     this.nextBtn = document.querySelector(this.option.nextBtn);
+    this.coverWidth = this.cover.scrollWidth;
     this.itemWidth = this.item.offsetWidth;
     this.itemHeight = this.item.offsetHeight;
     this.itemLength = this.items.length;
+    this.isTransiting = false;
     this.offset = 0;
     this.current = 0;
 
@@ -52,6 +54,9 @@ class Carousel {
     return lastItem.offsetWidth;
   }
 
+  isCloneItem() {
+    return this.current === 0 || this.current === this.items.length - 1
+  }
 
   setCarouselSize() {
     this.el.style.width = this.itemWidth;
@@ -71,6 +76,8 @@ class Carousel {
   attachBtnEvent() {
     this.prevBtn.addEventListener("click", () => this.btnEventHandler("prev"));
     this.nextBtn.addEventListener("click", () => this.btnEventHandler("next"));
+
+    this.cover.addEventListener("transitionend", () => this.isTransiting = false)
   }
   
   btnEventHandler(direction) {
@@ -78,6 +85,8 @@ class Carousel {
   }
 
   movePrev() {
+    if(this.isTransiting) return;
+
     this.current--;
     this.setItemSize();
     this.setCarouselSize();
@@ -86,7 +95,9 @@ class Carousel {
     this.move();
     if (this.option.infinite) {
       if (this.isCloneItem()) {
-
+        this.offset -= this.coverWidth;
+        setTimeout(() => this.moveWithoutAnimation(), 500)
+        this.current += this.itemLength;
       }
     } else {
       this.checkMovable();
@@ -94,6 +105,8 @@ class Carousel {
   }
 
   moveNext() {
+    if(this.isTransiting) return;
+
     this.current++;
     this.setItemSize();
     this.setCarouselSize();
@@ -103,7 +116,9 @@ class Carousel {
 
     if (this.option.infinite) {
       if (this.isCloneItem()) {
-
+        this.offset += this.coverWidth;
+        setTimeout(() => this.moveWithoutAnimation(), 500)
+        this.current -= this.itemLength;
       }
     } else {
       this.checkMovable();
@@ -111,6 +126,8 @@ class Carousel {
   }
 
   move() {
+    this.isTransiting = true;
+
     this.cover.style.transition = `transform ${this.option.duration}ms ${this.option.easing}`;
     this.cover.style.transform = `translate3D(${this.offset}px, 0, 0)`;
   }
@@ -133,7 +150,7 @@ class Carousel {
 }
 
 const carousel = new Carousel(".benefit-content", {
-  // infinite: true,
+  infinite: true,
   prevBtn: ".arrow-left",
   nextBtn: ".arrow-right"
 });
