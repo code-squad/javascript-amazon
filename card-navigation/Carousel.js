5adadcafc04101fb736ac7ba10d7d2333cd94994
PAPICO <ocipap0531@gmail.com>
2019-06-17 14:29:10 +0900

    [Step11-2] Allen & PAPICO (#53)
    * Feat: html 구조 및 css 스타일링 작업
    
    * feat: benefit-card 추가
    
    * feat: Carousel 클래스 생성
    
    - construtor 속성 정의
    - mergeOption 매서드 작성
    
    * feat: init 매서드 로직 구현
    
    - setCarouselSize
    - addCarouselClass
    - attachBtnEvent
    - btnEventHandler
    
    * feat: 버튼 이벤트 동작 추가
    
    - movePrev
    - moveNext
    - move
    - setItemSize
    
    * feat: init 내부 infinite 로직 구현
    
    - addCloneItem
    - moveWithoutAnimation
    
    * feat: 무한 슬라이드 비활성시 동작 구현
    
    - checkMovable
    
    * docs: content 내용 추가
    
    * feat: 무한 슬라이드 로직 구현 & transition 중 이벤트 blocking
    
    - isCloneItem
    - cover에 transitionend 이벤트 추가
    
    * feat: stepList와 slider 연동
    
    - stepEventHandler
    - toggleStepClass
    
    * fix: 무한 slider 버그 수정
    
    * refactor: carousel 가변 기능 보류 & 중복함수 제거

---
+++ card-navigation/Carousel.js
@@ -0,0 +1,135 @@
+class Carousel {
+  constructor(el, option) {
+    this.el = document.querySelector(el);
+    this.cover = this.el.firstElementChild;
+    this.items = this.cover.children;
+    this.option = Carousel.mergeOption(option);
+    this.prevBtn = document.querySelector(this.option.prevBtn);
+    this.nextBtn = document.querySelector(this.option.nextBtn);
+    this.stepList = document.querySelector(this.option.stepList).children
+    this.itemWidth = this.items[0].offsetWidth;
+    this.itemLength = this.items.length;
+    this.isTransiting = false;
+    this.offset = 0;
+    this.current = 0;
+
+    this.init();
+  }
+
+  static mergeOption(option) {
+    const default_option = {
+      prevBtn: ".btn_prev",
+      nextBtn: ".btn_next",
+      step: ".step-list",
+      animate: true,
+      easing: "ease-in-out",
+      duration: "500",
+      infinite: false
+    };
+    return {
+      ...default_option,
+      ...option
+    };
+  }
+
+  init() {
+    // this.addCarouselClass(); 좀 더 라이브러리화 할때 변경 예정
+
+    if (this.option.infinite) {
+      this.addCloneItem();
+      this.offset -= this.itemWidth;
+      this.animateMove(false);
+      this.current++;
+    }
+    this.attachBtnEvent();
+    this.checkMovable();
+  }
+
+  addCloneItem() {
+    let firstItem = this.items[0];
+    let lastItem = this.items[this.itemLength - 1];
+    this.cover.insertBefore(lastItem.cloneNode(true), this.cover.firstChild);
+    this.cover.appendChild(firstItem.cloneNode(true));
+  }
+
+  isCloneItem() {
+    return this.current === 0 || this.current === this.items.length - 1
+  }
+  /*
+  addCarouselClass() {
+    this.cover.classList.add("carousel-cover");
+    Array.from(this.items).forEach(el => el.classList.add("carousel-item"));
+  }
+  */
+
+  attachBtnEvent() {
+    this.prevBtn.addEventListener("click", () => this.btnEventHandler("prev"));
+    this.nextBtn.addEventListener("click", () => this.btnEventHandler("next"));
+
+    this.cover.addEventListener("transitionend", () => this.isTransiting = false)
+
+    Array.from(this.stepList).forEach((el, i) => {
+      el.dataset.id = (this.option.infinite) ? i + 1 : i;
+      el.addEventListener("click", this.stepEventHandler.bind(this));
+    })
+  }
+
+  btnEventHandler(direction) {
+    direction = direction === "prev" ? -1 : 1;
+    if (this.isTransiting) return;
+
+    this.current += direction;
+
+    this.offset -= this.itemWidth * direction;
+    this.animateMove(true);
+    if (this.option.infinite) {
+      if (this.isCloneItem()) {
+        this.offset += this.itemWidth * this.itemLength * direction;
+        setTimeout(() => this.animateMove(false), this.option.duration);
+        this.current -= this.itemLength * direction;
+      }
+    } else {
+      this.checkMovable();
+    }
+    this.toggleStepClass();
+  }
+
+  animateMove(animate) {
+    this.isTransiting = animate;
+    this.cover.style.transform = `translate3D(${this.offset}px, 0, 0)`;
+    this.cover.style.transition = (animate) ? `transform ${this.option.duration}ms ${this.option.easing}` : 'none';
+  }
+
+  stepEventHandler(evt) {
+    let curId = Number(evt.target.dataset.id);
+
+    this.offset = this.offset - (this.itemWidth * (curId - this.current));
+    this.current = curId;
+    this.animateMove(true);
+
+    if (!this.option.infinite) {
+      this.checkMovable();
+    }
+    this.toggleStepClass()
+  }
+
+  checkMovable() {
+    if (this.current == 0) {
+      this.prevBtn.classList.add("arrow-disable");
+    } else if (this.current == this.itemLength - 1) {
+      this.nextBtn.classList.add("arrow-disable");
+    } else {
+      this.prevBtn.classList.remove("arrow-disable");
+      this.nextBtn.classList.remove("arrow-disable");
+    }
+  }
+
+  toggleStepClass() {
+    Array.from(this.stepList).forEach(el => {
+      el.classList.remove("active");
+    })
+    this.stepList[(this.option.infinite) ? this.current - 1 : this.current].classList.add("active")
+
+  }
+}
+
