2162bcc9419b66b2e6253dba519d022e3cb84432
park78951 <park78951@gmail.com>
2019-06-17 15:47:32 +0900

    delete unnecessary comment in each file

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -27,15 +27,9 @@ class Carousel {
     this.offset = offset;
     this.currentItem = currentItem;
     this.itemsLength = this.items.length;
-    // 설정 정보
     this.config = config
   }
   init() {
-    /*
-      carousel-item의 width/height는 가변이다.
-      따라서 carousel의 윈도우 역할을 하는 carousel 요소의 width/height는
-      첫번째 carousel-item의 width/height를 취득해 셋팅한다.
-    */
     this.carousel.style.width = this.item.offsetWidth + 'px';
     this.carousel.style.height = this.item.offsetHeight + 'px';
     this.attachEvent();
@@ -50,29 +44,17 @@ class Carousel {
     this.headerItems.forEach(card => card.addEventListener('click', this.cardClick.bind(this)))
   }
   moveToNext() {
-    // carousel-container 요소를 왼쪽으로 이동시키기 위해 이동거리를 캐러셀 아이템의 너비만큼 감소시킨다.
     this.offset -= this.itemWidth;
-    // 다음 슬라이더로 이동하기 위해 carousel-container 요소를 왼쪽으로 이동시킨다.
     this.moveMain();
-    // 현재 표시 중인 캐러셀 아이템 인덱스(1~4)
     this.currentItem++;
-    // prev, next 버튼 활성화/비활성화 결정
-    // this.checkMovable();
     if(this.isClone()) this.fakeMove();
-    // ** added by Q
     this.moveHeader(this.currentItem-1);
   }
   moveToPrev() {
-    // carousel-container 요소를 오른쪽으로 이동시키기 위해 이동거리를 캐러셀 아이템의 너비만큼 증시킨다.
     this.offset += this.itemWidth;
-    // 이전 슬라이더로 이동하기 위해 carousel-container 요소를 오른쪽으로 이동시킨다.
     this.moveMain();
-    // 현재 표시 중인 캐러셀 아이템 인덱스(1~4)
     this.currentItem--;
-    // prev, next 버튼 활성화/비활성화 결정
-    // this.checkMovable();
     if(this.isClone()) this.fakeMove();
-    // ** added by Q
     this.moveHeader(this.currentItem-1);
   }
   moveMain() {
@@ -109,7 +91,6 @@ class Carousel {
 
   cardClick(e) {
     const clickedIndex = this.getCardIndex(e.target.closest(".carousel__header--item"));
-    // ** added by Q
     const currentIndex = this.currentItem - 1;
     this.offset += this.itemWidth * (currentIndex - clickedIndex);
     this.moveHeader(clickedIndex);
@@ -117,7 +98,6 @@ class Carousel {
     this.moveMain();
     this.currentItem = clickedIndex + 1;
   } 
-  // ** added by Q
   moveHeader(clickedIndex) {
     this.header.querySelector(".active").classList.remove("active");
     this.headerItems[clickedIndex].classList.add("active");
