4fab8d88d8803e6d1fb1bf02af544f418bf14cb2
Gren <39091443+fed-gren@users.noreply.github.com>
2019-06-17 14:13:27 +0900

    [Step11-2] Jin & Gren (#52)
    * init: set dev env
    
    - sass script 작성
    
    * add gitignore
    
    - node, sass, vscode
    
    * feat: flex mixins
    
    - flexbox를 위한 mixin 추가
    
    * feat: markup
    
    - HTML 작성
    
    * feat: style
    
    * feat: set card-wrapper size
    
    - carousel 동작을 위한 js 파일 생성
    - DOM 캐싱
    - window에 load 이벤트 등록. load 되면 card-wrapper 크기를 첫 번째 card 만큼 조절
    
    * fix: adjust width
    
    - flex 속성값을 수정해서 carousel container width 초기 설정에 오류 수정
    
    * feat: es modules 적용
    
    * fix: load 이후 opacity 수정
    
    - size 조정 이후 화면에 보여지게 수정
    - css 수정
    
    *  feat: move card slider
    
    * feat: navigation click
    
    - 네비게이션 클릭 시 해당 아이템으로 이동
    
    * feat: infinite
    
    - carousel infinite 기능 구현
    
    * refactor: carousel move
    
    - 1차 리팩토링
    - 카드 이동 로직을 메서드 하나로 통합
    
    * refactor: move logic
    
    - 파라미터에 객체 리터럴 사용
    - moveSlider 메서드 작성. move에서 분리
    - isEndOfCard 메서드 작성. move에서 분리
    
    * add: image
    
    * style: css 수정
    
    - nav item 너비, 높이 조정
    - card image 적용
    
    * feat: carousel option
    
    - 옵션값 입력 가능하도록 설정
    
    * style: header, button
    
    - h3 -> h2 사용
    - 버튼 hover 속성 추가
    
    * feat: nav item selected class
    
    - 선택된 카드 nav 확대 기능 추가
    
    * refactor: change method
    
    - 주석 제거
    - map -> forEach 사용

---
+++ carousel.js
@@ -0,0 +1,109 @@
+class Carousel {
+  constructor(config) {
+    //DOM
+    this.container = document.querySelector(".container");
+    this.navItems = this.container.querySelectorAll(".nav-item");
+    this.cardWrapper = this.container.querySelector(".card-wrapper");
+    this.cardSlider = this.container.querySelector(".card-slider");
+    this.card = this.cardSlider.querySelector(".card");
+    this.prevButton = this.container.querySelector(".prev");
+    this.nextButton = this.container.querySelector(".next");
+
+    // values
+    this.initIndex = 0;
+    this.itemWidth = this.card.getBoundingClientRect().width;
+    this.offset = 0;
+    this.currentItem = this.initIndex + 1;
+    this.itemLength = this.cardSlider.querySelectorAll(".card").length;
+
+    this.defaultConfig = {
+      infinite: true
+    };
+
+    this.config = this.mergeConfig(config);
+  }
+
+  mergeConfig(config) {
+    return Object.assign(this.defaultConfig, config);
+  }
+
+  init() {
+    this.cardWrapper.style.width = `${this.itemWidth}px`;
+    this.attatchEvent();
+    if (this.config.infinite) {
+      this.cloneVirtualCard();
+      this.move({
+        getId: () => this.initIndex + 1
+      });
+    } else {
+      this.isMovable();
+    }
+    this.container.style.opacity = 1;
+  }
+
+  cloneVirtualCard() {
+    const firstCard = this.cardSlider.firstElementChild.cloneNode(true);
+    const lastCard = this.cardSlider.lastElementChild.cloneNode(true);
+    this.cardSlider.insertAdjacentElement("afterbegin", lastCard);
+    this.cardSlider.insertAdjacentElement("beforeend", firstCard);
+  }
+
+  attatchEvent() {
+    this.prevButton.addEventListener("click", () =>
+      this.move({
+        to: this.itemLength,
+        from: 1,
+        getId: () => this.currentItem - 1
+      })
+    );
+    this.nextButton.addEventListener("click", () =>
+      this.move({
+        to: 1,
+        from: this.itemLength,
+        getId: () => this.currentItem + 1
+      })
+    );
+    this.navItems.forEach((item, index) => {
+      item.addEventListener("click", () =>
+        this.move({
+          getId: () => index + 1
+        })
+      );
+    });
+  }
+
+  isMovable() {
+    if (this.config.infinite) return;
+    this.prevButton.disabled = this.currentItem === 1 ? true : false;
+    this.nextButton.disabled =
+      this.currentItem === this.itemLength ? true : false;
+  }
+
+  move({ to, from, getId }) {
+    const id = this.isEndOfCards(from) ? to : getId();
+    this.offset = this.config.infinite
+      ? -(this.itemWidth * id)
+      : -(this.itemWidth * (id - 1));
+    this.currentItem = id;
+    this.isMovable();
+    this.selectNav();
+    this.moveSlider();
+  }
+
+  moveSlider() {
+    this.cardSlider.style.transform = `translateX(${this.offset}px)`;
+  }
+
+  isEndOfCards(from) {
+    return this.config.infinite && this.currentItem === from;
+  }
+
+  selectNav() {
+    this.navItems.forEach((item, index) => {
+      if (index + 1 === this.currentItem) item.classList.add("selected");
+      else item.classList.remove("selected");
+    });
+  }
+}
+
+export default Carousel;
