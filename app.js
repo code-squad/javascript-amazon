9c29172a07832a4b593abcb73e076fbf2c53d017
park78951 <park78951@gmail.com>
2019-06-17 16:30:14 +0900

    add DOMContentLoaded event in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import config from './src/js/config.js';
 import Carousel from './src/js/carousel.js'
 
-const carousel = new Carousel(config);
-carousel.init();
+window.addEventListener('DOMContentLoaded', () => {
+  const carousel = new Carousel(config);
+  carousel.init();
+})
