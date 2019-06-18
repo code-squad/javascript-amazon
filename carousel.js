5a934d4c001851ec24ac1a352772d7bc67f54091
leehwarang <hwrng2@gmail.com>
2019-06-18 11:56:34 +0900

    refactor Carousel 클래스 내부에 Btn에 관련된 로직도 추가

--- carousel.js
+++ carousel.js
@@ -16,70 +16,45 @@
 //   currentPointer = navPointer;
 // });
 
-// let currentPointer = -carouselWidth;
-// prevBtn.addEventListener("click", function() {
-//   let active = document.querySelector(".active");
-//   let activeIndex = active.getAttribute("data-index");
-//   carousel.children[Number(activeIndex) - 1].classList.add("active");
-//   currentPointer += carouselWidth;
-//   carousel.style.left = `${currentPointer}px`;
-
-//   carousel.children[activeIndex].classList.remove("active");
-//   let newActive = carousel.children[Number(activeIndex) - 1];
-//   if (newActive.classList.contains("clone")) {
-//     carousel.style.left = `-${carouselWidth * carouselInitCnt}px`;
-//     newActive.classList.remove("active");
-//     carousel.children[carouselInitCnt].classList.add("active");
-//     currentPointer = -(carouselWidth * carouselInitCnt);
-//   }
-// });
-
-// nextBtn.addEventListener("click", function() {
-//   let active = document.querySelector(".active");
-//   let activeIndex = active.getAttribute("data-index");
-
-//   carousel.children[Number(activeIndex) + 1].classList.add("active");
-
-//   let newActive = carousel.children[Number(activeIndex) + 1];
-//   currentPointer -= carouselWidth;
-//   carousel.style.left = `${currentPointer}px`;
-
-//   carousel.children[activeIndex].classList.remove("active");
-
-//   if (newActive.classList.contains("clone")) {
-//     carousel.style.left = `-${carouselWidth}px`;
-//     newActive.classList.remove("active");
-//     carousel.children[1].classList.add("active");
-//     currentPointer = -carouselWidth;
-//   }
-// });
-
 class Carousel {
   constructor(input, option) {
     this.carousel = document.querySelector(input);
     this.carouselWrap = this.carousel.parentNode;
     this.carouselItems = this.carousel.children;
     this.carouselWidth = this.carousel.offsetWidth;
+    this.carouselItemsInitCnt = this.carouselItems.length;
     this.carouselItemsCnt = this.carouselItems.length;
+    this.currentPointer = -this.carouselWidth;
     this.option = option;
+
+    this.prevBtn = document.querySelector(".btn-prev");
+    this.nextBtn = document.querySelector(".btn-next");
   }
 
   init() {
-    this.appendCloneItem(
-      this.carousel,
-      this.carouselItems[0],
-      this.carouselItems[this.carouselItemsCnt - 1]
-    );
-    let carouselFullWidth = this.calcCarouselFullWidth(this.option.infinite);
+    if (this.option.infinite) {
+      this.appendCloneItem(
+        this.carousel,
+        this.carouselItems[0],
+        this.carouselItems[this.carouselItemsCnt - 1]
+      );
+      this.carouselItemsCnt = this.carouselItems.length;
+    }
+    let carouselFullWidth = this.calcCarouselFullWidth();
     this.setCarouselFullWidth(carouselFullWidth);
     this.setAttrToElement("data-index");
-    this.addElementToClass();
+
+    if (this.option.infinite) {
+      this.carousel.style.left = `-${this.carouselWidth}px`;
+      this.carouselItems[1].classList.add("active");
+    } else {
+      this.carouselItems[0].classList.add("active");
+    }
+
+    this.attchEventToBtn();
   }
 
-  calcCarouselFullWidth(isInfinite) {
-    if (isInfinite) {
-      this.carouselItemsCnt += 2;
-    }
+  calcCarouselFullWidth() {
     return this.carouselWidth * this.carouselItemsCnt;
   }
 
@@ -99,7 +74,6 @@ class Carousel {
     parentNode.prepend(lastCloneItem);
     parentNode.appendChild(firstCloneItem);
 
-    // 기능은 동작하는데 error 발생
     this.addElementToClass(firstCloneItem, "clone");
     this.addElementToClass(lastCloneItem, "clone");
   }
@@ -107,8 +81,68 @@ class Carousel {
   setAttrToElement(attrName) {
     let cnt = this.carouselItems.length;
     for (let i = 0; i < cnt; i += 1) {
-      this.carouselItems[i].setAttribute(attrName, i);
+      if (this.option.infinite) {
+        this.carouselItems[i].setAttribute(attrName, i);
+      } else {
+        this.carouselItems[i].setAttribute(attrName, i + 1);
+      }
     }
+  }
+
+  getActiveItem() {
+    let active = document.querySelector(".active");
+    return active.getAttribute("data-index");
+  }
+
+  updateActiveItem(activeIndex, moveValue) {
+    this.carouselItems[activeIndex].classList.remove("active");
+    this.carouselItems[Number(activeIndex) + moveValue].classList.add("active");
+    let newActiveItem = document.querySelector(".active");
+    return newActiveItem;
+  }
+
+  moveCarousel(moveValue) {
+    this.currentPointer += moveValue * this.carouselWidth;
+    this.carousel.style.left = `${this.currentPointer}px`;
+  }
+
+  moveCloneCarousel(newPointerValue) {
+    this.carousel.style.left = `-${newPointerValue}px`;
+    this.currentPointer = -newPointerValue;
+  }
+
+  updateCloneActiveItem(newActiveItem, value) {
+    newActiveItem.classList.remove("active");
+    this.carouselItems[value].classList.add("active");
+  }
+
+  attchEventToBtn() {
+    this.prevBtn.addEventListener("click", () => {
+      let activeIndex = this.getActiveItem();
+      let newActiveItem = this.updateActiveItem(activeIndex, -1);
+      this.moveCarousel(1);
+
+      if (this.option.infinite) {
+        if (newActiveItem.classList.contains("clone")) {
+          this.updateCloneActiveItem(newActiveItem, this.carouselItemsInitCnt);
+          let newPointerValue = this.carouselWidth * this.carouselItemsInitCnt;
+          this.moveCloneCarousel(newPointerValue);
+        }
+      }
+    });
+    this.nextBtn.addEventListener("click", () => {
+      let activeIndex = this.getActiveItem();
+      let newActiveItem = this.updateActiveItem(activeIndex, 1);
+      this.moveCarousel(-1);
+
+      if (this.option.infinite) {
+        if (newActiveItem.classList.contains("clone")) {
+          this.updateCloneActiveItem(newActiveItem, 1);
+          let newPointerValue = this.carouselWidth;
+          this.moveCloneCarousel(newPointerValue);
+        }
+      }
+    });
   }
 }
 
