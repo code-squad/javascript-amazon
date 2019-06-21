6c2813d511cd8951765987270575b8de62b5fb4e
fed-gren <deveb22@gmail.com>
2019-06-21 15:09:13 +0900

    refactor: optionize navigation
    - carousel에서 nav 클래스명을 입력하면 Jungle UI의 navigation이 생성되도록 수정

--- Jungle.js
+++ Jungle.js
@@ -2,10 +2,17 @@ import Carousel from "./components/Carousel.js";
 import Navigation from "./components/Navigation.js";
 
 class Jungle {
+  constructor() {
+
+  }
   createCarousel(classes, options) {
     const carousel = new Carousel(classes, options);
     carousel.init();
+    if("nav" in classes) {
+      const nav = this.createNavigation({nav: classes.nav});
+    }
     return carousel;
+
   }
 
   createNavigation(classes) {
--- app.js
+++ app.js
@@ -3,6 +3,5 @@ import Jungle from "./Jungle.js";
 const jungle = new Jungle();
 
 window.onload = () => {
-  const carousel = jungle.createCarousel({container : ".container", slider: ".card-slider"});
-  const navigation = jungle.createNavigation({nav: ".nav-item"});
+  const carousel = jungle.createCarousel({container : ".container", slider: ".card-slider", nav: ".nav-item"});
 };
