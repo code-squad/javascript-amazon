f6639f970a24b99c9e13e0de7f8107d9529dc325
eugene94 <eugenekang94@gmail.com>
2019-06-26 14:22:32 +0900

    Feat: local data fetch

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const MyPromise = require('./promise/promise');
+const data = fetch('./resources/localData.json');
 
-const myFirstPromise = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve({ name: 'Success!', id: 123123 });
-  }, 2000);
-});
-
-myFirstPromise
-  .then(successMessage => {
-    return successMessage.name;
-  })
-  .then(data => {
-    console.log(`data is ${data}`);
-  })
-  .catch(error => console.log(`oops, this is ${error}`));
-
-// error test code
-// myFirstPromise
-//   .then(successMessage => {
-//     successMessage.hello();
-//   })
-//   .then(data => {
-//     console.log(`data is ${data}`);
-//   })
-//   .catch(error => console.log(`oops, I found error! error: [${error}]`));
+// prettier-ignore
+data
+  .then(response => response.json())
+  .then(data => console.log(data));
