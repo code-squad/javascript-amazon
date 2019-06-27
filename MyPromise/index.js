21a83a47f2e9a563e220b7c50c2dee223b26a00c
fed-gren <deveb22@gmail.com>
2019-06-27 19:00:17 +0900

    feat: then, reject, catch

--- MyPromise/index.js
+++ MyPromise/index.js
@@ -1,53 +1,93 @@
 const state = require("./constants/PromiseState");
+const type = require("./constants/TypesString");
 
 class MyPromise {
   constructor(action) {
     this.state = state.PENDING;
     this.value = undefined;
 
-    this.microtasks = [];
-
-    action(resolve.bind(this), reject.bind(this));
+    this.handler = undefined;
 
     function resolve(value) {
+      if (this.handler === undefined) return;
+
       this.state = state.FULFILLED;
       this.value = value;
 
-      this.microtasks.forEach(handler => {
-        setTimeout(() => {
-          handler(this.value);
-        }, 0);
-      }, this);
+      setTimeout(() => {
+        const handler =
+          this.state === state.FULFILLED
+            ? this.handler.onfulfill
+            : this.handler.onreject;
+        this.value = handler(this.value);
+      }, 0);
     }
 
     function reject(reason) {
-      console.log("I am reject");
+      if (this.handler === undefined) return;
+
+      this.state = state.REJECTED;
+      this.value = reason;
+
+      setTimeout(() => {
+        const handler = this.handler.onreject;
+        this.value = handler(this.value);
+      }, 0);
+    }
+
+    try {
+      action(resolve.bind(this), reject.bind(this));
+    } catch (error) {
+      reject(error);
     }
   }
 
-  then(cb) {
-    this.microtasks.push(cb);
-    return this;
+  then(onfulfill, onreject) {
+    return new MyPromise((resolve, reject) => {
+      const internalOnfulfill = value => {
+        try {
+          resolve(
+            typeof onfulfill === type.FUNCTION ? onfulfill(value) : value
+          );
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      const internalOnreject = reason => {
+        try {
+          if (typeof onreject === type.FUNCTION) {
+            resolve(onreject(reason));
+          } else {
+            reject(reason);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      this.handler = {
+        onfulfill: internalOnfulfill,
+        onreject: internalOnreject
+      };
+    });
+  }
+
+  catch(onreject) {
+    return this.then(null, onreject);
   }
 }
 
 let myFirstPromise = new MyPromise((resolve, reject) => {
   setTimeout(() => {
-    resolve({"name":"Success!", "id" :123123});
+    resolve({ name: "Success!", id: 123123 });
   }, 1000);
 });
 
-// myFirstPromise.then(data => {
-//   console.log(data);
-//   return data;
-// }).then(data => {
-//   setTimeout(() => {
-//     console.log(data, "2");
-//   }, 1000);
-// });
-myFirstPromise.then(data => {
-  console.log(data);
-  return data.name;
-}).then(data => {
-  console.log(data);
-});
+myFirstPromise
+  .then(successMessage => {
+    return successMessage.name;
+  })
+  .then(data => {
+    console.log(`data is ${data}`);
+  });
