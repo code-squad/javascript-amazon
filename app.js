b80440fb2ec165803d4cb9d0c34225949af40fdb
eugene94 <eugenekang94@gmail.com>
2019-06-28 21:40:26 +0900

    Feat: HTML templating
    - html에서 template로 추가 가능한 dom 제거
    - index.js에서 template작업

--- app.js
+++ app.js
@@ -2,22 +2,59 @@ import Jungle from './Jungle/index.js';
 
 const jungle = new Jungle();
 
-window.addEventListener('DOMContentLoaded', () => {
-  const carousel = jungle.createCarousel({
-    elClassNameObj: {
-      container: '.container',
-      slider: '.card-slider',
-      nav: '.nav'
-    }
-  });
+const getData = async () =>
+  await fetch('./resources/localData.json').then(response => response.json());
 
-  // const carousel2 = jdungle.createCarousel({
-  //   elClassNameObj: { container: ".container2", slider: ".card-slider2" },
-  //   options: {
-  //     duration: 100,
-  //     animation: "ease-in",
-  //     infinite: false
-  //   }
-  // });
-  // const nav = jungle.createNavigation({ elClassNameObj: { nav: '.nav2' } });
+const makeCard = data => {
+  return `
+    <div class="card" key=${data._id}>
+      <div class="thumb">
+      <img src="${data.imgURL}" alt="card-thumbnail" />
+      </div>
+      <div class="content">
+        <h2>${data.title}</h2>
+        <ul>
+          ${data.desc.reduce((acc, cur) => {
+            const li = `<li>${cur}</li>`;
+            acc += li;
+            return acc;
+          }, '')}
+        </ul>
+      </div>
+    </div>`;
+};
+
+const makeNavItem = data => {
+  return `
+    <div class="nav-item ${data.color}">${data.title}</div>
+  `;
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  const data = getData();
+
+  data
+    .then(cards => {
+      const cardElement = cards.reduce((acc, cur) => {
+        acc += makeCard(cur);
+        return acc;
+      }, '');
+
+      const navItemElement = cards.reduce((acc, cur) => {
+        acc += makeNavItem(cur);
+        return acc;
+      }, '');
+
+      document.querySelector('.card-slider').insertAdjacentHTML('afterbegin', cardElement);
+      document.querySelector('.nav').insertAdjacentHTML('afterbegin', navItemElement);
+    })
+    .then(_ => {
+      jungle.createCarousel({
+        elClassNameObj: {
+          container: '.container',
+          slider: '.card-slider',
+          nav: '.nav'
+        }
+      });
+    });
 });
