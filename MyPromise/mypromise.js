9143778e09604025132489869de6437d3476f9c4
leehwarang <hwrng2@gmail.com>
2019-06-30 21:39:24 +0900

    feat: resolve가 중첩된 then에서 동작하도록 기능 추가

--- MyPromise/mypromise.js
+++ MyPromise/mypromise.js
@@ -1,32 +1,48 @@
 class MyPromise {
-    constructor(func) {
-      this.resolve = this.resolve.bind(this);
-      func(this.resolve);
-      this.resolveFun;
-    }
-  
-    resolve(argument) {
-      this.resolveFun(argument);
-    }
-  
-    then(thenfirstCB) {
-      this.resolveFun = thenfirstCB;
-    }
-  
-    reject() {}
-  
-    catch() {}
+  constructor(func) {
+    this.callbackQueue = [];
+    this.argumentQueue = [];
+    this.resolve = this.resolve.bind(this);
+    func(this.resolve);
   }
-  
-  let cb = (resolve, reject) => {
-    setTimeout(() => {
-      resolve("Success!");
-    }, 3000);
-  };
-  
-  let myFirstPromise = new MyPromise(cb);
-  
-  myFirstPromise.then(successMessage => {
-    console.log("Yay! " + successMessage);
+
+  resolve(argument) {
+    let arg, resolveFunc, result;
+    this.argumentQueue.push(argument);
+
+    while (this.argumentQueue.length > 0 && this.callbackQueue.length > 0) {
+      arg = this.argumentQueue.shift();
+      resolveFunc = this.callbackQueue.shift();
+      result = resolveFunc(arg);
+      if (this.callbackQueue.length > 0) {
+        this.argumentQueue.push(result);
+      }
+    }
+  }
+
+  then(thenfirstCB) {
+    this.callbackQueue.push(thenfirstCB);
+    console.log(this);
+    return this;
+  }
+
+  reject() {}
+
+  catch() {}
+}
+
+let cb = (resolve, reject) => {
+  setTimeout(() => {
+    resolve({ name: "Success!", id: 123123 });
+  }, 1000);
+};
+
+let myFirstPromise = new MyPromise(cb);
+
+myFirstPromise
+  .then(successMessage => {
+    return successMessage.name;
+  })
+  .then(data => {
+    console.log(`data is ${data}`);
   });
-  
