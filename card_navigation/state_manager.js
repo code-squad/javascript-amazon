b7c5593f0a26abcbbf4057d4d9e230039ae1a186
allen <revenoir7@gmail.com>
2019-07-02 15:10:52 +0900

    refactor: StateManager클래스 메소드 작성
    - input을 받아서 state를 업데이트 하는 로직 작성

--- card_navigation/state_manager.js
+++ card_navigation/state_manager.js
@@ -1,14 +1,46 @@
 import Publisher from './publisher.js'
 
 export default class StateManager extends Publisher {
-  constructor(startIdx, panelQuantity) {
+  constructor({ startIdx, quantityToSlide, panelQuantity }) {
     super();
     this.state = {
       currentIdx: startIdx,
       targetIdx: startIdx,
       direction: null,
+      quantityToSlide: quantityToSlide,
       panelQuantity: panelQuantity
     };
   }
 
+  setState(data) {
+    this.state = { ...this.state, ...data };
+
+    if (data.direction) {
+      this.updateTargetIdx(this.state);
+      this.updateDirection(this.state);
+      this.updateCurrentIdx(this.state);
+      //TODO: pagination추가되면 함수이름에 따라 notify 시점을 달리해줘야할듯
+      //(캐러셀 - sync하기 전, pagination - sync한 이후 notify)
+      console.log(this.state.targetIdx)
+      this.notify(this.state);
+    }
+  }
+
+  updateTargetIdx(state) {
+    let { currentIdx, direction, quantityToSlide } = state;
+    direction = direction === 'prev' ? -1 : 1;
+    state.targetIdx = currentIdx + direction * quantityToSlide;
+  }
+
+  updateDirection(state) {
+    const direction = state.targetIdx - state.currentIdx;
+    state.direction = direction < 0 ? 'prev' : 'next';
+  }
+
+  updateCurrentIdx(state) {
+    let { targetIdx, panelQuantity } = state;
+    state.currentIdx = targetIdx < 0 || targetIdx > panelQuantity - 1
+      ? panelQuantity - Math.abs(targetIdx)
+      : targetIdx;
+  }
 }
