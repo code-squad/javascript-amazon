62aa284959071bbe98d1c2b22a501620e637f4d6
sunwook kim <koownusmik@gmail.com>
2019-07-02 15:30:53 +0900

    feat: then 함수가 Promise가 fulfilled되었을때 실행되도록 구현
    then은 내부에서 cb가 상태가 변했을때 실행되도록한다.
    따라서 resolve에서 상태를 바꾸고, then을 한번 더 호출해야한다.
    이에따라 then의 콜백함수를 변수로 기억하도록 하였다. (변수가 하나 더 생겨서 살짝 지저분한것같다)

--- myPromise/myPromise.js
+++ myPromise/myPromise.js
@@ -1,46 +1,42 @@
 class MyPromise {
-    constructor(cb){
+    constructor(cb) {
         this.state = 'pending';
         this.value = undefined;
+        this.thensCallback = undefined;
         cb(this.resolve.bind(this), this.reject.bind(this));
     }
 
-    resolve(value){
+    resolve(value) {
         this.value = value;
         this.state = 'fulfilled';
+        //resolve에서 저장한 then의 콜백함수를 then과 함께 다시실행하도록 함
+        this.then(this.thensCallback);
     }
 
-    reject(){
+    reject() {
 
     }
 
-    then(cb){
-        // 상태가 pending 이 아닌지 확인한다. pending에서 fulfilled로 바뀌면 실행되도록 해야한다. 
-        // 어떻게 해야 then 함수가 바로 실행되지 않고, Promise의 상태가 바뀐 다음에 실행되도록 할 수 있을까? 
-        // --> 1. resolve에서 상태를 바꾼 다음에 then 함수를 실행한다. 
-        // <-- 그런데 전역에서 이미 then 함수가 실행된다. 따라서 then 함수는 이미 불려져야한다는 이야기다.
-        // --> 2. then 함수가 실행되면 무엇을 하는가를 잘 생각해보자  
-        // <-- then함수는 콜백함수들을 다 등록을 한다. 따라서 then은 일단은 콜백함수를 등록하는 로직이 필요하다. 
-        // 콜백함수를 등록한다. 상태가 바뀌면 등록했던 콜백함수를 실행한다. 따라서 then 함수에서는 콜백함수만 등록하는것으로 하고..
-        // 등록된 콜백함수를 실행하는건 
-
-    
-        // 콜백함수는 받는다 치는데, 콜백함수의 인자는 어떻게 받는담? 
-        // if(this.state !== 'pending') cb()
-        let extraParam = this.value ;
-        const thensPromise = cb(extraParam);
-        return thensPromise
+    then(cb) {
+        let extraParam = this.value;
+        this.thensCallback = cb;
+        if (this.value !== undefined) {
+            const thensPromise = cb(extraParam);
+            return thensPromise
+        }
     }
 
-    
+
 }
 
-let mySecondPromise = new MyPromise((MyResolve, MyReject) => 
-    MyResolve({"name":"Success!", "id" :123123})
-  )
+let mySecondPromise = new MyPromise((MyResolve, MyReject) => {
+    setTimeout(() => {
+        MyResolve({ "name": "Success!", "id": 123123 });
+    }, 1000);
+})
 
 
-mySecondPromise.then((successMessage)=>{
+mySecondPromise.then((successMessage) => {
     return successMessage.name;
 })
 
@@ -50,7 +46,7 @@ mySecondPromise.then((successMessage)=>{
 //       resolve({"name":"Success!", "id" :123123});
 //     }, 1000);
 //   });
-  
+
 //   myFirstPromise.then((successMessage) => {
 //     return successMessage.name;
 //   }).then((data) => {
