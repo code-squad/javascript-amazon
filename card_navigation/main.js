e1327014896b91eec1479bdcd99b08eba5ce458b
allen <revenoir7@gmail.com>
2019-07-02 11:46:02 +0900

    feat: json data를 받아오는 getJsonData 메소드 작성

--- card_navigation/main.js
+++ card_navigation/main.js
@@ -1,10 +1,20 @@
-const init = _ => {
+import $ from './allenibrary.js'
+import Carousel from './carousel.js'
+
+const getJsonData = url => fetch(url).then(res => res.json());
+
+const initCarousel = () => {
   const carousel = new Carousel(".benefit-content", {
     infinite: true,
     prevBtn: ".arrow-left",
     nextBtn: ".arrow-right",
     stepList: ".benefit-list"
   });
+}
+
+const init = _ => {
+  const data = getJsonData('localData.json');
+  initCarousel();
 };
 
 window.addEventListener("DOMContentLoaded", _ => init());
