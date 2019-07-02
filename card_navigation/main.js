6fe7ccdbe4cb387ff0c7963a178d73e7ff4ea1cb
allen <revenoir7@gmail.com>
2019-07-02 11:51:09 +0900

    feat: 받아온 data를 template을 활용하여 렌더링하는 메소드 작성

--- card_navigation/main.js
+++ card_navigation/main.js
@@ -1,6 +1,27 @@
 import $ from './allenibrary.js'
 import Carousel from './carousel.js'
 
+const getDescLists = (descLists) => {
+  return descLists.reduce((acc, curr) => acc + `<li>${curr}</li>`, '')
+}
+
+const insertCards = (data) => {
+  let tpl = data.reduce((acc, curr) => {
+    const { id, title, desc, imgURL } = curr;
+    return acc + `<li class='benefit-card' id=${id} style='background-image:url(${imgURL})'>
+                <div class='benefit-desc'>
+                  <h4 class='benefit-title'>${title}</h4>
+                  <ul class='desc-list'>
+                    ${getDescLists(desc)}
+                    </ul>
+                </div>
+                </li>`
+  }, '')
+
+  const cardWrapper = $('.card-wrapper');
+  cardWrapper.insertAdjacentHTML('beforeend', tpl);
+}
+
 const getJsonData = url => fetch(url).then(res => res.json());
 
 const initCarousel = () => {
@@ -14,7 +35,9 @@ const initCarousel = () => {
 
 const init = _ => {
   const data = getJsonData('localData.json');
-  initCarousel();
+  data
+    .then(data => insertCards(data))
+    .then(() => initCarousel())
 };
 
 window.addEventListener("DOMContentLoaded", _ => init());
