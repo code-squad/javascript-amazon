7992c1e3bed2c7c08d68a8bee52245d55375a224
sunwook kim <koownusmik@gmail.com>
2019-07-03 15:34:59 +0900

    feat: then에서 기존의 Promise 객체를 수정하여 구현
    1. this.callback를 this.callbackQueue로 변경
    2. executeThensCallback 함수를 생성
    이 함수는 등록된 콜백함수를 실행하고 promise 객체의 value를 변경하고 콜백큐를 수정하며 resolve에서 실행된다.
    3. 하나의 promise에서 실행되므로 resolve에서 while문을 통해 콜백큐에 등록된 콜백들을 실행한다.
    
    4. 그러나 이런 방식은 Promise생성시 pending이 resolve로 바뀌면 나머지 Promise는 자동으로 resolve상태로 담아있게된다.

--- myPromise/myPromise.js
+++ myPromise/myPromise.js
@@ -2,53 +2,51 @@ class MyPromise {
     constructor(cb) {
         this.state = 'pending';
         this.value = undefined;
-        this.thensCallback = undefined;
+        this.thensCallbackQueue = [];
         cb(this.resolve.bind(this), this.reject.bind(this));
     }
 
     resolve(value) {
         this.value = value;
         this.state = 'fulfilled';
-        //resolve에서 저장한 then의 콜백함수를 then과 함께 다시실행하도록 함
-        this.then(this.thensCallback);
+        // MyPromise 를 then마다 만들지 않고 한번에 처리하려다 보니 발생한 현상.. 
+        while(this.thensCallbackQueue.length !== 0){
+            const currentCallback = this.thensCallbackQueue[0];
+            this.excuteThensCallback(currentCallback);
+        }
     }
 
-    reject() {
+    reject(value) {}
 
+    then(cb){
+        if(!this.thensCallbackQueue.includes(cb)) {
+            this.thensCallbackQueue.push(cb);
+            return this;
+        }
     }
 
-    then(cb) {
-        let extraParam = this.value;
-        this.thensCallback = cb;
-        // console.log('여기는 다 실행됨',this.value);
-        if (this.value === undefined) return 
-        const fetchedValue = cb(extraParam);
+    excuteThensCallback(cb) {
+        const fetchedValue = cb(this.value);
+        this.value = fetchedValue;
+        this.thensCallbackQueue.shift();
     }
-
-
 }
-
-let mySecondPromise = new MyPromise((MyResolve, MyReject) => {
+const myFirstPromise = new MyPromise((MyResolve, MyReject) => {
     setTimeout(() => {
         MyResolve({ "name": "Success!", "id": 123123 });
     }, 1000);
 })
-
-
-mySecondPromise.then((successMessage) => {
-    // console.log("여기는 Promise의 상태가 변해야만 실행됨",successMessage)
+myFirstPromise.then((successMessage) => {
     return successMessage.name;
-})
+}).then((data) => {
+    console.log(`data is ${data}`);
+    return data
+  }).then((data2) => {
+      console.log(`data2 인 ${data2}를 잘 받았습니다.`);
+      return data2
+  }).then((data3) => { 
+      console.log(`data3 is ${data3}`);
+  })
 
-// 구현되어야할 예제 
-// let myFirstPromise = new MyPromise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve({"name":"Success!", "id" :123123});
-//     }, 1000);
-//   });
 
-//   myFirstPromise.then((successMessage) => {
-//     return successMessage.name;
-//   }).then((data) => {
-//     console.log(`data is ${data}`)
-//   })
+
