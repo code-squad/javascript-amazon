20b52bd41347b63b4c395731f190250f132d29ea
allen <revenoir7@gmail.com>
2019-07-04 16:37:24 +0900

    refactor: delegate 스니펫을 기존 유틸로 이동 및 classList를 활용하여 작동하게끔 수정

--- card_navigation/carouselUI/allenibrary.js
+++ card_navigation/carouselUI/allenibrary.js
@@ -2,4 +2,25 @@ const $ = (selector) => {
   return document.querySelector(selector);
 }
 
-export default $
+const delegate = (el, eventType, domElProperty, funcMap) => {
+  el.addEventListener(eventType, ({ target }) => {
+    if (domElProperty === 'classList') {
+      target[domElProperty].forEach(className => {
+        if (funcMap[className]) funcMap[className](target);
+      });
+    }
+    else funcMap[target[domElProperty]](target);
+  })
+}
+// const ul = document.querySelector('ul');
+// const eventType = 'click';
+// const domElProperty = 'tagName';
+
+// const funcMap = {
+//   IMG: (target) => { log.innerHTML = "clicked" + target.src },
+//   LI: (target) => { log.innerHTML = 'clicked' + target.firstChild.src }
+// }
+
+// delegate(ul, eventType, domElProperty, funcMap)
+
+export { $, delegate }
