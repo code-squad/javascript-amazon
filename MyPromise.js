92f7845915f4fa60d411932d8ba30a3084fcb199
papico <ocipap0531@gmail.com>
2019-07-05 16:45:46 +0900

    feat: 기본 코드 구조 작성

---
+++ MyPromise.js
@@ -0,0 +1,23 @@
+class MyPromise {
+  constructor() {
+    
+  }
+  static resolve() {
+    
+  }
+  static reject() {
+    
+  }
+  static all() {
+    
+  }
+  static race() {
+    
+  }
+  then() {
+    
+  }
+  catch() {
+    
+  }
+}
