2585f6ecdde7bcca1a9dc4394c8b35610da235df
P-iknow <apricotsoul@gmail.com>
2019-07-08 07:45:12 +0900

    feat: app.js 엔트리 포인트 작성
    - stateUpdater를 StateManager 클래스의 스테틱 메소드로 추가, 이를
    명시적으로 보이기 위해 entry 포인트에 작성

--- step14/app.js
+++ step14/app.js
@@ -2,7 +2,10 @@
 /*eslint-disable */
 
 import config from './src/js/config.js';
-import Carousel from './src/js/carousel.js';
+import stateUpdater from './src/js/stateUpdater.js';
+import StateManager from './src/js/StateManager.js';
+import Main from './src/component/Main/Main.js';
+import Nav from './src/component/Nav/Nav.js';
 import carouselTemplate from './src/component/carousel.js';
 import renderHTML from './src/js/render.js';
 
@@ -12,7 +15,16 @@ window.addEventListener('DOMContentLoaded', () => {
     url: 'http://127.0.0.1:5500/data.json',
     templateFunc: carouselTemplate,
   }).then(_ => {
-    const carousel = new Carousel(config);
-    carousel.init();
+    StateManager.getUpdatedStateFrom = stateUpdater;
+    const stateManager = new StateManager();
+    const main = new Main(config);
+    const nav = new Nav(config);
+
+    stateManager.addObserver(main, nav);
+    main.addSubjet(stateManager);
+    nav.addSubjet(stateManager);
+
+    main.init();
+    nav.init();
   });
 });
