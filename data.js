df8607db293343c82a07667901ae760f30b80c25
kaku <gizmo7duck@gmail.com>
2019-07-08 16:48:26 +0900

    Step11-2 아마존 캐러셀 완료 (#60)
    * Complete Step11-2 HTML, CSS
    
    * carousel comlete
    
    * CHANNY/kaku Step 11-2 Complete
    
    * 피드백 반영

--- data.js
+++ data.js
@@ -1,4 +1,16 @@
 const data = [{
+    img:'./images/card4.png',
+    title:'Over 2 million songs ad free',
+    text: `<li>Save on groceries and get an additiona <span>10% off
+    select sale items</span>in all US Whole Food Market stores
+    </li>
+    <li><span>Save 20%</span> off diapers, baby food, and more </li>
+    <li><span>Earn 5% back</span> at Amazon.com with the Amazon
+    Prime Rewards Visa Card</li>
+    <li>Get exclusive <span>deals and discounts</span> and 30-minute 
+    early access to Lightning Deals</li>`
+}
+    ,{
     img: './images/card1.png',
     title : "Fast, Free Delivery",
     text : `<li>Save on groceries and get an additiona <span>10% off
@@ -14,34 +26,45 @@ const data = [{
     title:'Exclusive deals and offers',
     text:`<li>Save on groceries and get an additiona <span>10% off
     select sale items</span>in all US Whole Food Market stores
-</li>
-<li><span>Save 20%</span> off diapers, baby food, and more </li>
-<li><span>Earn 5% back</span> at Amazon.com with the Amazon
-Prime Rewards Visa Card</li>
-<li>Get exclusive <span>deals and discounts</span> and 30-minute 
-early access to Lightning Deals</li>`
+    </li>
+    <li><span>Save 20%</span> off diapers, baby food, and more </li>
+    <li><span>Earn 5% back</span> at Amazon.com with the Amazon
+    Prime Rewards Visa Card</li>
+    <li>Get exclusive <span>deals and discounts</span> and 30-minute 
+    early access to Lightning Deals</li>`
 },{
     img:'./images/card3.png',
     title:'Prime Originals, movies and TV shows',
     text: `<li>Save on groceries and get an additiona <span>10% off
     select sale items</span>in all US Whole Food Market stores
-</li>
-<li><span>Save 20%</span> off diapers, baby food, and more </li>
-<li><span>Earn 5% back</span> at Amazon.com with the Amazon
-Prime Rewards Visa Card</li>
-<li>Get exclusive <span>deals and discounts</span> and 30-minute 
-early access to Lightning Deals</li>`
+    </li>
+    <li><span>Save 20%</span> off diapers, baby food, and more </li>
+    <li><span>Earn 5% back</span> at Amazon.com with the Amazon
+    Prime Rewards Visa Card</li>
+    <li>Get exclusive <span>deals and discounts</span> and 30-minute 
+    early access to Lightning Deals</li>`
 },{
     img:'./images/card4.png',
     title:'Over 2 million songs ad free',
     text: `<li>Save on groceries and get an additiona <span>10% off
     select sale items</span>in all US Whole Food Market stores
-</li>
-<li><span>Save 20%</span> off diapers, baby food, and more </li>
-<li><span>Earn 5% back</span> at Amazon.com with the Amazon
-Prime Rewards Visa Card</li>
-<li>Get exclusive <span>deals and discounts</span> and 30-minute 
-early access to Lightning Deals</li>`
+    </li>
+    <li><span>Save 20%</span> off diapers, baby food, and more </li>
+    <li><span>Earn 5% back</span> at Amazon.com with the Amazon
+    Prime Rewards Visa Card</li>
+    <li>Get exclusive <span>deals and discounts</span> and 30-minute 
+    early access to Lightning Deals</li>`
+}, {
+    img: './images/card1.png',
+    title : "Fast, Free Delivery",
+    text : `<li>Save on groceries and get an additiona <span>10% off
+        select sale items</span>in all US Whole Food Market stores
+    </li>
+    <li><span>Save 20%</span> off diapers, baby food, and more </li>
+    <li><span>Earn 5% back</span> at Amazon.com with the Amazon
+    Prime Rewards Visa Card</li>
+    <li>Get exclusive <span>deals and discounts</span> and 30-minute 
+    early access to Lightning Deals</li>`
 }]
 
 export default data
--- script-1.js
+++ script-1.js
@@ -1,61 +1,99 @@
 import data from './data.js';
 
 
-class MainContent {
+class Carousel {
     constructor(data) {
-        this.data = data;
-        this.position = 0;
+        this.htmlContent = data;
+        // this.position = 0;
         this.wrapper = document.querySelector('.content-wrapper');
         this.arrows = document.querySelectorAll('.right-arrow, .left-arrow');
         this.navBar = document.querySelector('.navigation-bar');
-        this.item = document.querySelector('.content-section')
+        this.item = document.querySelector('.content-section');
+        this.menuBar = Array.from(this.navBar.children)             
     }
-    // wrapper 상위 엘리먼트 불러오기
-    // 컨텐츠 엘리먼트 가로, 세로 넓이 불러오기
-    // css 이동 속성 만들기 (transform, translate: 이동)
+    
     init() {
-        this.generateElement(this.data);
+        this.render(this.htmlContent);
         this.registerEvent();
+        this.mainContent = document.querySelector('.main-content');
+        this.contentWidth = this.mainContent.offsetWidth;    
+        this.fullWidth = this.contentWidth * (this.htmlContent.length - 1);
+        this.position = -this.contentWidth;   
+        this.wrapper.style.transform = `translate3d(${this.position}px, 0, 0)`;
     }
-    generateElement(data) {
-        for (let i = 0; i < data.length; i++) {
+
+    render (data) {
+        data.forEach((el, i) => {
             let templete = `
                 <div class="main-content" data-index="${i}">
                 <div class="content-img">
-                    <img src="${data[i].img}" alt="">
+                    <img src="${el.img}" alt="">
                 </div>
                 <div class="text-content">
-                    <p>${data[i].title}</p>
+                    <p>${el.title}</p>
                     <ul>
-                        ${data[i].text}
+                        ${el.text}
                     </ul>
                 </div>
                 `;
             this.wrapper.insertAdjacentHTML('beforeend', templete)
+        })
+    }
+
+    registerEvent () {
+        const [leftArrow, rightArrow] = this.arrows
+        leftArrow.addEventListener('click', this.carouselPrev.bind(this))
+        rightArrow.addEventListener('click', this.carouselNext.bind(this))
+        this.navBar.addEventListener('click', this.carouselNavbar.bind(this))
+        this.wrapper.addEventListener('transitionend', this.moveWithoutTransition.bind(this))
+        this.wrapper.addEventListener('transitionend', this.scaleNavbar.bind(this))
+    }
+
+    carouselPrev () {
+        if (this.position < 0) {
+            this.position += this.contentWidth;
+            this.move()
         }
     }
 
-    registerEvent() {
-        this.arrows[1].addEventListener('click', this.carouselArrow.bind(this))
-        this.arrows[0].addEventListener('click', this.carouselArrow.bind(this))
-        this.navBar.addEventListener('click', this.carouselNavbar.bind(this))
+    carouselNext () {
+        if(this.position > -this.fullWidth)
+        this.position -= this.contentWidth;
+        this.move();
     }
 
+    move() {
+        this.wrapper.style.transition = '400ms'
+        this.wrapper.style.transform = `translate3d(${this.position}px, 0, 0)`;
+    }
 
-    carouselArrow (event) {
-        const arrow = event.target.parentNode.className[0]
-        if ((this.position < 0 && arrow === 'l') || (this.position > -2700 && arrow === 'r')) {
-            arrow === 'r' ? this.position -= 900 : this.position += 900
+    moveWithoutTransition() {
+        if (this.position === 0) {
+            this.position = -this.contentWidth * 4
+            this.wrapper.style.transition = ''
+            this.wrapper.style.transform = `translate3d(${this.position}px, 0, 0)`;
+        } else if ( this.position === -this.contentWidth * (data.length -1)) {
+            this.position = -this.contentWidth * 1
+            this.wrapper.style.transition = ''
             this.wrapper.style.transform = `translate3d(${this.position}px, 0, 0)`;
         }
     }
 
-    carouselNavbar(event) {
-        const menuIndex = event.target.getAttribute('data-index')
-        this.position = -900 * menuIndex;
-        this.wrapper.style.transform = `translate3d(${this.position}px, 0, 0)`;
+    scaleNavbar () {
+        const contentIndex = -this.position/this.contentWidth - 1
+        this.menuBar.forEach(item => {
+            item.classList.remove('scale')
+        })
+        this.menuBar[contentIndex].classList.add('scale')
     }
-}
 
-const content = new MainContent(data)
+    carouselNavbar(event) {
+        const menuIndex = event.target.dataset.index
+        console.log(event.target)
+        this.position = -this.contentWidth * menuIndex;
+        this.move()
+        }
+    }
+
+const content = new Carousel(data)
 content.init();
