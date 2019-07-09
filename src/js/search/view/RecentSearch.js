dafab90eaa1e39ec6686a9ce0d1477029fa6f9ca
park78951 <park78951@gmail.com>
2019-07-09 21:25:21 +0900

    Separate folders with files created.
    Create files and sepearate them with right folders after first design
    for step15-1

---
+++ src/js/search/view/RecentSearch.js
@@ -0,0 +1,5 @@
+import recentTemplate from './template/Template';
+
+export default class recentSearch {
+
+}
---
+++ src/js/search/view/template/Template.js
@@ -0,0 +1,15 @@
+export default searchTemplate = {
+  createListContainer() {
+    return `
+        <ul class="search__searcheable-list">
+        
+        </ul>
+    `
+  },
+
+  createSearcheableList(recentSearch) {
+    return `
+      <li class="search__searcheable-list--item">${recentSearch}</li>
+    `
+  }
+}
