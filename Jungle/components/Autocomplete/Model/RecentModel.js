848322c686ce45a15c41e4310ee0c1c4548a6fea
fed-gren <deveb22@gmail.com>
2019-07-09 10:52:41 +0900

    feat: RecentModel, test code
    - 최근 검색어 데이터 저장소인 RecentModel 작성

---
+++ Jungle/components/Autocomplete/Model/RecentModel.js
@@ -0,0 +1,18 @@
+import MyEventEmitter from "../../../../MyEventEmitter/index.js";
+
+export default class RecentModel extends MyEventEmitter {
+  constructor({maxLen}) {
+    super();
+
+    this.queue = [];
+    this.maxLen = maxLen;
+  }
+
+  push(data) {
+    while(this.queue.length >= this.maxLen) {
+      this.queue.shift();
+    }
+
+    this.queue.push(data);
+  }
+}
