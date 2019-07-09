4ed510d5e46680388d317c8679c47b869c2ac19d
sunwook kim <koownusmik@gmail.com>
2019-07-09 20:04:00 +0900

    refactor: Controller
    Controller
    1. App.js에서 받던 데이터를 init에서 받도록 수정
    이때 async를 사용하여 then없이 구현함
    
    2. registerEvents 함수안에 navList 변수 추가하여 디버깅 가능하게 함.
    
    3.init안에서 data의 length를 받아서 넘겨주도록함. constructor에서 this.countOfCards를 미리 등록하였음.
    
    4. handler 함수안에 deconstructing을 사용하여 direction을 깔끔하게 받도록 수정
    이때 preventDefault()는 deconstructing시 this 바인딩문제가 생겨 event로 실행함.
    
    5. moveIndex, makeTemplate 와 같은 함수이름을 가독성 좋게 수정함.
    함수의 역할을 최대한 분리하고자 하였음.

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import Carousel from './Carousel.js';
-import Navigation from './Naviagtion.js';
-import Controller from './Controller.js';
+import CarouselView from "./CarouselView.js";
+import NavigationView from "./NaviagtionView.js";
+import Controller from "./Controller.js";
 const leftBtn = document.querySelector(".contents__button_left");
 const rightBtn = document.querySelector(".contents__button_right");
 const cardList = document.querySelector(".carousel__contents");
 const navbar = document.querySelector(".carousel__title");
 const animationTime = 200;
-fetch("./localData.json")
-.then(response=>response.json())
-.then((contentsData)=>{
-    const carousel = new Carousel({ leftBtn, rightBtn, cardList, animationTime,contentsData  });
-    const navigation = new Navigation({ navbar,contentsData  });
-    const amazonController = new Controller({ carousel, navigation ,animationTime});
-    
-    amazonController.init();
-})
+const dataURL = "./localData.json";
 
-
-
-
-
+const carouselView = new CarouselView({
+  leftBtn,
+  rightBtn,
+  cardList,
+  animationTime
+});
+const navigationView = new NavigationView({ navbar });
+const amazonController = new Controller({
+  carouselView,
+  navigationView,
+  animationTime
+});
+amazonController.init(dataURL);
---
+++ NaviagtionView.js
@@ -0,0 +1,30 @@
+class NavigationView {
+  constructor({ navbar }) {
+    this.navbar = navbar;
+  }
+  render(contentsData) {
+    const contentsTemplate = contentsData.reduce((contentsTemplate, card) => {
+      const liTemplate = `
+        <li class="title__card title__card_${card.color}">
+        ${card.title}
+        </li>
+        `;
+      return (contentsTemplate += liTemplate);
+    }, `<ol>`);
+    this.navbar.insertAdjacentHTML(
+      "afterbegin",
+      contentsTemplate.concat("</ol>")
+    );
+  }
+
+  drawCurrentNavItem(currentIndex, previousIndex = undefined) {
+    const navList = this.navbar.firstElementChild.children;
+    let previousItem = navList[previousIndex];
+    let currentItem = navList[currentIndex];
+
+    if (previousIndex !== undefined) previousItem.classList.toggle("curser");
+    currentItem.classList.toggle("curser");
+  }
+}
+
+export default NavigationView;
