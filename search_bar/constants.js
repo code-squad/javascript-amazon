4ffb44fc165d757550d9c4f35e9ebb733f2a8f45
allen <revenoir7@gmail.com>
2019-07-10 13:16:05 +0900

    feat: 상수값을 저장할 constant.js 작성

---
+++ search_bar/constants.js
@@ -0,0 +1,9 @@
+const COMPONENTS_NAME = {
+  AUTO_COMPLETE: 'autoComplete',
+  RECENT_KEYWORDS: 'recentKeywords',
+  SEARCH: 'search'
+}
+
+const AUTO_COMPLETE_DELAY = 300;
+
+export { COMPONENTS_NAME, AUTO_COMPLETE_DELAY }
