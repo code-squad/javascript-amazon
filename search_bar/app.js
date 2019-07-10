eb8b6133d902e67e89b4340acf9f8b42299bbfe0
allen <revenoir7@gmail.com>
2019-07-10 15:26:23 +0900

    feat: Search.addKeydownEvent메서드 작성

--- search_bar/app.js
+++ search_bar/app.js
@@ -4,7 +4,9 @@ import RecentKeywords from './components/recent_keywords.js'
 import Search from './components/search.js'
 import StateManager from './components/state_manager.js'
 
+const input = 'input'; //TODO: className 부여해서 별도 설정파일로 분리
+
 const stateManager = new StateManager();
-const search = new Search(stateManager);
+const search = new Search(stateManager, input);
 const recentKeywords = new RecentKeywords(stateManager);
 const autoComplete = new AutoComplete(stateManager);
--- search_bar/components/search.js
+++ search_bar/components/search.js
@@ -3,13 +3,22 @@ import Subscriber from '../../utils/Subscriber.js'
 import { COMPONENTS_NAME } from '../constants.js'
 
 class Search extends Subscriber {
-  constructor(publisher) {
+  constructor(publisher, selector) {
     super();
     this.init(publisher);
+    this.addKeydownEvent(selector)
   }
 
   init(publisher) {
     this.subscribe(COMPONENTS_NAME.SEARCH, publisher);
+  }
+
+  addKeydownEvent(selector) {
+    _.on(_.$(selector), 'keydown', this.handleKeydown.bind(this));
+  }
+
+  handleKeydown({ target }) {
+    this.publisher.setState({ currentValue: target.value });
   }
 
 }
