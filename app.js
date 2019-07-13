27c4fb27bf471086ffbb6e3dece03cfd38e5592f
fed-gren <deveb22@gmail.com>
2019-07-13 16:34:58 +0900

    refactor: change file name
    - 사용하지 않는 코드 제거

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import Carousel from "./Jungle/components/Carousel/index.js";
-import Navigation from "./Jungle/components/Navigation/index.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   //DOM
   const carousel_div = document.querySelector(".carousel");
-  const navigation_div = document.querySelector(".navigation");
 
   new Carousel({
     carouselElement: carousel_div,
