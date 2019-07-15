7a882490a1dd6e13b7b5792fdc787a1e35c594fc
park78951 <park78951@gmail.com>
2019-07-15 19:52:30 +0900

    Add function to select list in autocomplete list by key press.
    1. add mehthod in autocompleteView.js
       - add properties to store changed stutus.
       - add focusItem method to manage all selection view in autocomplete list.
       - add firstFocus method to select at first when nothing has not been
       selected.
       - add moveFocus method to move selection to another in list.
       - add focusToggle method to add or remove class to highlight on one
       of list
       - add resetFocus and clearAutocomList methods to be reset the
       properties to select list.
    2. add methods in controlSearch.js
       - add setTimeout in removeAutocomList method to delete autocomplete
       list after made with setTimeout in order.
       - add selectAutocomList to controll selecting list.
          - use preventdefault to prevent cursor in input focused from being
          moved with kepressed by up and down arrows.
       - addEventHanler for the selectAutocomList method.
    3. add ul tag in html to replace template literal and style in css for
    the function to select autocomplete list.

--- src/js/search/view/Autocomplete.js
+++ src/js/search/view/Autocomplete.js
@@ -5,6 +5,10 @@ export default class Autocomplete {
   constructor() {
     this.inputBox = document.querySelector('.input__box');
     this.autocomContainer = this.inputBox.querySelector('.search__searcheable-list');
+    this.autocomItems = null;
+    this.autocomItemsLen = null;
+    this.curItemIdx = null;
+    this.focusedDom = null;
   }
 
   attachAutocomList(words, inputVal, numOfList) {
@@ -15,7 +19,7 @@ export default class Autocomplete {
       const accumulatedTemplate = acc+template.createSearcheableList(cur);
       return accumulatedTemplate
     }, '');
-    ut.appendElLastly(this.autocomContainer, autocomList);
+    this.delayedListRenderer(autocomList, 300);
   }
 
   emphasizeLetters(word, emLetter) {
@@ -25,10 +29,71 @@ export default class Autocomplete {
   
     splitWord.splice(matchedIdx, letterLen, template.createBoldLetter(emLetter));
     return splitWord.join('');
-
   }
   
   dettachAutocomList() {
+    this.autocomContainer.classList.add('hide');
     this.autocomContainer.innerHTML = ''
+    this.resetFocus();
+    this.clearAutocomList();
+  }
+  
+  delayedListRenderer(template, delayedTime) {
+    setTimeout(() => {
+      console.log(this.autocomItems);
+      ut.appendElLastly(this.autocomContainer, template);
+      this.autocomContainer.classList.remove('hide');
+      this.autocomItems = this.autocomContainer.children;
+    }, delayedTime);
+  }
+
+  focusItem({keyCode}) {
+    if (!this.autocomItems) return;
+    this.autocomItemsLen = this.autocomItems.length;
+    if(this.focusedDom === null) {
+      this.firstFocus(keyCode);
+    } else {
+      this.moveFocus(keyCode);
+    }
+  }
+
+  firstFocus(keyCode) {
+    const firstItemIdx = 0,
+          lastItemIdx = this.autocomItemsLen - 1;
+    if(keyCode === 40) {
+      this.focusedDom = this.autocomItems[firstItemIdx];
+      this.curItemIdx = 0;
+    } else {
+      this.focusedDom = this.autocomItems[lastItemIdx];
+      this.curItemIdx = this.autocomItemsLen-1;
+    }
+    this.focusToggle(this.focusedDom);
+  }
+
+  moveFocus(keyCode) {
+    this.focusToggle(this.focusedDom);
+    let nextItemIdx;
+    keyCode === 40 ? nextItemIdx = this.curItemIdx + 1 : nextItemIdx = this.curItemIdx - 1;
+    if(nextItemIdx < 0 || nextItemIdx >= this.autocomItemsLen) {
+      this.resetFocus();
+      return;
+    }
+    this.focusedDom = this.autocomItems[nextItemIdx];
+    this.curItemIdx = nextItemIdx;
+    this.focusToggle(this.focusedDom);
+  }
+
+  focusToggle(targetElem) {
+    targetElem.classList.toggle('focus');
+  }
+
+  resetFocus() {
+    this.focusedDom = null;
+    this.curItemIdx = null;
+  }
+  
+  clearAutocomList() {
+    this.autocomItems = null;
+    this.autocomItemslen = null;
   }
 }
