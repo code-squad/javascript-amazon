df7757e972ca219aad2dc299fa563db3799968d0
park78951 <park78951@gmail.com>
2019-07-15 19:52:30 +0900

    Add function to select list in autocomplete list by key press.
    1. add mehthod in autocompleteView.js
       - add properties to store changed stutus.
       - add focusItem method to manage all selection view in autocomplete list.
       - add firstFocus method to select at first when nothing has not been
       selected.
       - add moveFocus method to move selection to another in list.
       - add focusToggle method to add or remove class to highlight on one
       of list
       - add resetFocus and clearAutocomList methods to be reset the
       properties to select list.
    2. add methods in controlSearch.js
       - add setTimeout in removeAutocomList method to delete autocomplete
       list after made with setTimeout in order.
       - add selectAutocomList to controll selecting list.
          - use preventdefault to prevent cursor in input focused from being
          moved with kepressed by up and down arrows.
       - addEventHanler for the selectAutocomList method.
    3. add ul tag in html to replace template literal and style in css for
    the function to select autocomplete list.

--- src/js/search/control/controlSearch.js
+++ src/js/search/control/controlSearch.js
@@ -5,11 +5,12 @@ export default class ControlSearch {
     this.autocompleteView = autocompleteView;
     this.numOfList = numOfList;
     this.autocomHandler = this.autocomHandler.bind(this);
+    this.selectAutocomList = this.selectAutocomList.bind(this);
     // this.getAutocomVal = this.getAutocomVal.bind(this);
     // this.showAutocomList = this.showAutocomList.bind(this);
   }
   /*
-    TODO: 1. Refactoring하기.
+    TODO: 1. Refactoring하기. - 
           2. 최근 검색 List 구현하기.
           3. 자동완성 data JSON으로 만들어서 Model에 전달.
           4. li에 'tabindex="1"' 속성 사용 / nextsibling
@@ -18,11 +19,12 @@ export default class ControlSearch {
   eventHandler() {
     const searchBar = this.searchBarView.getSearchBarDom();
     searchBar.addEventListener('input', this.autocomHandler);
+    searchBar.addEventListener('keydown', this.selectAutocomList)
   }
 
   autocomHandler(e) {
     if(!e.srcElement.value) {
-      this.removeAutocomList();
+        this.removeAutocomList();
       return;
     }
     this.storeCurrentInput(e);
@@ -42,10 +44,26 @@ export default class ControlSearch {
   showAutocomList(values, inputVal) {
     this.removeAutocomList();
     // this.numOflist option으로 받도록 설정 (리스트 갯수 설정)
-    this.autocompleteView.attachAutocomList(values, inputVal, this.numOfList);
+    if(values.length) this.autocompleteView.attachAutocomList(values, inputVal, this.numOfList);
   }
   
   removeAutocomList() {
-    this.autocompleteView.dettachAutocomList();
+    setTimeout( () => {
+      this.autocompleteView.dettachAutocomList();
+    },300)
   }
+
+  selectAutocomList(e) {
+    if(e.keyCode === 38 || e.keyCode === 40) {
+      e.preventDefault();
+      this.autocompleteView.focusItem(e);
+    }
+  }
+  
+  // selectAutocomList(e) {
+    
+  //   if(e.keyCode === 38 || e.keyCode === 40) {
+  //     e.preventDefault();
+  //   }
+  // }
 }
