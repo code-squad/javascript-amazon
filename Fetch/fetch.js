a36ebefb25ae073fcf86de53c2c34e8c8e44bc06
Snowjang <asdka4618@gmail.com>
2019-07-17 21:54:08 +0900

    feat: fetch API를 이용한 local json 데이터 가져오기
    fetch를 이용하여 local에 있는 json데이터파일을 가져와
    웹 페이지에 뿌려봄

---
+++ Fetch/fetch.js
@@ -0,0 +1,30 @@
+const dataURL = "./localData.json";
+const targetId = "1c3d42";
+
+const findByTargetId = data => data.find(item => item._id === targetId);
+const render = items => {
+  return items.map(item => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h2>${item.title}</h2>
+        <p>${item.desc}</p>
+        <img src="${item.imgName}"/>
+      `;
+    return li;
+  });
+};
+
+fetch(dataURL)
+  .then(response => {
+    if (response.status !== 200) {
+      console.log(`Status code: ${response.status}`);
+      return;
+    }
+    return response.json();
+  })
+  .then(data => {
+    const carouselData = findByTargetId(data);
+    const listItems = render(carouselData.list);
+    const ul = document.querySelector(".carousel");
+    return listItems.forEach(li => ul.appendChild(li));
+  });
