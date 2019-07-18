50655e6f42d19e6c9e604f24df592dc60e1640c3
sunwook kim <koownusmik@gmail.com>
2019-07-18 16:46:43 +0900

    feat: sleep 함수 구현

--- Search/SearchController.js
+++ Search/SearchController.js
@@ -10,16 +10,17 @@ class SearchController {
   }
 
   registerEvents(){
-    // input 이벤트가 발생할때 this.inputViewKeyDownHandler 이런 핸들러함수가 일어날것이다~
-    //// keydown 말고 input으로 해야함 
-    this.inputView.div.addEventListener('input',({target:{value}})=>this.inputViewKeyDownHandler(value));
+    this.inputView.ul.addEventListener('input',({target:{value}})=>this.inputViewKeyDownHandler(value));
+    // console.log("inputView 실제영역",this.inputView.div);
+  }
+
+  // util 함수 
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   async inputViewKeyDownHandler(value){
-    // input 이벤트가 일어난곳에서 e.target.value 로 데이터를 받고 받은 인자를 모델에게 전달한다. 
-    //// 확인을 해볼수가 없으니 inputView , Model 만들고 연결도 해야함! 
-    console.log(value);
-    //모델에게 value 보내고 , matchedData를 받아오고 , matchedView에 보내준다. 
+    await this.sleep(300);
     const matchedData = await this.searchModel.find(value);
     await this.matchedView.render(matchedData);
     
