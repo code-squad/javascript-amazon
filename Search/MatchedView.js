136458020e7463c1afd8ea8953af0faf593843b0
sunwook kim <koownusmik@gmail.com>
2019-07-18 17:20:46 +0900

    refactor: render 함수
    fast-return 으로  있을때 삭제하도록하고,
    
    반복되는 템플릿추가와 insert 로직을 조건문에서 분리

--- Search/MatchedView.js
+++ Search/MatchedView.js
@@ -4,29 +4,23 @@ class MatchedView {
     this.liTemplates = undefined;
   }
 
-  // 이전에 입력이 없었으면 추가하고, 있으면 교체
   render(data) {
     const {
       body: { suggestions }
     } = data;
-    console.log("suggestions", suggestions);
+    // console.log("요청의종류", data.body.responseId);
     this.ul.style.display = "block";
-    if (this.liTemplates === undefined) {
-      this.liTemplates = suggestions.reduce((accum, cur) => {
-        accum += `<li class="suggestions">${cur.value}</li>`;
-        return accum;
-      }, "");
-      this.ul.insertAdjacentHTML("afterbegin", this.liTemplates);
-    }
-    while(this.ul.firstElementChild){
-      this.ul.removeChild(this.ul.firstElementChild);
+
+    if (this.liTemplates) {
+      while (this.ul.firstElementChild) {
+        this.ul.removeChild(this.ul.firstElementChild);
+      }
     }
     this.liTemplates = suggestions.reduce((accum, cur) => {
       accum += `<li class="suggestions">${cur.value}</li>`;
       return accum;
     }, "");
     this.ul.insertAdjacentHTML("afterbegin", this.liTemplates);
-
   }
 }
 
