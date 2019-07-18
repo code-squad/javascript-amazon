1fe3394f09dba8d174283d25248936bb82c9a99a
sunwook kim <koownusmik@gmail.com>
2019-07-18 17:06:35 +0900

    feat: 자동완성시 mvp2 구현
    li template 가 계속 추가되지 않고 없으면만들고, 있으면 교체하는 로직으로 수정

--- Search/MatchedView.js
+++ Search/MatchedView.js
@@ -1,18 +1,32 @@
 class MatchedView {
-  constructor({matchedUl}){
-    this.ul = document.querySelector(matchedUl)
+  constructor({ matchedUl }) {
+    this.ul = document.querySelector(matchedUl);
+    this.liTemplates = undefined;
   }
-  
-  render(data){
-    const {body:{suggestions}} = data
-    console.log("suggestions",suggestions);
-    this.ul.style.display = 'block'
-    const liTemplates = suggestions.reduce((accum,cur)=>{
-      accum += `<li class="suggestions">${cur.value}</li>`
-      return accum
-    },'')
-    this.ul.insertAdjacentHTML("afterbegin",liTemplates)
-     
+
+  // 이전에 입력이 없었으면 추가하고, 있으면 교체
+  render(data) {
+    const {
+      body: { suggestions }
+    } = data;
+    console.log("suggestions", suggestions);
+    this.ul.style.display = "block";
+    if (this.liTemplates === undefined) {
+      this.liTemplates = suggestions.reduce((accum, cur) => {
+        accum += `<li class="suggestions">${cur.value}</li>`;
+        return accum;
+      }, "");
+      this.ul.insertAdjacentHTML("afterbegin", this.liTemplates);
+    }
+    while(this.ul.firstElementChild){
+      this.ul.removeChild(this.ul.firstElementChild);
+    }
+    this.liTemplates = suggestions.reduce((accum, cur) => {
+      accum += `<li class="suggestions">${cur.value}</li>`;
+      return accum;
+    }, "");
+    this.ul.insertAdjacentHTML("afterbegin", this.liTemplates);
+
   }
 }
 
--- Search/SearchController.js
+++ Search/SearchController.js
@@ -1,29 +1,30 @@
 class SearchController {
-  constructor({inputView , matchedView , searchModel}){
+  constructor({ inputView, matchedView, searchModel }) {
     this.inputView = inputView;
     this.matchedView = matchedView;
     this.searchModel = searchModel;
   }
-  
-  init(){
+
+  init() {
     this.registerEvents();
   }
 
-  registerEvents(){
-    this.inputView.ul.addEventListener('input',({target:{value}})=>this.inputViewKeyDownHandler(value));
+  registerEvents() {
+    this.inputView.ul.addEventListener("input", ({ target: { value } }) =>
+      this.inputViewKeyDownHandler(value)
+    );
     // console.log("inputView 실제영역",this.inputView.div);
   }
 
-  // util 함수 
+  // util 함수
   sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  async inputViewKeyDownHandler(value){
+  async inputViewKeyDownHandler(value) {
     await this.sleep(300);
     const matchedData = await this.searchModel.find(value);
     await this.matchedView.render(matchedData);
-    
   }
 }
 
