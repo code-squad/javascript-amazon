4b8c4a2816bbfb2b05611aecbfe503603e190fd0
sunwook kim <koownusmik@gmail.com>
2019-07-22 10:06:01 +0900

    하이라이트 기능 mvp 구현
    replace를 이용해 새로운 태그를 추가함.
    
    그런데 이때 bold를 css로 제어해야하는 이유를 모르겠음. 그냥 태그만 추가해도 원래는 bold가 추가되어야하지 않나?

--- Search/SearchController.js
+++ Search/SearchController.js
@@ -20,41 +20,34 @@ class SearchController {
     );
   }
 
-  async inputViewInputHandler(value) {
+  async inputViewInputHandler(inputValue) {
     await sleep(300);
-    const matchedData = await this.searchModel.find(value);
+    const matchedData = await this.searchModel.find(inputValue);
     console.log("matchedData", matchedData);
-    await this.matchedView.render(matchedData);
+    await this.matchedView.render(matchedData,inputValue);
   }
 
   inputViewKeyDownHandler(code) {
-
-    if(["ArrowDown","ArrowUp","Enter"].includes(code)){
+    if (["ArrowDown", "ArrowUp", "Enter"].includes(code)) {
       const lists = this.matchedView.ul.querySelectorAll("li");
       if (code === "ArrowDown") {
         this.matchedView.curserIndex += 1;
-         
-        if(this.matchedView.curserIndex === lists.length ) this.matchedView.curserIndex = 0;
-        
-        lists.forEach(list=>list.classList.remove("activated"))
+
+        if (this.matchedView.curserIndex === lists.length)
+          this.matchedView.curserIndex = 0;
+        lists.forEach(list => list.classList.remove("activated"));
         lists[this.matchedView.curserIndex].classList.add("activated");
-
-
-
-
       }
-  
+
       if (code === "ArrowUp") {
         this.matchedView.curserIndex -= 1;
-        if(this.matchedView.curserIndex === -1 ) this.matchedView.curserIndex = lists.length-1;
-        lists.forEach(list=>list.classList.remove("activated"))
+        if (this.matchedView.curserIndex === -1)
+          this.matchedView.curserIndex = lists.length - 1;
+        lists.forEach(list => list.classList.remove("activated"));
         lists[this.matchedView.curserIndex].classList.add("activated");
         // this.matchedView.curserIndex--;
-
       }
     }
-
-
   }
 }
 
