6c8f19aded844ff6e50666389d4dd2a98041e07f
P-iknow <apricotsoul@gmail.com>
2019-07-22 18:36:24 +0900

    doc: 자동완성 미션에 관한 문서 정리
    - 기타 파일구조 정리

--- step15/app.js
+++ step15/app.js
@@ -1,7 +1,7 @@
-import InputView from './InputView.js';
-import ResultView from './ResultView.js';
-import Controller from './Controller.js';
-import Model from './Model.js';
+import InputView from './src/js/InputView.js';
+import ResultView from './src/js/ResultView.js';
+import Controller from './src/js/Controller.js';
+import Model from './src/js/Model.js';
 
 const inputView = new InputView();
 const resultView = new ResultView();
