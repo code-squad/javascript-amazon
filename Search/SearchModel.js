7543af0714414724b5e22b78b0760f8de69fdd38
sunwook kim <koownusmik@gmail.com>
2019-07-31 16:00:00 +0900

    refactor: try ~ catch 개선
    404일때 데이터가 없는것이 정상이라고 판단되어 catch에서 처리하지 않음.
    
    나머지 알수없는(?) 에러만 catch에 넣음.

--- Search/SearchModel.js
+++ Search/SearchModel.js
@@ -7,12 +7,12 @@ class SearchModel {
   async find(input) {
     try {
       const response = await fetch(this.fetchURL + input);
-      const fetchedData = await response.json();
-      if(fetchedData.statusCode === 404) throw new Error('404에러:  데이터가 없습니다'); //데이터가 없을때 예외처리
-      return fetchedData;
+      const fetchedData = await response.json(); 
+      if(fetchedData.statusCode === 404) return; // 데이터가 없을때
+      return fetchedData; // 있을때
     } catch(error) {
-      console.warn(error);
-      return;
+      console.warn(error); //알수없는 에러 
+      return; 
     }
   }
 
