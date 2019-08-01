a4d52b156f743dc18a4f3c9513cac4ec42c08030
sunwook kim <koownusmik@gmail.com>
2019-08-01 23:29:28 +0900

    chore: delete magic number

--- Search/HistoryView.js
+++ Search/HistoryView.js
@@ -2,13 +2,14 @@ class HistoryView {
   constructor({ historyUl }) {
     this.ul = document.querySelector(historyUl);
     this.liTemplates = undefined;
+    this.showCount = 5;
   }
+
   render(data) {
     if (data.length === 0) return; // 빈화면 일때 처리
 
     this.initializeTemplate();
-    let historys = data.reverse();
-    historys = historys.slice(0, 5);
+    let historys = data.reverse().slice(0, this.showCount);
     this.liTemplates = historys.reduce((accum, cur) => {
       accum += `<li class="results">${cur}</li>`;
       return accum;
@@ -16,17 +17,17 @@ class HistoryView {
     this.ul.insertAdjacentHTML("afterbegin", this.liTemplates);
     this.ul.classList.add("render");
   }
+
   hide() {
     this.ul.classList.remove("render");
   }
 
   initializeTemplate() {
     if (!this.liTemplates) return;
-  
+
     while (this.ul.firstElementChild) {
       this.ul.removeChild(this.ul.firstElementChild);
     }
-     
   }
 }
 
