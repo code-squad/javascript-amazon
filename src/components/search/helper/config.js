3b168e342ffb76949320a0b3a93cb347b0bb7866
Sangkyu <park78951@gmail.com>
2019-08-05 17:06:58 +0900

    Revise fetch url to heroku

--- src/components/search/helper/config.js
+++ src/components/search/helper/config.js
@@ -1,7 +1,7 @@
 
   export const DELAYED_TIME = 1200;
   export const LIMITED_NUM =  5;
-  export const AUTOCOM_DATA_URL = 'http://localhost:3000/api?query=';
+  export const AUTOCOM_DATA_URL = 'https://ssangq-amazon.herokuapp.com/api?query=';
   export const CLASS_AUTOVIEW = 'search__auto-view';
   export const CLASS_RECENT_SEARCH = 'search__recent-search';
   export const CLASS_AUTOCOMPLETE = 'search__autocomplete';
--- src/components/search/helper/message.js
+++ src/components/search/helper/message.js
@@ -1,2 +1,2 @@
 export const NOT_FOUND = '지원하는 단어가 아닙니다.';
-export const UNEXPECTED_ERROR = '올바른 접근이 아닙니다.'
+export const UNEXPECTED_ERROR = '올바른 접근이 아닙니다.';
