deb886598639c9e57584fc57e5ed5b75312ce1d6
Sangkyu <park78951@gmail.com>
2019-08-05 15:42:28 +0900

    Revise codes
    - revise variable names to deploy on heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "src")));
 app.use('/', router)
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 });
