383e1ebccfd9f2bb477cb74ec2afd4df2dc70fb7
leehwarang <hwrng2@gmail.com>
2019-08-07 22:51:00 +0900

    feat: 검색자동완성의 검색어 fetch 요청에 응답하는 라우팅 처리

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var fs = require("fs");
+var path = require("path");
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,4 +10,19 @@ router.get("/", function(req, res, next) {
   res.render("/index.html");
 });
 
+router.get("/amazon_autocomplete", function(req, res, next) {
+  const searchKeyword = req.query.suggestion;
+  const jsonPath = path.join(
+    __dirname,
+    "..",
+    "public",
+    "data",
+    `${searchKeyword}.json`
+  );
+  fs.readFile(jsonPath, (err, data) => {
+    if (err) throw err;
+    const jsonData = JSON.parse(data);
+    return res.json(jsonData);
+  });
+});
 module.exports = router;
