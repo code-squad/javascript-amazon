ac0119461483f4a69f48d4b46ed703a3bf9d9dab
leehwarang <hwrng2@gmail.com>
2019-08-07 14:51:58 +0900

    setting&feat: express 환경 설정 및 파일 디렉토리 구조 변경

---
+++ public/UI_component/searchUI/autoCompleteView.js
@@ -0,0 +1,32 @@
+import { $ } from "../../javascripts/Utills/mylibrary.js";
+import ModalView from "./modalView.js";
+
+class AutoCompleteView extends ModalView {
+  constructor(inputTag) {
+    super(inputTag);
+  }
+
+  init(func) {
+    super.init(func);
+    this.config.attrName = "autoIndex";
+  }
+
+  makeliTemplate(arr) {
+    const result = arr.reduce((acc, cur) => {
+      acc += `<li>${cur}</li>`;
+      return acc;
+    }, "");
+
+    this.modal.insertAdjacentHTML("beforeend", result);
+  }
+
+  makeModalContent(arr) {
+    this.removeChildAll();
+    this.makeliTemplate(arr);
+    this.showModalWindow();
+    this.setAttribute();
+    this.initHighlight();
+  }
+}
+
+export default AutoCompleteView;
---
+++ public/UI_component/searchUI/recentSearchView.js
@@ -0,0 +1,52 @@
+import { $ } from "../../javascripts/Utills/mylibrary.js";
+import ModalView from "./modalView.js";
+
+class RecentSearchView extends ModalView {
+  constructor(inputTag) {
+    super(inputTag);
+    this.recentKeyword = [];
+  }
+
+  init(func) {
+    super.init(func);
+    this.config.attrName = "recentIndex";
+  }
+
+  makeliTemplate() {
+    const arr = [...this.recentKeyword];
+    let result = "";
+    if (arr.length === 0) {
+      result = `<li>최근 검색어 없음</li>`;
+    } else {
+      result = arr.reduce((acc, cur) => {
+        acc += `<li>${cur}</li>`;
+        return acc;
+      }, "");
+    }
+    this.modal.insertAdjacentHTML("beforeend", result);
+  }
+
+  makeModalContent() {
+    this.removeChildAll();
+    this.makeliTemplate();
+    this.showModalWindow();
+    this.setAttribute();
+    this.initHighlight();
+  }
+
+  saveRecentKeyword(inputValue) {
+    const maxLength = 5;
+
+    if (
+      this.recentKeyword.length === maxLength ||
+      this.recentKeyword.length > maxLength
+    ) {
+      this.recentKeyword.pop();
+    }
+
+    this.recentKeyword.unshift(inputValue);
+    this.notify();
+  }
+}
+
+export default RecentSearchView;
