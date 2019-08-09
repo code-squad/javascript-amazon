620b437c918d7aedd2ef7cf9cc98ad8031bd390d
선욱 <43375739+dxvinci@users.noreply.github.com>
2019-08-09 14:22:46 +0900

    [wook]  step15-3 리뷰 부탁드립니다  (#107)
    * feat : inputView 내부에 debounceInputHandler 구현
    
    * chore: 함수로 idx 변경

--- Search/InputView.js
+++ Search/InputView.js
@@ -1,3 +1,4 @@
+
 class InputView {
   constructor({ input, submitBtn }) {
     this.input = document.querySelector(input);
@@ -5,23 +6,25 @@ class InputView {
     this.inputHandler = null;
     this.keyDownHandler = null;
     this.clickHandler = null;
+    this.timer = null;
     this.initEvents();
   }
 
   initEvents() {
-    this.input.addEventListener("input", ({ target: { value } }) =>
-      this.inputHandler(value)
-    );
-
+    this.input.addEventListener("input", ({ target: { value } }) =>this.debounceInputHandler(value));
     this.input.addEventListener("keydown", e => this.keyDownHandler(e));
-
-    this.btn.addEventListener("click", e => {
-      this.clickHandler(e);
-    });
+    this.btn.addEventListener("click", e => this.clickHandler(e));
   }
 
   render(value) {
     this.input.value = value;
+  }
+
+  debounceInputHandler(value){
+    if(this.timer){
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(()=>{this.inputHandler(value)},300);
   }
 }
 
