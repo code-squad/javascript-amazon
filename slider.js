0151820c065c8c3975e30b1f8c8052e044a42b72
noto01 <noto0131@gmail.com>
2020-02-11 17:58:19 +0900

    [add] html 기본구조 완성 css 기본 스타일 완성 js previous, next 버튼 구현 시작

---
+++ slider.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", event => {
+    const carousel = document.querySelector(".slider__list");
+    const arrayLI = document.querySelectorAll(".slider__item");
+    const imageSize = document.querySelector(".slider__item").offsetWidth;
+    const previousBtn = document.querySelector(".btn__previous");
+    const nextBtn = document.querySelector(".btn__next");
+    let count = 0;
+
+    nextBtn.addEventListener("click", event => {
+        count--;
+        carousel.style.transform = `translateX(${count * imageSize}px)`;
+    });
+
+    previousBtn.addEventListener("click", event => {
+        count++;
+        carousel.style.transform = `translateX(${count * imageSize}px)`;
+    });
+    controlLi()
+});
+
+function controlLi() {
+    const ul = document.querySelector(".slider__list");
+    const firstChild = ul.firstElementChild;
+    const lastChild = ul.lastElementChild;
+    console.log('vv', lastChild);
+}
