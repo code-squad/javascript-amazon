bc092bc32a33baace9204580d7cf26b3b704e461
Rachel4858 <rachel458458@gmail.com>
2020-02-12 00:18:22 +0900

    remove commennt

--- src/CarouselManager.js
+++ src/CarouselManager.js
@@ -1,9 +1,7 @@
 class CarouselManager {
   constructor(props) {
-    // todo: props로 menuButtonElements DOM들을 받았지, 그 DOM들을 돌면서 이벤트리스너를 붙여줄거야.
     this.appendEventListenerToMenuBtn(props.menuButtonElements);
     this.carouselFirstElement = props.carouselFirstElement;
-    // todo: props로 arrowButtonElements DOM들을 받았지, 그 DOM들을 돌면서 이벤트리스너를 붙여줄거야.
   }
 
   appendEventListenerToMenuBtn(elements) {
