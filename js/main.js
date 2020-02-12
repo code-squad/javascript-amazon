1ccfe583f441075b68beb406e7e8cf9a98c389da
sgdg <seungdeng17@gmail.com>
2020-02-12 22:16:21 +0900

    refactor. cardui 미션 제출 전 메서드, 스타일 속성 정리

--- js/main.js
+++ js/main.js
@@ -6,65 +6,41 @@ class Slide {
         this.itemsCount = this.el.items.length - 1;
         this.nextHandler = this.nextHandler.bind(this);
         this.prevHandler = this.prevHandler.bind(this);
-        this.tsEndHandler = this.tsEndHandler.bind(this);
         this.init();
     }
 
     init() {
         navContainer.style.width = `${(OPTION_DATA.slideOption.NAV_CARD_WIDTH * DATA.itemContents.length) + (5 * DATA.itemContents.length)}px`;
-        slideContainer.style.width = `${OPTION_DATA.slideOption.VIEWER_WIDTH * (DATA.itemContents.length + 2)}px`;
-        this.el.wrap.style.transform = `translateX(${-this.VIEWER_WIDTH}px)`;
-        this.makeDummy();
+        slideContainer.style.width = `${OPTION_DATA.slideOption.VIEWER_WIDTH * DATA.itemContents.length}px`;
     }
 
     nextHandler() {
         this.el.next.style.transform = "scale(1)";
-        if (this.curItem === 0 && this.el.wrap.style.transform !== `translateX(${(this.curItem + 1) * -this.VIEWER_WIDTH}px)`) return;
         if (this.curItem === this.itemsCount) {
-            this.tsChecker();
             this.curItem = 0;
             this.navScaleCtl(this.curItem, this.itemsCount);
-            const x = (this.itemsCount + 2) * -this.VIEWER_WIDTH;
+            const x = this.curItem * -this.VIEWER_WIDTH;
             this.el.wrap.style.transform = `translateX(${x + "px"})`;
-            this.el.wrap.addEventListener("transitionend", this.tsEndHandler);
         } else {
-            this.tsChecker();
             this.curItem++;
             this.navScaleCtl(this.curItem, this.curItem - 1);
-            const x = (this.curItem * -this.VIEWER_WIDTH) - this.VIEWER_WIDTH;
+            const x = this.curItem * -this.VIEWER_WIDTH;
             this.el.wrap.style.transform = `translateX(${x + "px"})`;
         }
     }
 
     prevHandler() {
         this.el.prev.style.transform = "scale(1)";
-        if (this.curItem === this.itemsCount && this.el.wrap.style.transform !== `translateX(${(this.curItem + 1) * -this.VIEWER_WIDTH}px)`) return;
         if (this.curItem === 0) {
-            this.tsChecker();
             this.curItem = this.itemsCount;
             this.navScaleCtl(this.itemsCount, 0);
-            const x = 0;
+            const x = this.curItem * -this.VIEWER_WIDTH;
             this.el.wrap.style.transform = `translateX(${x + "px"})`;
-            this.el.wrap.addEventListener("transitionend", this.tsEndHandler);
         } else {
-            this.tsChecker();
             this.curItem--;
             this.navScaleCtl(this.curItem, this.curItem + 1);
-            const x = (this.curItem * -this.VIEWER_WIDTH) - this.VIEWER_WIDTH;
+            const x = this.curItem * -this.VIEWER_WIDTH;
             this.el.wrap.style.transform = `translateX(${x + "px"})`;
-        }
-    }
-
-    tsEndHandler() {
-        const x = this.curItem === 0 ? -this.VIEWER_WIDTH : -this.VIEWER_WIDTH * this.el.items.length;
-        this.el.wrap.classList.remove("on-transition");
-        this.el.wrap.style.transform = `translateX(${x + "px"})`;
-        this.el.wrap.removeEventListener("transitionend", this.tsEndHandler);
-    }
-
-    tsChecker() {
-        if (!this.el.wrap.classList.contains("on-transition")) {
-            this.el.wrap.classList.add("on-transition");
         }
     }
 
@@ -73,29 +49,14 @@ class Slide {
         this.el.cards[prevItem].classList.remove("slide-nav-selected");
     }
 
-    makeDummy() {
-        const firstItem = this.el.items[0];
-        const lastDummy = _$("li");
-        lastDummy.innerHTML = firstItem.innerHTML;
-        lastDummy.classList.add("slide-item");
-        this.el.wrap.lastElementChild.after(lastDummy);
-
-        const lastItem = this.el.items[this.el.items.length - 1];
-        const firstDummy = _$("li");
-        firstDummy.innerHTML = lastItem.innerHTML;
-        firstDummy.classList.add("slide-item");
-        this.el.wrap.firstElementChild.before(firstDummy);
-    }
-
     addBtnEvent() {
         this.el.cards.forEach((node, idx) => {
             node.addEventListener("click", () => {
                 if (idx === this.curItem) return;
-                this.tsChecker();
                 const prevItem = this.curItem;
                 this.curItem = idx;
                 this.navScaleCtl(this.curItem, prevItem);
-                const x = (this.curItem * -this.VIEWER_WIDTH) - this.VIEWER_WIDTH;
+                const x = this.curItem * -this.VIEWER_WIDTH;
                 this.el.wrap.style.transform = `translateX(${x + "px"})`;
             });
         });
