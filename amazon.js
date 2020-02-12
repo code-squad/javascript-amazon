b4e76d6d7f304087493dea3776d03e7520e19ae0
Lee SeEun <sally4405@naver.com>
2020-02-12 15:16:50 +0900

    상단 menu a 태그에서 p 태그로 수정. js 버튼 클릭과 메뉴 클릭시의 index 변화를 하나로 통일해서 관리.

--- amazon.js
+++ amazon.js
@@ -1,86 +1,74 @@
-
-
-class SlideService {
-    constructor(buttonEl, textTarget) {
-        this.buttonEl = buttonEl;
+class TargetInfo {
+    constructor(firstIndex, imgTarget, textTarget, menuList) {
+        this.imgTarget = imgTarget;
         this.textTarget = textTarget;
-        this.onMouseDownHandler();
+        this.menuList = menuList;
+        this.targetIndex = firstIndex;
     }
 
-    onMouseDownHandler = () => {
-        let [left, right] = document.querySelectorAll("button");
-        let clickCount = 0;
+    handler() {
+        this.imgTarget.style.transform = `translateX(${this.targetIndex * (-250)}px)`;
+        this.textTarget.style.transform = `translateY(${this.targetIndex * (-250)}px)`;
+        this.resize();
+    }
 
-        left.addEventListener("mousedown", () => {
-            clickCount += 250;
-            if (clickCount > 0) clickCount = -750;
-            this.buttonEl.style.transform = `translateX(${clickCount}px)`;
-            this.textTarget.style.transform = `translateY(${clickCount}px)`;
-        });
-        right.addEventListener("mousedown", () => {
-            clickCount -= 250;
-            if (clickCount < -750) clickCount = 0;
-            this.buttonEl.style.transform = `translateX(${clickCount}px)`;
-            this.textTarget.style.transform = `translateY(${clickCount}px)`;
-        });
+    resize() {
+        this.menuList.forEach(element => {
+            element.style.transform = "scale(1, 1)";
+        })
+        this.menuList[this.targetIndex].style.transform = "scale(1.09, 1.09)";
     }
 }
 
-class ChangeScaleMenu {
-    constructor(buttonEl, textTarget) {
-        this.buttonEl = buttonEl;
-        this.textTarget = textTarget;
-        this.onScaleHandler();
+
+class SlideService {
+    constructor(firstIndex, imgTarget, textTarget, menuList) {
+        this.target = new TargetInfo(firstIndex, imgTarget, textTarget, menuList);
+        this.onButtonHandler();
+        this.onMenuHandler();
+
     }
 
-    onScaleHandler = () => {
-        let el = document.querySelectorAll(".menu-list>li")
-        let preIndex = 0;
-        
-        el[0].addEventListener("mousedown", () => {
-            this.resize(el, 0, preIndex);
-            preIndex = 0;
-        });
+    onButtonHandler() {
+        let [left, right] = document.querySelectorAll("button");
 
-        el[1].addEventListener("mousedown", () => {
-            // this.buttonEl.style.transform = "translateX(-250px)";
-            // this.textTarget.style.transform = "translateY(-250px)";
-            // el[1].style.transform = "scale(1.25, 1.25)";
-            this.resize(el, 1, preIndex);
-            preIndex = 1;
+        left.addEventListener("mousedown", () => {
+            this.target.targetIndex--;
+            if (this.target.targetIndex < 0) this.target.targetIndex = 3;
+            this.target.handler();
         });
-
-        el[2].addEventListener("mousedown", () => {
-            this.resize(el, 2, preIndex);
-            preIndex = 2;
-        });
-
-        el[3].addEventListener("mousedown", () => {
-            // this.buttonEl.style.transform = "translateX(-750px)";
-            // this.textTarget.style.transform = "translateY(-750px)";
-            this.resize(el, 3, preIndex);
-            preIndex = 3;
+        right.addEventListener("mousedown", () => {
+            this.target.targetIndex++;
+            if (this.target.targetIndex > 3) this.target.targetIndex = 0;
+            this.target.handler();
         });
     }
 
-    resize(el, index, preIndex) {
-        el[preIndex].style.transform = "scale(0.8, 0.8)";
-        el[index].style.transform = "scale(1.25, 1.25)";
-        this.buttonEl.style.transform = `translateX(${-250*index}px)`;
-        this.textTarget.style.transform = `translateY(${-250*index}px)`;
+    onMenuHandler() {
+        let menuList = document.querySelectorAll(".menu-list>li")
+
+        for (let i = 0; i < menuList.length; i++) {
+            menuList[i].addEventListener("mousedown", () => {
+                this.target.targetIndex = i;
+                this.target.handler();
+            });
+        }
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
-    const target = document.querySelector(".content__img-child");
-    const textTarget = document.querySelector(".content__text-childs");
-    const ss = new SlideService(target, textTarget);
-    const cc = new ChangeScaleMenu(target, textTarget);
+    let firstIndex = (parseInt(Math.random() * 4));
+    console.log(firstIndex);
 
-    let number = (parseInt(Math.random() * 4)) * 250 * (-1);
-    console.log(number);
-    target.style.transform = `translateX(${number}px)`;
-    textTarget.style.transform = `translateY(${number}px)`;
+    const imgTarget = document.querySelector(".content__img-child");
+    const textTarget = document.querySelector(".content__text-childs");
+    const menuList = document.querySelectorAll(".menu-list>li");
+
+    imgTarget.style.transform = `translateX(${firstIndex * (-250)}px)`;
+    textTarget.style.transform = `translateY(${firstIndex * (-250)}px)`;
+    menuList[firstIndex].style.transform = "scale(1.09, 1.09)";
+
+    const ss = new SlideService(firstIndex, imgTarget, textTarget, menuList);
 });
