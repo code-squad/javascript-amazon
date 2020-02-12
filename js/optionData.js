ab61b9c4ee852ec19b403134d27d1d062dcacb25
sgdg <seungdeng17@gmail.com>
2020-02-12 21:50:24 +0900

    add. optionData.js 파일 추가
    네비 카드 색상, 너비, 뷰어 너비, tag 클래스 네임 정보 담고있음

---
+++ js/optionData.js
@@ -0,0 +1,20 @@
+const OPTION_DATA = {
+    navCardColors: [
+        "#4691BC",
+        "#9B2165",
+        "#398377",
+        "#D76530",
+    ],
+    slideOption: {
+        NAV_CARD_WIDTH: 220,
+        VIEWER_WIDTH: 900,
+        FIRST_ITEM_INDEX: 0,
+    },
+    elementOption: {
+        prev: ".prev-btn",
+        next: ".next-btn",
+        slide_item_wrap: ".slide-item-wrap",
+        slide_items: ".slide-item",
+        slide_nav_li: ".slide-nav li",
+    }
+}
