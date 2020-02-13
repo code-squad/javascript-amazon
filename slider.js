e83e0040dc564f60ebf272c03e10dd532a8d493b
noto01 <noto0131@gmail.com>
2020-02-13 22:24:17 +0900

    [modified] return추가 및  constant정의 오류 수정

--- slider.js
+++ slider.js
@@ -43,19 +43,19 @@ class CarouselSlider {
         this.carousel = carousel,
 
             this.constant = {
-                FIRSTCLONE = 'slider-firstClone',
-                LASTCLONE = 'slider-lastClone',
-                SLIDES = '.slider__list',
-                SLIDE_ITEM = '.slider__item',
-                SLIDER_BTNS = '#slider__btn button',
-                FIRSTSLIDE_INDEX = 1,
-                LASTSLIDE_INDEX = 2,
+                FIRSTCLONE: 'slider-firstClone',
+                LASTCLONE: 'slider-lastClone',
+                SLIDES: '.slider__list',
+                SLIDE_ITEM: '.slider__item',
+                SLIDER_BTNS: '#slider__btn button',
+                FIRSTSLIDE_INDEX: 1,
+                LASTSLIDE_INDEX: 2,
             }
     }
 
     getSliderInfo() {
         this.slideItems = $(this.constant.SLIDE_ITEM, true);
-        this.slideItemlength = this.slideItems.length;
+        return this.slideItemlength = this.slideItems.length;
     }
 
     cloneSlide() {
@@ -63,20 +63,20 @@ class CarouselSlider {
         //위에서 구해놓은 this.slideItems[0]이 좋은지 firstElementChild로 의미를 명확하게 하는게 좋은지
         const lastSlide = this.carousel.slides.lastElementChild;
         const firstClone = firstSlide.cloneNode(true);
-        firstClone.id = FIRSTCLONE;
+        firstClone.id = this.constant.FIRSTCLONE;
         const lastClone = lastSlide.cloneNode(true);
-        lastClone.id = LASTCLONE;
+        lastClone.id = this.constant.LASTCLONE;
         this.carousel.slides.append(firstClone);
         this.carousel.slides.prepend(lastClone);
         this.getSliderInfo();
-        this.carousel.moveSlides(this.carousel.slideIndex);
+        return this.carousel.moveSlides(this.carousel.slideIndex);
     }
 
     setSliderBtns() {
         const [previousBtn, nextBtn] = $(this.constant.SLIDER_BTNS, true);
 
         this.setClickEvent(previousBtn);
-        this.setClickEvent(nextBtn, true);
+        return this.setClickEvent(nextBtn, true);
     }
 
     setClickEvent(sliderBtn, plusIndex) {
@@ -85,24 +85,26 @@ class CarouselSlider {
             this.carousel.moveSlides(this.carousel.slideIndex);
             this.checkBothEndsSlide();
         });
+        return;
     }
 
     checkBothEndsSlide() {
         const lastSlideIndex = this.slideItemlength - this.constant.LASTSLIDE_INDEX;
         let currentSlideId = this.slideItems[this.carousel.slideIndex].id;
 
-        if (currentSlideId === LASTCLONE) {
+        if (currentSlideId === this.constant.LASTCLONE) {
             this.changeSlideIndex(lastSlideIndex);
         }
 
-        if (currentSlideId === FIRSTCLONE) {
+        if (currentSlideId === this.constant.FIRSTCLONE) {
             this.changeSlideIndex(this.constant.FIRSTSLIDE_INDEX);
         }
+        return;
     }
 
     changeSlideIndex(index) {
         this.carousel.slideIndex = index;
-        this.carousel.moveSlides(this.carousel.slideIndex);
+        return this.carousel.moveSlides(this.carousel.slideIndex);
     }
 }
 
@@ -111,17 +113,18 @@ class CarouselCardMenu {
         this.carousel = carousel,
 
             this.constant = {
-                CARD_BTN = '.card button',
+                CARD_BTN: '.card button',
             }
     }
 
     setCardBtns() {
-        const cardBtns = $(this.CARD_BTN, true);
+        const cardBtns = $(this.constant.CARD_BTN, true);
         cardBtns.forEach((btn, index) => {
             btn.addEventListener('click', () => {
                 this.carousel.moveSlides(index + 1);
             })
         })
+        return;
     }
 }
 
