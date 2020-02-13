0f453cf2b5d2a0b1c930ef438c5ec6e9f25d19b8
noto01 <noto0131@gmail.com>
2020-02-13 20:13:17 +0900

    [modified] js 클래스 carousel, slider, cardMenu로 분리 carousel을 전부 slider와 cardMenu에 인자로 전달

--- slider.js
+++ slider.js
@@ -1,4 +1,3 @@
-//클래스 
 const $ = (selector, all) => {
     if (all) return document.querySelectorAll(selector);
     return document.querySelector(selector);
@@ -6,58 +5,70 @@ const $ = (selector, all) => {
 
 
 window.addEventListener('DOMContentLoaded', () => {
+    const carousel = carouselService();
+    carousel.getSlideSize();
+    const carouselSlider = new CarouselSlider(carousel);
+    carouselSlider.getSliderInfo();
+    carouselSlider.cloneSlide();
+    carouselSlider.setSliderBtns();
+    const carouselCardMenu = new CarouselCardMenu(carousel);
+    carouselCardMenu.setCardBtns();
+});
 
-    const cardMenuBtns = $('.card-menu__card button', true);
-    const slider = $('.slider__list');
-    // const size = $(".slider__item").clientWidth;
-    const carousel = new carouselService({
-        cardMenuBtns: cardMenuBtns,
-        slider: slider,
+
+function carouselService() {
+    const SLIDES = '.slider__list';
+
+    const slides = $(SLIDES);
+    const carousel = new CarouselService({
+        slides: slides,
         slideIndex: 1
-    });
-    carousel.getSliderInfo();
-    carousel.cloneSlide();
-    carousel.setSliderBtns();
-    carousel.setCardBtns();
-})
+    })
+    return carousel;
+}
 
 
-
-// 모듈2
 const FIRSTSLIDE = 'slider-firstClone';
 const LASTSLIDE = 'slider-lastClone';
 
+class CarouselService {
+    constructor(sliderData) {
+        this.slides = sliderData.slides;
+        this.slideIndex = sliderData.slideIndex;
+    }
 
-class carouselService {
-    constructor(option) {
-        this.cardBtns = option.cardMenuBtns;
-        this.slider = option.slider;
-        this.slideIndex = option.slideIndex;
+    getSlideSize() {
+        return this.slideSize = this.slides.firstElementChild.clientWidth;
+    }
+
+    moveSlides(slideIndex) {
+        return this.slides.style.transform = 'translateX(' + (-this.slideSize * slideIndex) + 'px)';
+    }
+}
+
+class CarouselSlider {
+    constructor(carousel) {
+        this.carousel = carousel;
     }
 
     getSliderInfo() {
-        const SLIDES = '.slider__item';
-        this.slides = $(SLIDES, true);
-        this.slideSize = this.slides[0].clientWidth;
-        this.length = this.slides.length;
+        const SLIDE_ITEM = '.slider__item';
+        this.slideItems = $(SLIDE_ITEM, true);
+        this.slideItemlength = this.slideItems.length;
     }
 
     cloneSlide() {
-        const firstSlide = this.slides[0];
-        const lastSlide = this.slides[this.length - 1];
-
+        const firstSlide = this.carousel.slides.firstElementChild;
+        //위에서 구해놓은 this.slideItems[0]이 좋은지 firstElementChild로 의미를 명확하게 하는게 좋은지
+        const lastSlide = this.carousel.slides.lastElementChild;
         const firstClone = firstSlide.cloneNode(true);
         firstClone.id = FIRSTSLIDE;
         const lastClone = lastSlide.cloneNode(true);
         lastClone.id = LASTSLIDE;
-        this.slider.append(firstClone);
-        this.slider.prepend(lastClone);
+        this.carousel.slides.append(firstClone);
+        this.carousel.slides.prepend(lastClone);
         this.getSliderInfo();
-        this.moveSlides(this.slideIndex);
-    }
-
-    moveSlides(slideIndex) {
-        return this.slider.style.transform = 'translateX(' + (-this.slideSize * slideIndex) + 'px)';
+        this.carousel.moveSlides(this.carousel.slideIndex);
     }
 
     setSliderBtns() {
@@ -65,27 +76,28 @@ class carouselService {
         const [previousBtn, nextBtn] = $(SLIDER_BTNS, true);//클래스 아이디??
 
         previousBtn.addEventListener('click', () => {
-            this.slideIndex--;
+            this.carousel.slideIndex--;
             this.moveAndCheckSlide();
+        });
 
-        })
         nextBtn.addEventListener('click', () => {
-            this.slideIndex++;
+            this.carousel.slideIndex++;
             this.moveAndCheckSlide();
-        })
-
+        });
     }
 
     moveAndCheckSlide() {
-        this.moveSlides(this.slideIndex);
+        this.carousel.moveSlides(this.carousel.slideIndex);
         this.checkBothEndsSlide();
     }
 
+
     checkBothEndsSlide() {
         const FIRSTSLIDE_INDEX = 1;
-        const lastSlideIndex = this.length - 2;
+        const LASTSLIDE_INDEX = 2;
+        const lastSlideIndex = this.slideItemlength - LASTSLIDE_INDEX;
 
-        let currentSlideId = this.slides[this.slideIndex].id;
+        let currentSlideId = this.slideItems[this.carousel.slideIndex].id;
 
         if (currentSlideId === LASTSLIDE) {
             this.changeSlideIndex(lastSlideIndex);
@@ -97,19 +109,33 @@ class carouselService {
     }
 
     changeSlideIndex(index) {
-        this.slideIndex = index;
-        this.moveSlides(this.slideIndex);
+        this.carousel.slideIndex = index;
+        this.carousel.moveSlides(this.carousel.slideIndex);
+    }
+}
+
+class CarouselCardMenu {
+    constructor(carousel) {
+        this.carousel = carousel;
     }
 
     setCardBtns() {
-        this.cardBtns.forEach((btn, index) => {
+        const cardBtns = $('.card button', true);
+        cardBtns.forEach((btn, index) => {
             btn.addEventListener('click', () => {
-                this.moveSlides(index + 1);
+                this.carousel.moveSlides(index + 1);
             })
         })
     }
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
 
