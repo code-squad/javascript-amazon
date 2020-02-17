2d08d9b35e45bc8a7f98d62b47ab049022641bd7
choisohyun <30427711+choisohyun@users.noreply.github.com>
2020-02-17 17:29:13 +0900

    [미션 제출] Animation (#137)
    * Initial commit
    
    * feat. html, css UI 틀 추가
    
    - 아마존 슬라이드 화면을 클론코딩한 파일 업로드
    - 슬라이드 클릭 시 화면 이동은 아직 구현하지 않음
    - 이미지, 콘텐츠는 dummy 파일 이용함
    
    * feat. html, css UI 틀 추가
    
    - 아마존 슬라이드 화면을 클론코딩한 파일 업로드
    - 슬라이드 클릭 시 화면 이동은 아직 구현하지 않음
    - 이미지, 콘텐츠는 dummy 파일 이용함
    
    * feat. prev,next 버튼 클릭 시 슬라이드 이동 기능 추가
    
    - 슬라이드의 양 옆 화살표 버튼 클릭 시 다음 화면이 나오는 기능을 구현함
    - 상단의 네비게이션 클릭 시 이동은 아직 구현하지 못함
    - 마지막 슬라이드에서 다음 버튼을 누르면 첫 번째 슬라이드가 나옴
    - 처음 슬라이드에서 이전 버튼을 누르면 마지막 슬라이드가 나옴
    
    * refactor. function -> class로 변경
    
    - js 코드를 ES6 Classes로 표현하기 위해 변경함
    - 기능 추가는 없음
    
    * feat. navigation 버튼 클릭 시 슬라이드 이동 기능 추가
    
    - 각각의 navigation 버튼을 클릭하면 해당 버튼의 index를 얻어와 이동함
    - 슬라이드 애니메이션 기능은 아직 추가하지 않음
    
    * docs. Delete image files, duplicate files
    
    * feat. Add Mock Data & html 자동생성파일 완성
    
    data.js - mock data를 임의로 배열로 저장함
    mockData.js - mock data를 html 파일로 순서대로 추가될 수 있도록 함
    
    * feat. Add dummy element for infinite slides
    
    - addDummySlide(function)
     부드럽게 넘어가는 무한 슬라이드를 구현하기 위해 생성함
     slide의 first, last child를 각각 마지막과 처음에 넣어 줌
     추후 파일명, 변수명 등 리팩토링 예정임
    
    * refactor. Rename & change class structure
    
    - index.js -> main.js, navigationBar.js, carousel.js로 클래스 나눔
    - slideSetting.js -> 슬라이드 구현에 필요한 요소를 객체로 따로 저장함
    - sprinkleData.js -> mockData.js에 배열로 있는 데이터를 뿌려주는 역할을
    함
    - navigationBar.js와 carousel.js는 다른 클래스이지만 중복되는 코드가
    존재함
    => 추후 리팩토링 필요
    
    * style. Edit arrow button position
    
    화살표 버튼의 위치가 어색해 width를 100% -> 800px로 변경함

---
+++ js/mockData.js
@@ -0,0 +1,48 @@
+const mockData = [
+  {
+    title: "Moulin Rouge",
+    image: "http://dummyimage.com/227x119.jpg/dddddd/000000",
+    header: "1Analog Circuit Design manager",
+    contents: [
+      "Morbi vel lectus in quam fringilla rhoncus. ",
+      "Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+      "Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, ",
+      "Maecenas pulvinar lobortis est. Phasellus sit amet erat. ."
+    ]
+  },
+  {
+    title: "One Night with the King",
+    image: "http://dummyimage.com/249x161.png/5fa2dd/ffffff",
+    header: "2Clinical Specialist",
+    contents: [
+      "Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
+      "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.",
+      "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis."
+    ]
+  },
+  {
+    title: "Hanna",
+    image: "http://dummyimage.com/109x136.bmp/dddddd/000000",
+    header: "3Safety Technician I",
+    contents: [
+      "Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. ",
+      "Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, ",
+      "Etiam vel augue.",
+      "Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. "
+    ]
+  },
+  {
+    title: "Smile",
+    image: "http://dummyimage.com/229x242.bmp/ff4444/ffffff",
+    header: "4Executive Secretary",
+    contents: [
+      "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl.",
+      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. ",
+      "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. ",
+      "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus. Phasellus in felis. Donec semper sapien a libero. Nam dui."
+    ]
+  }
+];
+
+export { mockData };
