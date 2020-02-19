bf2af47ab1cbaee0621df7ff4d5301e19476b9cc
sangwon21 <junothyme@gmail.com>
2020-02-20 00:22:31 +0900

    feat: localStorage를 이용한 캐싱
    why: 클래스의 멤버 변수로 가지고 있는 것보다 브라우저의 localStorage를
    통해 캐싱을 하는 방법이 조금 더 적합하다고 생각하여 바꿨다.

--- client/src/main.js
+++ client/src/main.js
@@ -10,10 +10,16 @@ const addEvents = () => {
 
 const constructHTML = async () => {
     const slider = new Slider();
-    if (!slider.doesCardDataExist()) {
+    let cards = window.localStorage.getItem('cards');
+    if (!cards) {
         const cardData = await slider.fetchCardData();
-        slider.setCardData(cardData);
+        window.localStorage.setItem("cards", JSON.stringify(cardData));
+        cards = cardData;
+        slider.setCardData(cards);
+        $("#slider").innerHTML = slider.render();
+        return;
     }
+    slider.setCardData(JSON.parse(cards));
     $("#slider").innerHTML = slider.render();
 }
 
