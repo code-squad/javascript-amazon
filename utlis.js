5e31def5821a25428206319b909fcf639bbb8174
Hu2y <sanghun_@daum.net>
2020-02-19 22:42:57 +0900

    [add] koa server, 루트 경로 라우팅 처리

--- utlis.js
+++ utlis.js
@@ -1,6 +1,4 @@
-
 const $ = el => document.querySelector(el)
 const $$ = el => document.querySelectorAll(el)
-const loaded = func => window.addEventListener("DOMContentLoaded", func)
 
-
+// 전역변수를 없애려고 객체를 활용 할 예정입니다 const hueyUtils = { $() {}, .....}
