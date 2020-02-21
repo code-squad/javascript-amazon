1f6c760e1465e1645239a56adfe4130d80e4c5ce
sgdg <seungdeng17@gmail.com>
2020-02-21 22:26:22 +0900

    feat. fetch해서 가져온 json데이터를 local storage에 캐시 함

--- js/main.js
+++ js/main.js
@@ -2,22 +2,31 @@ import { OPTION_DATA } from './optionData.js';
 import DataRender from './dataRender.js';
 import Slide, { NavCard } from './slide.js';
 
-const dataJsonURL = "http://localhost:8080/json/localData.json";
 
-function startSlideService() {
-    fetch(dataJsonURL)
-        .then(response => response.json())
-        .then(json => {
-            const dataRender = new DataRender(OPTION_DATA, json);
-            dataRender.setNav();
-            dataRender.setSlide();
-
-            OPTION_DATA.slideOption.FIRST_ITEM_INDEX = Math.floor(Math.random() * OPTION_DATA.slideOption.ITEM_COUNT);
-
-            const navCard = new NavCard(OPTION_DATA.slideOption);
-            const slide = new Slide(OPTION_DATA.slideOption, [navCard]);
-            slide.run();
-        });
+function main() {
+    const realMadridData = localStorage.getItem('REAL-MADRID');
+    if (realMadridData) {
+        startSlideService(JSON.parse(realMadridData));
+    } else {
+        fetch("http://localhost:8080/json/localData.json")
+            .then(response => response.json())
+            .then(json => {
+                localStorage.setItem('REAL-MADRID', JSON.stringify(json));
+                startSlideService(json);
+            });
+    }
 }
 
-startSlideService();
+function startSlideService(data) {
+    const dataRender = new DataRender(OPTION_DATA, data);
+    dataRender.setNav();
+    dataRender.setSlide();
+
+    OPTION_DATA.slideOption.FIRST_ITEM_INDEX = Math.floor(Math.random() * OPTION_DATA.slideOption.ITEM_COUNT);
+
+    const navCard = new NavCard(OPTION_DATA.slideOption);
+    const slide = new Slide(OPTION_DATA.slideOption, [navCard]);
+    slide.run();
+}
+
+main();
