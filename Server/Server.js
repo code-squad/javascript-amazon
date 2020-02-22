d10337cd5d9b951bc6cc5e6461749e18af14c254
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-02-22 21:36:27 +0900

    [수정] Server 에서 CORS 를 허용하도록 수정
    Server 에서 CORS 를 허용하도록 수정.
    
    Why >
    * 포트가 다른 서버의 자원을 요청하여 이슈가 발생.
    
    package.json, package-lock.json >
    * CORS 관련 정보가 추가되었음.
    
    Server.js >
    * CORS 를 허가하는 코드 추가.

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,11 @@
 const port = 8080;
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const data = fs.readFileSync('localData.json', 'utf8');
 const contents = JSON.parse(data);
 
+app.use(cors());
 app.get('/', (req, res) => res.send(contents));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
