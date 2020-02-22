fcba98fa30d370d04c96df46fb72fa623477dece
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-02-22 23:41:54 +0900

    [수정] SlideEnum 수정, localStorage 캐싱 되도록 수정
    SlideEnum 수정 >
    * 잘못된 SlideEnum 작성으로 인해 버튼들 오동작 하는 문제 수정.
    
    localStorage 에 캐싱 되도록 수정 >
    * localStroage 에 캐싱 된 데이터를 재 사용하지 않았던 문제 수정.

--- SlideEnum.js
+++ SlideEnum.js
@@ -1,7 +1,7 @@
-const SlideEnum = {
-    INCREASE_CURRENT_INDEX: 0,
-    DECREASE_CURRENT_INDEX: 1,
-    CHANGE_CURRENT_INDEX: 2
-}
+const SlideEnum = Object.freeze({
+    "INCREASE_CURRENT_INDEX": 0,
+    "DECREASE_CURRENT_INDEX": 1,
+    "CHANGE_CURRENT_INDEX": 2
+});
 
-export default {SlideEnum};
+export default SlideEnum;
