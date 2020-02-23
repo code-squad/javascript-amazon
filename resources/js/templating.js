6b8340c5f1b71b4d3507faf2b7a4d76b7f78c1e8
최성익 <tjddlr92@gmail.com>
2020-02-23 17:05:23 +0900

    Feat: DOM Templating 작업
    JSON 파일에서 데이터를 받아서 나누고, HTML에 들어갈 정보를 일일이 적는 식으로 했는데, 하면서도 변경에 취약할 거 같다는 생각이 계속 들었다.
    어떤 식으로 해야 변경에 취약하지 않을까? DOM Templating에선 이런 부분을 어떻게 유연하게 처리하는지...
    
    어쨌든 DOM Templating 작업은 완료됐는데, 여기에 Carousel.js 파일이 제대로 붙지 않아서 기능하지 못한다.
    문제점을 해결하기 위해 templating 안의 내용을 하나의 Class로 만들고, carousel 과 templating 둘 다 모듈 export로 빼낸 후,
    main.js 파일을 따로 만들어 그 안에서 fetching을 하고 절차적으로 해보려고 한다. 현재 carousel.js가 json 데이터가 fetch 되기 전에 DOM을 참고해서 벌어지는 에러같다.

--- resources/js/templating.js
+++ resources/js/templating.js
@@ -8,12 +8,43 @@ window.addEventListener("DOMContentLoaded", () => {
       const carouselBtn = data[1];
       const carouselCard = data[2];
 
-      const item = {
+      const itemData = {
         name: carouselItem.name,
         title: getValuefromData(carouselItem.list, `title`),
         desc: getValuefromData(carouselItem.list, `desc`),
         imgSrc: getValuefromData(carouselItem.list, `imgSrc`),
       };
+
+      const btnData = {
+        name: carouselBtn.name,
+        id: getValuefromData(carouselBtn.list, `id`),
+        imgClass: getValuefromData(carouselBtn.list, `imgClass`),
+        text: getValuefromData(carouselBtn.list, `text`),
+        href: getValuefromData(carouselBtn.list, `href`),
+      };
+
+      const cardData = {
+        name: carouselCard.name,
+        title: getValuefromData(carouselCard.list, `title`),
+        href: getValuefromData(carouselCard.list, `href`),
+      };
+
+      templatingHTML({
+        size: 4,
+        cardClass: `card`,
+        sliderClass: `slider`,
+        btnClass: `btn`,
+      });
+
+      function templatingHTML(option) {
+        const wrap = option.wrapperClass || `.wrap`;
+        const carouselSize = option.size;
+        const cardHTML = renderCard(makeClassName(option.cardClass), cardData, carouselSize);
+        const btnHTML = renderBtn(makeClassName(option.btnClass), btnData);
+        const sliderHTML = renderSlider(makeClassName(option.sliderClass), itemData, carouselSize, btnHTML);
+
+        $(wrap).innerHTML = cardHTML + sliderHTML;
+      }
 
       function getValuefromData(obj, str) {
         const newData = [];
@@ -34,14 +65,31 @@ window.addEventListener("DOMContentLoaded", () => {
         });
       }
 
-      console.log(renderSlider(makeClassName("slider"), item, 4));
+      function renderSlider(className, data, size, btnHTML) {
+        let sliderItem = "";
+        for (let i = 0; i < size; i++) {
+          sliderItem += `<li class="${className.li}"><div class="${className.imgArea}"><img src="${data.imgSrc[i]}"/></div><div class="${className.content}"><h1>${data.title[i]}</h1><p>${data.desc[i]}</p></div></li>`;
+        }
+        const resultHTML = `<div class="${className.name}"><div class="${className.container}"><ul class="${className.ul}">${sliderItem}</ul></div>${btnHTML}</div>`;
+        return resultHTML;
+      }
 
-      function renderSlider(className, data, size) {
+      function renderCard(className, data, size) {
         let cardItem = "";
         for (let i = 0; i < size; i++) {
-          cardItem += `<li class="${className.li}"><div class="${className.imgArea}"><img src="${data.imgSrc[i]}"/></div><div class="${className.content}"><h1>${data.title[i]}</h1><p>${data.desc[i]}</p></div></li>`;
+          cardItem += `<li class="${className.li}"><a href="${data.href[i]}"><span>${data.title[i]}</span></a></li>`;
         }
-        let resultHTML = `<div class="${className.name}><div class="${className.container}"><ul class="${className.ul}">${cardItem}</ul></div></div>`;
+        const resultHTML = `<div class="${className.container}"><ul class="${className.ul}">${cardItem}</ul></div>`;
+        return resultHTML;
+      }
+
+      function renderBtn(className, data) {
+        let btnItem = "";
+        const btnSize = 2;
+        for (let i = 0; i < btnSize; i++) {
+          btnItem += `<div class="${className.name}-${data.id[i]}"><a href="${data.href[i]}"><i class="${data.imgClass[i]}"><span class="blind">${data.text[i]}</span></i></a></div>`;
+        }
+        const resultHTML = `<div class="${className.name}">${btnItem}</div>`;
         return resultHTML;
       }
     });
