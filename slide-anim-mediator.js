58622569f0b68a4e831e3893419c26d32db132ae
Reese Kim <reesekimm@gmail.com>
2020-02-23 17:11:57 +0900

    Relocate mediator's methods
    Moved 'register', 'slide' methods into function

--- slide-anim-mediator.js
+++ slide-anim-mediator.js
@@ -9,7 +9,38 @@ export default function() {
   let navIdx = 0;
   let cardIdx = 1;
 
-  const updateState_btn = function(index) {
+  const register = function(comp) {
+    components[comp.name] = comp;
+    comp.slideAnimMediator = this;
+  };
+
+  const slide = function(comp, index) {
+    _updateState(comp, index);
+    components.navigation.update(navIdx);
+    _moveCardList();
+    $ael(cardList, "transitionend", () => {
+      if (cards[cardIdx].classList.contains("first")) {
+        cardList.style.transition = "none";
+        cardIdx = cards.length - 2;
+        _moveCardList();
+      } else if (cards[cardIdx].classList.contains("last")) {
+        cardList.style.transition = "none";
+        cardIdx = 1;
+        _moveCardList();
+      }
+    });
+  };
+
+  const _updateState = function(comp, index) {
+    if (comp === "button") {
+      _updateState_btn(index);
+    } else if (comp === "navigation") {
+      _updateState_nav(index);
+    }
+    cardList.style.transition = "transform .5s ease-in-out";
+  };
+
+  const _updateState_btn = function(index) {
     const navElements = components.navigation.elements;
     if (index === 0) {
       cardIdx -= 1;
@@ -20,46 +51,19 @@ export default function() {
     }
   };
 
-  const updateState_nav = function(index) {
+  const _updateState_nav = function(index) {
     cardIdx = index + 1;
     navIdx = index;
   };
 
-  const updateState = function(comp, index) {
-    if (comp === "button") {
-      updateState_btn(index);
-    } else if (comp === "navigation") {
-      updateState_nav(index);
-    }
-    cardList.style.transition = "transform .5s ease-in-out";
-  };
-
-  const moveCardList = function() {
+  const _moveCardList = function() {
     const cardWidth = components.cards.cardWidth;
     cardList.style.transform =
       "translateX(" + `${-cardWidth * cardIdx}` + "px)";
   };
 
   return {
-    register: function(comp) {
-      components[comp.name] = comp;
-      comp.slideAnimMediator = this;
-    },
-    slide: function(comp, index) {
-      updateState(comp, index);
-      components.navigation.update(navIdx);
-      moveCardList();
-      $ael(cardList, "transitionend", () => {
-        if (cards[cardIdx].classList.contains("first")) {
-          cardList.style.transition = "none";
-          cardIdx = cards.length - 2;
-          moveCardList();
-        } else if (cards[cardIdx].classList.contains("last")) {
-          cardList.style.transition = "none";
-          cardIdx = 1;
-          moveCardList();
-        }
-      });
-    }
+    register: register,
+    slide: slide
   };
 }
