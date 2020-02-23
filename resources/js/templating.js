4319700e555b51711ce01de7b136e5639d51eaf6
최성익 <tjddlr92@gmail.com>
2020-02-23 17:45:50 +0900

    Feat: Class 구조 변경
    main.js 파일을 따로 만들어 그 안에서 carousel 과 templating 을 import 받아서 사용하게 만들었다. 예상대로 fetch 블록 안에서 두 객체를 순서대로 불러오니 제대로 작동한다.
    아쉬운 점은 class명을 뭔가 더 체계적으로.. 할 수 있을 거 같은데, 이 부분은 또 작성한 css랑도 연결되는 부분이라, 어떤 방법이 제일 좋을지? 고민이다.
    신기했던 점은 templating의 옵션으로 size의 다른 값을 줘도 잘 작동한다는 점. 데이터를 늘리면 6개 ~ 7개 까지도 잘 늘어날 것 같아서 이 부분은 마음에 든다.

--- resources/js/templating.js
+++ resources/js/templating.js
@@ -1,96 +1,103 @@
 import { $, $$ } from "./util.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("../data/localData.json")
-    .then(res => res.json())
-    .then(data => {
-      const carouselItem = data[0];
-      const carouselBtn = data[1];
-      const carouselCard = data[2];
+class Templating {
+  constructor(data, option) {
+    this.carouselItem = data[0];
+    this.carouselBtn = data[1];
+    this.carouselCard = data[2];
 
-      const itemData = {
-        name: carouselItem.name,
-        title: getValuefromData(carouselItem.list, `title`),
-        desc: getValuefromData(carouselItem.list, `desc`),
-        imgSrc: getValuefromData(carouselItem.list, `imgSrc`),
-      };
+    this.itemData = {
+      name: this.carouselItem.name,
+      title: this.getValuefromData(this.carouselItem.list, `title`),
+      desc: this.getValuefromData(this.carouselItem.list, `desc`),
+      imgSrc: this.getValuefromData(this.carouselItem.list, `imgSrc`),
+    };
 
-      const btnData = {
-        name: carouselBtn.name,
-        id: getValuefromData(carouselBtn.list, `id`),
-        imgClass: getValuefromData(carouselBtn.list, `imgClass`),
-        text: getValuefromData(carouselBtn.list, `text`),
-        href: getValuefromData(carouselBtn.list, `href`),
-      };
+    this.btnData = {
+      name: this.carouselBtn.name,
+      id: this.getValuefromData(this.carouselBtn.list, `id`),
+      imgClass: this.getValuefromData(this.carouselBtn.list, `imgClass`),
+      text: this.getValuefromData(this.carouselBtn.list, `text`),
+      href: this.getValuefromData(this.carouselBtn.list, `href`),
+    };
 
-      const cardData = {
-        name: carouselCard.name,
-        title: getValuefromData(carouselCard.list, `title`),
-        href: getValuefromData(carouselCard.list, `href`),
-      };
+    this.cardData = {
+      name: this.carouselCard.name,
+      title: this.getValuefromData(this.carouselCard.list, `title`),
+      href: this.getValuefromData(this.carouselCard.list, `href`),
+    };
 
-      templatingHTML({
-        size: 4,
-        cardClass: `card`,
-        sliderClass: `slider`,
-        btnClass: `btn`,
-      });
+    this.templatingHTML(option);
+  }
 
-      function templatingHTML(option) {
-        const wrap = option.wrapperClass || `.wrap`;
-        const carouselSize = option.size;
-        const cardHTML = renderCard(makeClassName(option.cardClass), cardData, carouselSize);
-        const btnHTML = renderBtn(makeClassName(option.btnClass), btnData);
-        const sliderHTML = renderSlider(makeClassName(option.sliderClass), itemData, carouselSize, btnHTML);
+  templatingHTML(option) {
+    const wrap = option.wrapperClassName || `.wrap`;
+    const carouselSize = option.size;
+    const cardHTML = this.renderCard(this.makeClassName(option.cardClassName), this.cardData, carouselSize);
+    const btnHTML = this.renderBtn(this.makeClassName(option.btnClassName), this.btnData);
+    const sliderHTML = this.renderSlider(this.makeClassName(option.sliderClassName), this.itemData, carouselSize, btnHTML);
 
-        $(wrap).innerHTML = cardHTML + sliderHTML;
-      }
+    $(wrap).innerHTML = cardHTML + sliderHTML;
+  }
 
-      function getValuefromData(obj, str) {
-        const newData = [];
-        for (let key in obj) {
-          newData.push(obj[key][str]);
-        }
-        return newData;
-      }
+  getValuefromData(obj, str) {
+    const newData = [];
+    for (let key in obj) {
+      newData.push(obj[key][str]);
+    }
+    return newData;
+  }
 
-      function makeClassName(str) {
-        return new Object({
-          name: str,
-          container: `${str}-container`,
-          ul: `${str}-list`,
-          li: `${str}-item`,
-          imgArea: `${str}-img-area`,
-          content: `${str}-content`,
-        });
-      }
-
-      function renderSlider(className, data, size, btnHTML) {
-        let sliderItem = "";
-        for (let i = 0; i < size; i++) {
-          sliderItem += `<li class="${className.li}"><div class="${className.imgArea}"><img src="${data.imgSrc[i]}"/></div><div class="${className.content}"><h1>${data.title[i]}</h1><p>${data.desc[i]}</p></div></li>`;
-        }
-        const resultHTML = `<div class="${className.name}"><div class="${className.container}"><ul class="${className.ul}">${sliderItem}</ul></div>${btnHTML}</div>`;
-        return resultHTML;
-      }
-
-      function renderCard(className, data, size) {
-        let cardItem = "";
-        for (let i = 0; i < size; i++) {
-          cardItem += `<li class="${className.li}"><a href="${data.href[i]}"><span>${data.title[i]}</span></a></li>`;
-        }
-        const resultHTML = `<div class="${className.container}"><ul class="${className.ul}">${cardItem}</ul></div>`;
-        return resultHTML;
-      }
-
-      function renderBtn(className, data) {
-        let btnItem = "";
-        const btnSize = 2;
-        for (let i = 0; i < btnSize; i++) {
-          btnItem += `<div class="${className.name}-${data.id[i]}"><a href="${data.href[i]}"><i class="${data.imgClass[i]}"><span class="blind">${data.text[i]}</span></i></a></div>`;
-        }
-        const resultHTML = `<div class="${className.name}">${btnItem}</div>`;
-        return resultHTML;
-      }
+  makeClassName(str) {
+    return new Object({
+      name: str,
+      container: `${str}-container`,
+      ul: `${str}-list`,
+      li: `${str}-item`,
+      imgArea: `${str}-img-area`,
+      content: `${str}-content`,
     });
-});
+  }
+
+  renderSlider(className, data, size, btnHTML) {
+    function makeCloneSlider() {
+      const lastCloneClassName = `last-clone`;
+      const firstCloneClassName = `first-clone`;
+      return {
+        lastClone: `<li class="${className.li}" id="${lastCloneClassName}"><div class="${className.imgArea}"><img src="${data.imgSrc[size - 1]}"/></div><div class="${className.content}"><h1>${
+          data.title[size - 1]
+        }</h1><p>${data.desc[size - 1]}</p></div></li>`,
+        firstClone: `<li class="${className.li}" id="${firstCloneClassName}"><div class="${className.imgArea}"><img src="${data.imgSrc[0]}"/></div><div class="${className.content}"><h1>${data.title[0]}</h1><p>${data.desc[0]}</p></div></li>`,
+      };
+    }
+
+    let sliderItem = makeCloneSlider().lastClone;
+    for (let i = 0; i < size; i++) {
+      sliderItem += `<li class="${className.li}"><div class="${className.imgArea}"><img src="${data.imgSrc[i]}"/></div><div class="${className.content}"><h1>${data.title[i]}</h1><p>${data.desc[i]}</p></div></li>`;
+    }
+    sliderItem += makeCloneSlider().firstClone;
+    const resultHTML = `<div class="${className.name}"><div class="${className.container}"><ul class="${className.ul}">${sliderItem}</ul></div>${btnHTML}</div>`;
+    return resultHTML;
+  }
+
+  renderCard(className, data, size) {
+    let cardItem = "";
+    for (let i = 0; i < size; i++) {
+      cardItem += `<li class="${className.li}"><a href="${data.href[i]}"><span>${data.title[i]}</span></a></li>`;
+    }
+    const resultHTML = `<div class="${className.container}"><ul class="${className.ul}">${cardItem}</ul></div>`;
+    return resultHTML;
+  }
+
+  renderBtn(className, data) {
+    let btnItem = "";
+    const btnSize = 2;
+    for (let i = 0; i < btnSize; i++) {
+      btnItem += `<div class="${className.name}-${data.id[i]}"><a href="${data.href[i]}"><i class="${data.imgClass[i]}"><span class="blind">${data.text[i]}</span></i></a></div>`;
+    }
+    const resultHTML = `<div class="${className.name}">${btnItem}</div>`;
+    return resultHTML;
+  }
+}
+
+export { Templating };
