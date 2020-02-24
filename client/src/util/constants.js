600f9be18b3fc44f661246633e4c6acd6845c805
sangwon21 <junothyme@gmail.com>
2020-02-25 00:10:12 +0900

    feat: 서버 URL 추가
    why: 요청을 보낼 서버 URL을 추가했다. 해당 주소로 요청을 보내게 되면,
    응답으로, 요청 문자열 기준으로 정렬된 배열을 준다. 이를 클라이언트에서
    렌더링하면 된다.

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -1,8 +1,15 @@
 export const URL = {
-    DEV : {API_SERVER: {
-        ADDRESS: "http://localhost:8080/cards"
-    }},
-    PROD:{API_SERVER: {
-        ADDRESS: "http://not_existed"
-    }}
+    DEV: {
+        API_SERVER_CARD: {
+            ADDRESS: "http://localhost:8080/cards"
+        },
+        API_SERVER_SEARCH: {
+            ADDRESS: "http://localhost:8080/search/"
+        }
+    },
+    PROD: {
+        API_SERVER: {
+            ADDRESS: "http://not_existed"
+        }
+    }
 }
