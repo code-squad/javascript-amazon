5a59247d80b876dc368ae068bc9cc53ee27c809e
sangwon21 <junothyme@gmail.com>
2020-02-24 14:50:32 +0900

    feat: search api 작성
    why: 클라이언트에서 특정 문자열을 기준으로, 그 특정 문자열로 시작하는
    책 제목들을 배열에 담아 보내주는, search api를 작성했다. 특히, 문자열은
    대소문자를 구별하기 때문에, 대소문자 처리를 해주고 데이터를 처리하도록
    했다.

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const searchRouter = require("./routes/search");
 
 const app = express();
 
@@ -24,14 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,7 +43,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Express server has started on port ${process.env.PORT || 3000}`)
 })
 
---
+++ server/routes/search.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const bookList = require("../public/json/book_list.json");
+
+/* GET users listing. */
+router.get('/:target', function (req, res, next) {
+    const { target } = req.params;
+    const { books } = bookList;
+
+    const hitList = [];
+
+    books.reduce((acc, cur) => {
+        console.log("cur is ", cur);
+        if (cur.toLowerCase().startsWith(target.toLowerCase())) {
+            acc.push(cur);
+        }
+        return acc;
+    }, hitList);
+
+    res.contentType('application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    hitList.length ? res.json({ hitList }) : res.json({});
+});
+
+module.exports = router;
