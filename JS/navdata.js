a65ed2c6f66877a1676ec8585d746fb3669c86af
최장호 <hmchoi651@naver.com>
2020-02-24 18:35:11 +0900

    [업데이트] JS 파일
    nav와 carousel data를 담고있는 mockdata 파일 생성
    mockdata를 기반으로 html에 그려주는 inner 파일 생성

---
+++ JS/navdata.js
@@ -0,0 +1,19 @@
+const navData = [
+  {
+    className: "nav_item",
+    innerText: "Menu_1"
+  },
+  {
+    className: "nav_item",
+    innerText: "Menu_2"
+  },
+  {
+    className: "nav_item",
+    innerText: "Menu_3"
+  },
+  {
+    className: "nav_item",
+    innerText: "Menu_4"
+  }
+];
+export default navData;
