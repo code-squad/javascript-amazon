fb1bb6ddb4add835e60b328099f3f0a9ac11c8c9
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-02-25 15:57:50 +0900

    [파일추가 및 수정] post 요청 처리 기능 추가
    post 요청 처리 기능 추가.
    
    Server.js >
    * Client 의 post 요청 (사용자 입력에 대한 suggestion list 반환)
      을 처리한는 기능 추가.
    
      -> words 배열에서 사용자 입력을 시작으로 하는 단어 배열을
         Client 에게 response 로 전달
    
    words.js (New file) >
    * Alphabet 순서로 정렬된 영단어 배열 추가

--- Server/Server.js
+++ Server/Server.js
@@ -4,7 +4,28 @@ const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const data = fs.readFileSync('localData.json', 'utf8');
+const words = require('./words.js').words;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => res.send(data));
+app.post('/', function(req, res) {
+    const extractedWords = extractSuggestionWord(req.body.userInputText);
+    res.send(extractedWords);
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+function extractSuggestionWord(userInputText) {
+    const extractedWords = [];
+
+    words.forEach(element => {
+        if (element.indexOf(userInputText) === 0) {
+            extractedWords.push(element);
+        }
+    });
+
+    return extractedWords;
+}
