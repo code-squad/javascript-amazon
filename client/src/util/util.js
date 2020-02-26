086b0087f7db81dfc73ddc158251cf6d669da2d8
sangwon21 <junothyme@gmail.com>
2020-02-26 23:16:35 +0900

    feat: CSS 스타일 다루는 util 함수 추가
    why: 일일이 DOM 조작을 하면서 CSS를 다루는 건, 성능적인 측면에서도,
    그리고 코드 가독성 측면에서도, 별로 좋지 못하다. 코드는 코드대로
    길어지고, 성능은 성능대로 계속해서 해당 DOM을 찾아야하기 떄문에 좋지
    못하다. 결정적으로 무엇을 하는 행동인지 명시적이지 못한 경우가 많아서
    이해하기 힘들다. 이에 이런 행동들을 한 번 감싸는 방법으로 util함수를
    만들었다.

--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,3 +1,22 @@
 export const $ = target => document.querySelector(target);
 export const $$ = target => document.querySelectorAll(target);
 
+Element.prototype.show = function () {
+    this.style.display = "block";
+    return this;
+}
+
+Element.prototype.hide = function () {
+    this.style.display = "hidden";
+    return this;
+}
+
+Element.prototype.html = function (innerHTML) {
+    this.innerHTML = innerHTML;
+    return this;
+}
+
+Element.prototype.setScrollTop = function (scrollUnit) {
+    this.style.scrollTop = scrollUnit;
+    return this;
+}
