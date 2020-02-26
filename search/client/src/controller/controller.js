a908da8524340f6df04c4d97df5d425b33ffd8fb
Hu2y <sanghun_@daum.net>
2020-02-26 18:01:15 +0900

    [add] controller 정의
     - input event를 달아주고,  입력된 value를 검증하는 함수를 만들었다.
     - 영어만 입력받도록 했고, 입력단어가 1글자 이상일때만 동작하도록 구성했다.
     - 데이터를 받아온다음에 view를 그리기 위해 async를 사용하였다.
     - input창이 비워진다면 자동완성탭을 사라지도록 추가하였다.

---
+++ search/client/src/controller/controller.js
@@ -0,0 +1,36 @@
+function Controller() {
+  this.searchInput = $("#searchInput");
+  this.autoList = $(".autoList")
+  this.searchModel = new SearchModel();
+  this.init();
+}
+
+Controller.prototype = {
+  init() {
+    this.inputHandler();
+  },
+
+  inputHandler() {
+    this.searchInput.addEventListener("input", this.validationInput.bind(this));
+  },
+
+  validationInput(e) {
+    let message = e.target.value;
+    let pattern = /^[a-zA-Z]+$/;
+    this.validateInput = message.length > 0 && pattern.test(message) === true;
+    if (this.validateInput === true) {
+      this.autoList.style.display = 'block'
+      const connectModel = async () => {
+        try {
+          await this.searchModel.fetchData(message);
+          await new autoCompleteView(this.searchModel.matchingItem);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      connectModel();
+    } else {
+      this.autoList.style.display = 'none'
+    }
+  }
+};
