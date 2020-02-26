a2ef5c76a37e199c66fa1588bac8473f383586ae
Reese Kim <reesekimm@gmail.com>
2020-02-26 20:48:48 +0900

    Apply express routing
    routing structure
    localhost:8080
     └ /amazon
    　└ /carousel-slider
    　└ /autocomplete

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import logger from "morgan";
+import router from "./router.js";
+import routes from "./routes.js";
 
 const PORT = 8080;
 
 const app = express();
 
 app.use(logger("dev"));
+
+app.use(routes.amazon, router);
 
 const handleListening = () =>
   console.log(`Listening on http://localhost:${PORT}...`);
---
+++ routes.js
@@ -0,0 +1,11 @@
+const AMAZON = "/amazon";
+const CAROUSEL_SLIDER = "/carousel-slider";
+const SEARCH_AUTOCOMPLETE = "/autocomplete";
+
+const routes = {
+  amazon: AMAZON,
+  carouselSlider: CAROUSEL_SLIDER,
+  searchAutocomplete: SEARCH_AUTOCOMPLETE
+};
+
+export default routes;
