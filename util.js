1a0f27ce0773b63ea01504e1a7a6283c5da056da
kiyoesjh <hyeyaa125@gmail.com>
2020-02-26 17:08:44 +0900

    delete old file

--- util.js
+++
@@ -1,8 +0,0 @@
-const $ = (select) => document.querySelector(select);
-const $$ = {
-  _prevBtn : $('.left-btn'),
-  _nextBtn : $('.right-btn'),
-  _slideWrap : $('.slide-list'),
-  _slideLi : $('.slide-list').children,
-  _pageNavi : $('.page-nav').querySelectorAll('li')
-};
