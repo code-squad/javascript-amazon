db8e05cf09f80fc73f322e1661d1b22455e9105f
Reese Kim <reesekimm@gmail.com>
2020-02-26 14:36:46 +0900

    Modify when to fetch data and render slider
    - When 'DOMContentLoaded' event occurred, decide to fetch data or render slider depending on the presence of cached data
    
    - Modify constant
        - naming CARDWIDTH -> CARD_WIDTH
        - add PORT constant

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 import { $qs } from "./util.js";
 import Carousel from "./components/carousel.js";
 
-const CARDWIDTH = 1080;
-const URL = "http://localhost:8080/amazon/slider.json";
+const CARD_WIDTH = 1080;
+const PORT = 8080;
+const URL = `http://localhost:${PORT}/amazon/slider.json`;
 
 let dataForSlider = JSON.parse(localStorage.getItem("slider"));
 
-if (!dataForSlider) {
+const fetchData = () => {
   fetch(URL)
     .then(res => res.json())
     .then(data => {
+      renderSlider(data);
       dataForSlider = data;
       localStorage.setItem("slider", JSON.stringify(data));
     })
     .catch(err => console.log(err));
-}
+};
 
-const renderSlider = () => {
-  const carouselSlider = new Carousel(dataForSlider, CARDWIDTH);
+const renderSlider = data => {
+  const carouselSlider = new Carousel(data, CARD_WIDTH);
   $qs("#carousel-slider").innerHTML = carouselSlider.render();
   carouselSlider.activateSlideAnimation();
 };
 
-window.addEventListener("load", renderSlider);
+window.addEventListener("DOMContentLoaded", () => {
+  dataForSlider ? renderSlider(dataForSlider) : fetchData();
+});
