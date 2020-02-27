f3fbbdcdad07a5968e929525e13cd2a1359b8949
Hu2y <sanghun_@daum.net>
2020-02-27 13:01:59 +0900

    [add] 자동완성 리스트에 키보드 위아래, 이벤트 추가하기 위해 util 추가

--- search/client/utlis.js
+++ search/client/utlis.js
@@ -1,2 +1,5 @@
-const $ = el => document.querySelector(el)
-const $$ = el => document.querySelectorAll(el)
+const $ = el => document.querySelector(el);
+const $$ = el => document.querySelectorAll(el);
+
+const KEYUP = 38;
+const KEYDOWN = 40;
