2704ac8fd3818571579226ed2b540b1b9ce1ae0f
Hu2y <sanghun_@daum.net>
2020-02-27 21:01:02 +0900

    [add] 자동완성 결과 keyDown 구현 (코드 정리 필요...)

--- search/client/src/controller/controller.js
+++ search/client/src/controller/controller.js
@@ -1,8 +1,8 @@
 function Controller() {
   this.searchInput = $("#searchInput");
   this.autoList = $(".autoList");
-  this.searchList = $(".searchList li")
   this.searchModel = new SearchModel();
+  this.autoListIndex = 0;
   this.init();
 }
 
@@ -14,8 +14,8 @@ Controller.prototype = {
 
   inputHandler() {
     this.searchInput.addEventListener("input", this.validationInput.bind(this));
-    // this.searchInput.addEventListener("keyup", this.inputKeyupEvent.bind(this));
-    // this.searchInput.addEventListener("keydown", this.inputKeydownEvent.bind(this));
+    this.searchInput.addEventListener("keyup", this.inputKeyupEvent.bind(this));
+    this.searchInput.addEventListener("keydown", this.inputKeydownEvent.bind(this));
   },
 
   validationInput(e) {
@@ -42,11 +42,43 @@ Controller.prototype = {
   },
 
   inputKeyupEvent(e) {
+    this.searchList = $(".searchList")
 
+    if(e.keyCode !== KEYUP) return
+    
   },
 
-  inputKeydownEvent() {
+  inputKeydownEvent(e) { //  일단 작동은 됨, 로직이 엉망.. 다 구현 후 코드 정리
+    this.searchList = $(".searchList")
 
+    if(e.keyCode !== KEYDOWN) return
+    if(this.autoListIndex === 0) {
+      this.autoListIndex++
+      this.selectClass(this.autoListIndex, 'add')
+    } else if (this.autoListIndex > 0 && this.autoListIndex < this.searchList.childElementCount) {
+      this.autoListIndex++
+      this.selectClass(this.autoListIndex, 'add')
+      this.selectClass(this.autoListIndex-1, 'remove')
+      this.scrollControl()
+    } else if (this.autoListIndex === this.searchList.childElementCount) {
+      this.selectClass(this.autoListIndex, 'remove')
+      this.autoListIndex = 0
+      this.autoList.scrollTop = 0
+    }
+  },
+
+  selectClass(index, method) { // Prototype로 util에 만들어 놓기.. Array.prototype.addOrRemove = function(...){}
+    if(method === 'add') {
+      $(`li:nth-child(${index})`).classList.add('selected')
+    } else if(method === 'remove') {
+      $(`li:nth-child(${index})`).classList.remove('selected')
+    } else {
+      return
+    }  
+  },
+  
+  scrollControl() {
+    this.autoList.scrollTop += 18 // 일단 구현부터...
   }
-
+  
 };
