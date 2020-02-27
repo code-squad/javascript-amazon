7c38efc83d9f6f04d0c9a497798af464a2b7a186
Hu2y <sanghun_@daum.net>
2020-02-27 20:56:45 +0900

    [utils] classList add,remove 유틸 추가 [로직을 더 정확하게 구현 해야됨]

--- search/client/utlis.js
+++ search/client/utlis.js
@@ -3,3 +3,14 @@ const $$ = el => document.querySelectorAll(el);
 
 const KEYUP = 38;
 const KEYDOWN = 40;
+
+// classList li용도... 
+Array.prototype.selectClassList = function ({targetClass, method, addClassName, index}) {
+  if(method === add) {
+    $(`${targetClass} li:nth-child(${index})`).classList.add(`${addClassName}`)
+  } else if(method === remove) {
+    $(`${targetClass} li:nth-child(${index})`).classList.remove(`${addClassName}`)
+  } else {
+    return
+  }
+}
