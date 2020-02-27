1c0aa99caa3e3d6ed54ae5d605fb7c878f49874f
Hu2y <sanghun_@daum.net>
2020-02-27 17:53:59 +0900

    [modify] 중첩함수 분리 함수안 비동기 로직은 IIFE로 즉시실행

--- search/client/src/controller/controller.js
+++ search/client/src/controller/controller.js
@@ -1,36 +1,52 @@
 function Controller() {
   this.searchInput = $("#searchInput");
   this.autoList = $(".autoList");
+  this.searchList = $(".searchList li")
   this.searchModel = new SearchModel();
   this.init();
 }
 
 Controller.prototype = {
+  constrouctor: Controller,
   init() {
     this.inputHandler();
   },
 
   inputHandler() {
     this.searchInput.addEventListener("input", this.validationInput.bind(this));
+    // this.searchInput.addEventListener("keyup", this.inputKeyupEvent.bind(this));
+    // this.searchInput.addEventListener("keydown", this.inputKeydownEvent.bind(this));
   },
 
   validationInput(e) {
     let message = e.target.value;
     let pattern = /^[a-zA-Z]+$/;
     this.validateInput = message.length > 0 && pattern.test(message) === true;
-    if (this.validateInput === true) {
+    if (this.validateInput) {
       this.autoList.style.display = 'block';
-      const connectModel = async () => {
-        try {
-          await this.searchModel.fetchData(message);
-          await new autoCompleteView(this.searchModel.matchingItem, message);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      connectModel();
+      this.connectModel(message)
     } else {
       this.autoList.style.display = 'none';
     }
+  },
+
+  connectModel(message) {
+    (async () => {
+      try {
+        await this.searchModel.fetchData(message);
+        new autoCompleteView(this.searchModel.matchingItem, message);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  },
+
+  inputKeyupEvent(e) {
+
+  },
+
+  inputKeydownEvent() {
+
   }
+
 };
