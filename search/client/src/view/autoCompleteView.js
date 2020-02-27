27b341e1f425e88da0c8090104685de3c72123d2
Hu2y <sanghun_@daum.net>
2020-02-27 20:59:42 +0900

    [modify] prototype객체 constructor 속성을 추가

--- search/client/src/view/autoCompleteView.js
+++ search/client/src/view/autoCompleteView.js
@@ -7,6 +7,7 @@ function autoCompleteView(autoCompleteData, inputValue) {
   
 
 autoCompleteView.prototype = {
+  constructor: autoCompleteView,
   render() {
     const autoCompleteList = this.autoCompleteData.reduce((template, autoItem) => 
     (template += `<li><span style="color: #42B883">${autoItem.substring(0, this.inputValue.length)}</span>${autoItem.substring(this.inputValue.length)}</li>`) , "");
--- search/client/src/view/searchBarView.js
+++ search/client/src/view/searchBarView.js
@@ -3,6 +3,7 @@ function SearchBarView() {
 }
 
 SearchBarView.prototype = {
+  constructor: SearchBarView,
   render() {
     const template = `<div class="searchContainer"><span class="amazonImage"><a href="" class="amazonLogo-link"><span class="amazonLogo"></span></a></span><form class="searchForm" action=""><div class="searchContent"><input autocomplete="off" id="searchInput" placeholder="Enter some text" name="name"/><div class="searchBtn"></div><div class="autoList"></div></div></form></div>`;
     return template;
