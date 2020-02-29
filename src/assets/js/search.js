dd2cb611e62b67c680d8a24af802fb923bb9f2ee
Reese Kim <reesekimm@gmail.com>
2020-02-29 21:47:43 +0900

    Render UI components

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -7,8 +7,30 @@ const SearchUI = function(container) {
 };
 
 SearchUI.prototype = {
-  render: function(container) {},
-  setElements: function(container) {},
+  render: function(container) {
+    const CLASSNAME_FORM = "search__form";
+    const CLASSNAME_INPUT = "search__input";
+    const CLASSNAME_BTN = "search__btn";
+    const CLASSNAME_AUTO_BOX = "search__auto-box";
+    const CLASSNAME_AUTO_ITEM = "search__auto-item";
+    const searchBar = (_, ...args) =>
+      `<form class="${args[0]}" method="get"><input class="${args[1]}" type="text" name="term" /><button class="${args[2]}">🔍</button></form>`;
+    const autoCompeleArea = (_, ...args) =>
+      `<ul class="${args[0]}" action="#none" style="display: none;"><li cass="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li><li class="${args[1]}"></li></ul>`;
+
+    container.innerHTML += searchBar`${CLASSNAME_FORM} ${CLASSNAME_INPUT} ${CLASSNAME_BTN}}`;
+    container.innerHTML += autoCompeleArea`${CLASSNAME_AUTO_BOX} ${CLASSNAME_AUTO_ITEM}`;
+  },
+  setElements: function(container) {
+    this.container = container;
+    this.formElement = this.container.querySelector(".search__form");
+    this.inputElement = this.container.querySelector(".search__input");
+    this.autoCompleteElement = this.container.querySelector(
+      ".search__auto-box"
+    );
+    this.formUI = new FormUI();
+    this.autoCompeleUI = new AutoCompleteUI();
+  },
   bindEventListeners: function() {}
 };
 
