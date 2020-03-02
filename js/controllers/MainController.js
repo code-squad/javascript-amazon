5427ab3d2d5ae5d605cbf49a78a5b2782272e552
baekCode <sosaer88@gmail.com>
2020-03-02 14:49:59 +0900

    add, 검색결과 출력 (view 입력 -> 컨트롤러 ->  모델 요청&전송 -> 결과뷰 출력)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,19 +1,19 @@
 import { $, $$ } from '../Util.js';
 import SearchView from '../views/SearchView.js';
-import SearchModel from '../models/SearchModel.js'
+import ResultView from '../views/ResultView.js';
+import SearchModel from '../models/SearchModel.js';
 
 export default {
     init() {
+        SearchModel.localStorageJson();
         SearchView.setup($('#search'))
             .on('@input', e => this.onInput(e.detail.input))
             .on('@keydown', () => this.onKeyDown())
             .on('@keyup', () => this.onKeyUp());
+        ResultView.setup($('#search'));
     },
     onInput(input) {
-        const keywordList = [];
-        if (input != '') {
-            this.onSearch(input);
-        }
+        return this.onSearch(input);
     },
     onKeyDown() {
         console.log('keydown');
@@ -22,6 +22,21 @@ export default {
         console.log('keyup');
     },
     onSearch(input) {
-        SearchModel.find(input)
+        if (!input) return this.onResultReset();
+        SearchModel.find().then(data => {
+            const list = [];
+            data.forEach(str => {
+                if (str.slice(0, input.length) === input) {
+                    list.push(str);
+                }
+            });
+            this.onSearchResult(list);
+        });
+    },
+    onSearchResult(list) {
+        ResultView.render(list);
+    },
+    onResultReset() {
+        ResultView.reset();
     }
 };
--- js/models/SearchModel.js
+++ js/models/SearchModel.js
@@ -1,5 +1,23 @@
 export default {
-    find(input){
-        console.log('model-',input)
+    localStorageJson() {
+        const keywordDATA = localStorage.getItem('keywordDATA');
+        if (!keywordDATA) {
+            const apiServer =
+                'https://baekcode.github.io/codesquad-FE/day4_search/keyword.json';
+            fetch(apiServer)
+                .then(res => res.text())
+                .then(body => {
+                    localStorage.setItem('keywordDATA', body);
+                });
+        }
+    },
+    find() {
+        const getData = localStorage.getItem('keywordDATA');
+        const getDataParse = JSON.parse(getData).keyword;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(getDataParse);
+            }, 0);
+        });
     }
-}
+};
