142ab024369ed1ead4f87f16d82a5af98ea2ec06
choisohyun <sohyun4283@naver.com>
2020-03-04 21:12:18 +0900

    feat. Handle when input search keyword
    - 검색 키워드 입력 시 화면 처리하는 기능 추가
    - 자동완성 리스트는 autoComplete에서 처리한 것을 기반으로 될 것임
    - autoComplete는 다음 커밋으로 할 예정

--- client/js/search/searchBox.js
+++ client/js/search/searchBox.js
@@ -18,6 +18,27 @@ SearchBox.prototype = {
       searchContent.className += " nav-active";
       event.stopPropagation();
     });
+  },
+
+  inputEvent(searchKeyword) {
+    const autoComplete = document.querySelector(".auto-complete");
+    const autoList = document.querySelectorAll(".auto-complete > ul > li");
+
+    window.addEventListener("click", () => {
+      autoComplete.style.visibility = "hidden";
+    });
+
+    if (searchKeyword) {
+      autoComplete.style.visibility = "visible";
+      autoList.forEach(value => {
+        value.innerHTML = `
+        <span style="font-weight: bold;">
+          ${searchKeyword}
+        </span>`;
+      });
+    } else {
+      autoComplete.style.visibility = "hidden";
+    }
   }
 };
 
